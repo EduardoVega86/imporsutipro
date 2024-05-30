d3d9aabf7942ece0c8346c636756e881327751f5
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-05-30 15:05:00 -0500

    pagina de login

--- Views/login.php
+++ Views/login.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <style>
         body {
-            background-color: #11143b;
+            background-color: #171931;
             color: #fff;
             background-size: cover;
             background-position: center;
