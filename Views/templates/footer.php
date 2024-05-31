1bb15cfd8205e4ebe7be4f3b6c66204e0556c67c
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-05-31 16:49:31 -0500

    menu nuevo

--- Views/templates/footer.php
+++ Views/templates/footer.php
@@ -1,13 +1,38 @@
+      <!-- Fin del contenido de la página -->
+      </div>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <link href="https://cdn.datatables.net/v/bs5/jq-3.7.0/jszip-3.10.1/dt-2.0.8/af-2.7.0/b-3.0.2/b-colvis-3.0.2/b-html5-3.0.2/b-print-3.0.2/cr-2.0.3/date-1.5.2/fc-5.0.1/fh-4.0.1/kt-2.12.1/r-3.0.2/rg-1.5.0/rr-1.5.0/sc-2.4.3/sb-1.7.1/sp-2.3.1/sl-2.0.3/sr-1.4.1/datatables.min.css" rel="stylesheet">
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
 <script src="https://cdn.datatables.net/v/bs5/jq-3.7.0/jszip-3.10.1/dt-2.0.8/af-2.7.0/b-3.0.2/b-colvis-3.0.2/b-html5-3.0.2/b-print-3.0.2/cr-2.0.3/date-1.5.2/fc-5.0.1/fh-4.0.1/kt-2.12.1/r-3.0.2/rg-1.5.0/rr-1.5.0/sc-2.4.3/sb-1.7.1/sp-2.3.1/sl-2.0.3/sr-1.4.1/datatables.min.js"></script>
+
+
+<script>
+        $(document).ready(function() {
+            const sidebar = $('#sidebar');
+            const toggleBtn = $('#toggle-btn');
+            const isSidebarCollapsed = localStorage.getItem('isSidebarCollapsed') === 'true';
+
+            if (isSidebarCollapsed) {
+                sidebar.addClass('sidebar-collapsed');
+                $('.content').addClass('content-collapsed');
+                $('.menu-text').hide();
+            }
+
+            toggleBtn.on('click', function() {
+                sidebar.toggleClass('sidebar-collapsed');
+                $('.content').toggleClass('content-collapsed');
+                $('.menu-text').toggle();
+                localStorage.setItem('isSidebarCollapsed', sidebar.hasClass('sidebar-collapsed'));
+            });
+        });
+    </script>
 </body>
+
+
 
 </html>
--- Views/templates/header.php
+++ Views/templates/header.php
@@ -6,79 +6,70 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IMPORSUITPRO</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
     <style>
         body {
-            font-family: Arial, sans-serif;
-        }
-        .navbar {
-            background-color: #1a1a2e;
-        }
-        .navbar-brand {
-            color: #fff;
-            font-weight: bold;
-        }
-        .navbar-toggler {
-            border: none;
-            color: #fff;
-            font-size: 24px;
-            background: none;
-        }
-        .nav-link {
-            color: #fff;
-        }
-        .right-section {
-            color: #fff;
-            margin-left: auto;
             display: flex;
-            align-items: center;
         }
-        .offcanvas-header {
-            border-bottom: 1px solid #ddd;
+        .sidebar {
+            width: 250px;
+            position: fixed;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            background-color: #343a40;
+            padding-top: 1rem;
         }
-        .offcanvas-body {
-            padding-top: 10px;
+        .sidebar a {
+            color: #fff;
+            padding: 10px;
+            text-decoration: none;
+            display: block;
+        }
+        .sidebar a:hover {
+            background-color: #007bff;
+        }
+        .content {
+            margin-left: 250px;
+            padding: 1rem;
+            width: 100%;
+        }
+        .sidebar-collapsed {
+            width: 80px;
+        }
+        .content-collapsed {
+            margin-left: 80px;
         }
     </style>
 </head>
 
-<body>
-<nav class="navbar navbar-expand-lg navbar-dark">
-    <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu" aria-controls="offcanvasMenu">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <a class="navbar-brand" href="#">IMPOR</a>
-        <div class="right-section">
-            <a href="#" class="nav-link">Tutoriales</a>
-            <span class="nav-link">$0.00</span>
-            <a href="#" class="nav-link"><i class="fa fa-sign-out-alt"></i></a>
-        </div>
-    </div>
-</nav>
+<body class="">
 
-<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasMenu" aria-labelledby="offcanvasMenuLabel">
-    <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasMenuLabel">Menu</h5>
-        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+<div class="sidebar" id="sidebar">
+        <a href="#" class="toggle-btn" id="toggle-btn">
+            <box-icon name="menu" color="#fff"></box-icon>
+        </a>
+        <a href="#"><box-icon name="home" color="#fff"></box-icon> <span class="menu-text">Inicio</span></a>
+        <a href="#"><box-icon name="cart" color="#fff"></box-icon> <span class="menu-text">Marketplace</span></a>
+        <a href="#"><box-icon name="user" color="#fff"></box-icon> <span class="menu-text">Clientes</span></a>
+        <a href="#"><box-icon name="cube" color="#fff"></box-icon> <span class="menu-text">Productos</span></a>
+        <a href="#"><box-icon name="shopping-bag" color="#fff"></box-icon> <span class="menu-text">Compras</span></a>
+        <a href="#"><box-icon name="receipt" color="#fff"></box-icon> <span class="menu-text">Pedidos</span></a>
+        <!-- Agrega más enlaces según sea necesario -->
     </div>
-    <div class="offcanvas-body">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Services</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Contact</a>
-            </li>
-        </ul>
-    </div>
-</div>
+    <div class="content">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">IMPORSUITPRO</a>
+                <div class="d-flex">
+                    <a class="nav-link" href="#">Tutoriales</a>
+                    <span class="navbar-text">$0.00</span>
+                </div>
+            </div>
+        </nav>
+        <!-- Aquí va el contenido de la página -->
