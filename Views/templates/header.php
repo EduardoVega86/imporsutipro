91dab3e5bdbc10b35ddb3ab0b502f30d5f0b93b7
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-01 09:55:57 -0500

    menu

--- Views/templates/header.php
+++ Views/templates/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -92,6 +93,16 @@
 
         .submenu-popup .menu-text {
             opacity: 1 !important;
+            display: inline !important;
+        }
+
+        .submenu-popup a {
+            padding: 5px 10px;
+            color: #fff;
+            padding: 10px;
+            text-decoration: none;
+            display: flex;
+            align-items: center;
         }
 
         .navbar-custom {
