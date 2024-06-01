fcea063a51aa9972a09bd419c88e64848d758431
User <User@DESKTOP-9FVJ3SL>
2024-06-01 12:07:53 -0500

    MARKETPLACE

--- Config/App/Conexion.php
+++ Config/App/Conexion.php
@@ -3,17 +3,21 @@ class Conexion
 {
     private $connection;
     private $marketplace;
+    
     public function __construct()
     {
+        date_default_timezone_set('America/Guayaquil');
         $pdo = "mysql:host=" . HOST . ";dbname=" . DB . ";charset=" . CHARSET;
         try {
             $this->connection = new PDO($pdo, USER, PASSWORD);
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // Establecer la zona horaria para la sesión actual de MySQL
+            $this->connection->exec("SET time_zone = '-05:00';");
         } catch (PDOException $e) {
-
-            echo "Error: " . $e->getMessage();
+            $this->handleError($e->getMessage());
         }
     }
+
     public function connect()
     {
         return $this->connection;
@@ -22,5 +26,14 @@ class Conexion
     public function close()
     {
         $this->connection = null;
+    }
+
+    private function handleError($message)
+    {
+        echo json_encode([
+            'status' => 'error',
+            'message' => $message
+        ]);
+        exit;
     }
 }
