0142d8347534ea1067516e1279f990e0e49580f4
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-03 10:08:05 -0500

    reponsive dashboar y menu

--- Views/Dashboard/dashboard.php
+++ Views/Dashboard/dashboard.php
@@ -73,26 +73,29 @@
         background: #343a40;
         color: #fff;
     }
+
     /* secciones principales  */
     .banner_estadisticas {
         display: flex;
         flex-direction: row;
     }
-    .tablas_estaditicas .content-container{
+
+    .tablas_estaditicas .content-container {
         display: flex;
         flex-direction: row;
     }
 
     .custom-container-fluid {
-    /* Extiende las propiedades de container-fluid */
-    padding-left: 15px;
-    padding-right: 15px;
-    margin-left: auto;
-    margin-right: auto;
-    /* Añade o sobrescribe propiedades aquí */
-    background-color: #f8f9fa; /* Ejemplo de propiedad sobrescrita */
-    /* Añade otras propiedades necesarias */
-}
+        /* Extiende las propiedades de container-fluid */
+        padding-left: 15px;
+        padding-right: 15px;
+        margin-left: auto;
+        margin-right: auto;
+        /* Añade o sobrescribe propiedades aquí */
+        background-color: #f8f9fa;
+        /* Ejemplo de propiedad sobrescrita */
+        /* Añade otras propiedades necesarias */
+    }
 
     /* responsive */
     @media (max-width: 768px) {
@@ -100,15 +103,15 @@
             flex-direction: column-reverse !important;
         }
 
-        .tablas_estaditicas .content-container{
+        .tablas_estaditicas .content-container {
             flex-direction: column !important;
         }
 
-        .stats-container{
+        .stats-container {
             width: 100%;
         }
 
-        .slider-container{
+        .slider-container {
             width: 100%;
         }
 
@@ -116,9 +119,10 @@
             width: 100%;
             max-width: calc(105% - 40px);
         }
-        .custom-container-fluid{
-            width: 100%;
-            margin: 0;
+
+        .custom-container-fluid {
+            width: 95%;
+            margin-left: 35px;
         }
     }
 </style>
@@ -227,7 +231,7 @@
                                 <td>20000</td>
                             </tr>
                         </tbody>
-                        </table>
+                    </table>
                 </div>
             </div>
         </div>
