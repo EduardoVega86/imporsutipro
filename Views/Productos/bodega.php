63d3c260a86fff0d9b7ee9fe1e5c42c515533470
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-04 16:55:07 -0500

    agregar_index

---
+++ Views/Productos/bodega.php
@@ -0,0 +1,69 @@
+<?php require_once './Views/templates/header.php'; ?>
+
+<style>
+    .table-striped tbody tr:nth-of-type(odd) {
+        background-color: rgba(0, 0, 0, .05);
+    }
+
+    .table-hover tbody tr:hover {
+        background-color: rgba(0, 0, 0, .075);
+    }
+
+    .table thead th {
+        background-color: #171931;
+        color: white;
+    }
+
+    .table th,
+    .table td {
+        text-align: center;
+        vertical-align: middle;
+    }
+
+    .centered {
+        text-align: center !important;
+        vertical-align: middle !important;
+    }
+</style>
+
+<style>
+    .filtros_producos {
+        display: flex;
+        flex-direction: row;
+    }
+
+    @media (max-width: 768px) {
+        .filtros_producos {
+            flex-direction: column;
+        }
+    }
+</style>
+<div class="custom-container-fluid">
+    <div class="container mt-5" style="max-width: 1600px;">
+        <h2 class="text-center mb-4">Guias</h2>
+        <!-- <div class="filtros_producos justify-content-between align-items-center mb-3">
+
+        </div> -->
+        <div class="table-responsive">
+            <!-- <table class="table table-bordered table-striped table-hover"> -->
+            <table id="datatable_guias" class="table table-striped">
+                <!-- <caption>
+                    DataTable.js Demo
+                </caption> -->
+                <thead>
+                    <tr>
+                        <th class="centered">ID</th>
+                        <th class="centered">Nombre</th>
+                        <th class="centered">Ciudad</th>
+                        <th class="centered">Responsable</th>
+                        <th class="centered">Telefono</th>
+                        <th class="centered">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody id="tableBody_guias"></tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<script src="<?php echo SERVERURL?>/Views/Pedidos/js/guias.js"></script> 
+<?php require_once './Views/templates/footer.php'; ?>
---
+++ Views/Productos/js/bodega.js
@@ -0,0 +1,97 @@
+let dataTable;
+let dataTableIsInitialized = false;
+
+const dataTableOptions = {
+  //scrollX: "2000px",
+  /* lengthMenu: [5, 10, 15, 20, 100, 200, 500], */
+  columnDefs: [
+    { className: "centered", targets: [0, 1, 2, 3, 4, 5, 6, 7, 8] },
+    /* { orderable: false, targets: [5, 6] }, */
+    /* { searchable: false, targets: [1] } */
+    //{ width: "50%", targets: [0] }
+  ],
+  pageLength: 10,
+  destroy: true,
+  language: {
+    lengthMenu: "Mostrar _MENU_ registros por página",
+    zeroRecords: "Ningún usuario encontrado",
+    info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+    infoEmpty: "Ningún usuario encontrado",
+    infoFiltered: "(filtrados desde _MAX_ registros totales)",
+    search: "Buscar:",
+    loadingRecords: "Cargando...",
+    paginate: {
+      first: "Primero",
+      last: "Último",
+      next: "Siguiente",
+      previous: "Anterior",
+    },
+  },
+};
+
+const initDataTable = async () => {
+  if (dataTableIsInitialized) {
+    dataTable.destroy();
+  }
+
+  await listGuias();
+
+  dataTable = $("#datatable_guias").DataTable(dataTableOptions);
+
+  dataTableIsInitialized = true;
+};
+
+const listGuias = async () => {
+  try {
+    const response = await fetch(
+      "https://new.imporsuitpro.com/productos/listarBodegas"
+    );
+    const guias = await response.json();
+
+    let content = ``;
+    guias.forEach((guia, index) => {
+      content += `
+                <tr>
+                    <td>${guia.id}</td
+                    <td>${guia.nombre}</td
+                    <td>${guia.direccion}</td>
+                    <td>${guia.responsable}</td>
+                    <td>${guia.contacto}</td>
+                    <td>
+                        <button class="btn btn-sm btn-primary"><i class="fa-solid fa-pencil"></i></button>
+                        <button class="btn btn-sm btn-danger"><i class="fa-solid fa-trash-can"></i></button>
+                    </td>
+                </tr>`;
+    });
+    document.getElementById("tableBody_guias").innerHTML = content;
+  } catch (ex) {
+    alert(ex);
+  }
+};
+
+window.addEventListener("load", async () => {
+  await initDataTable();
+});
+
+function formatPhoneNumber(number) {
+  // Eliminar caracteres no numéricos excepto el signo +
+  number = number.replace(/[^\d+]/g, "");
+
+  // Verificar si el número ya tiene el código de país +593
+  if (/^\+593/.test(number)) {
+    // El número ya está correctamente formateado con +593
+    return number;
+  } else if (/^593/.test(number)) {
+    // El número tiene 593 al inicio pero le falta el +
+    return "+" + number;
+  } else {
+    // Si el número comienza con 0, quitarlo
+    if (number.startsWith("0")) {
+      number = number.substring(1);
+    }
+    // Agregar el código de país +593 al inicio del número
+    number = "+593" + number;
+  }
+
+  return number;
+}
