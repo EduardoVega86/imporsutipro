f563d2b784be40656aeb8254e2a64b71b7580098
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-04 14:45:08 -0500

    agregar_index

--- Views/Pedidos/js/guias.js
+++ Views/Pedidos/js/guias.js
@@ -3,14 +3,14 @@ let dataTableIsInitialized = false;
 
 const dataTableOptions = {
     //scrollX: "2000px",
-    lengthMenu: [5, 10, 15, 20, 100, 200, 500],
+    /* lengthMenu: [5, 10, 15, 20, 100, 200, 500], */
     columnDefs: [
         { className: "centered", targets: [0, 1, 2, 3, 4, 5,6,7,8] },
         /* { orderable: false, targets: [5, 6] }, */
         /* { searchable: false, targets: [1] } */
         //{ width: "50%", targets: [0] }
     ],
-    pageLength: 3,
+    pageLength: 10,
     destroy: true,
     language: {
         lengthMenu: "Mostrar _MENU_ registros por página",
