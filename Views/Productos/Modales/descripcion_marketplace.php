325d13226dd9e1b63b93b0dd6f448fdbf632b43d
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-05 13:29:42 -0500

    agregar_index

--- Views/Productos/Modales/descripcion_marketplace.php
+++ Views/Productos/Modales/descripcion_marketplace.php
@@ -1,35 +1,60 @@
 <div class="modal fade" id="descripcion_productModal" tabindex="-1" aria-labelledby="descripcion_productModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="descripcion_productModalLabel">Descripción del Producto</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-md-6">
-              <h3 class="mb-3" style="text-decoration:underline;"><strong>Información</strong></h3>
-              <p class="texto_modal"><strong>Código del Producto:</strong> 10179</p>
-              <p class="texto_modal"><strong>Nombre Producto:</strong> Cepillo Secador</p>
-              <p class="texto_modal"><strong>Precio Proveedor:</strong> $10.00</p>
-              <p class="texto_modal"><strong>Precio Sugerido:</strong> $13.00</p>
-              <p class="texto_modal"><strong>Stock:</strong> <span class="text-success"><strong>1</strong></span></p>
-              <p class="texto_modal"><strong>Proveedor:</strong> <a href="https://edishop.imporsuit.com" target="_blank">https://edishop.imporsuit.com</a></p>
-              <p class="texto_modal"><a href="tel:+593998011578">+593998011578</a></p>
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="descripcion_productModalLabel">Descripción del Producto</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="col-md-6">
-              <img src="https://example.com/image.jpg" class="img-fluid rounded" alt="Product Image">
+            <div class="modal-body">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12 col-md-6 mb-4 mb-md-0">
+                            <h3 class="mb-3" style="text-decoration:underline;"><strong>Información</strong></h3>
+                            <p class="texto_modal"><strong>Código del Producto:</strong> 10179</p>
+                            <p class="texto_modal"><strong>Nombre Producto:</strong> Cepillo Secador</p>
+                            <p class="texto_modal"><strong>Precio Proveedor:</strong> $10.00</p>
+                            <p class="texto_modal"><strong>Precio Sugerido:</strong> $13.00</p>
+                            <p class="texto_modal"><strong>Stock:</strong> <span class="text-success"><strong>1</strong></span></p>
+                            <p class="texto_modal"><strong>Proveedor:</strong> <a href="https://edishop.imporsuit.com" target="_blank">https://edishop.imporsuit.com</a></p>
+                            <p class="texto_modal"><a href="tel:+593998011578">+593998011578</a></p>
+                        </div>
+                        <div class="col-12">
+                            <div id="productCarousel" class="carousel slide" data-bs-ride="carousel">
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <img src="https://example.com/image1.jpg" class="d-block w-100 fixed-size-img" alt="Product Image 1">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="https://example.com/image2.jpg" class="d-block w-100 fixed-size-img" alt="Product Image 2">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="https://example.com/image3.jpg" class="d-block w-100 fixed-size-img" alt="Product Image 3">
+                                    </div>
+                                </div>
+                                <button class="carousel-control-prev" type="button" data-bs-target="#productCarousel" data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#productCarousel" data-bs-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                </button>
+                            </div>
+                            <div class="carousel-thumbnails mt-2 d-flex justify-content-center">
+                                <img src="https://example.com/image1.jpg" class="img-thumbnail mx-1" alt="Thumbnail 1" data-bs-target="#productCarousel" data-bs-slide-to="0">
+                                <img src="https://example.com/image2.jpg" class="img-thumbnail mx-1" alt="Thumbnail 2" data-bs-target="#productCarousel" data-bs-slide-to="1">
+                                <img src="https://example.com/image3.jpg" class="img-thumbnail mx-1" alt="Thumbnail 3" data-bs-target="#productCarousel" data-bs-slide-to="2">
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <h6 class="mb-3">Descripción</h6>
+                    <p>Cepillo Secador</p>
+                </div>
             </div>
-          </div>
-          <hr>
-          <h3 class="mb-3" style="text-decoration:underline;"><strong>Descripción</strong></h3>
-          <p class="texto_modal">Cepillo Secador</p>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+            </div>
         </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-      </div>
     </div>
-  </div>
 </div>
