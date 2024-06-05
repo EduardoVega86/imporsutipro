f93c40ca7bef2e610aea9e672842422eaa5dcab9
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-05 12:19:36 -0500

    agregar_index

--- Views/Productos/Modales/descripcion_marketplace.php
+++ Views/Productos/Modales/descripcion_marketplace.php
@@ -8,7 +8,7 @@
       <div class="modal-body">
         <div class="container-fluid">
           <div class="row">
-            <div class="col-md-6">
+            <div class="col-12 col-md-6 mb-4 mb-md-0">
               <h3 class="mb-3" style="text-decoration:underline;"><strong>Información</strong></h3>
               <p class="texto_modal"><strong>Código del Producto:</strong> 10179</p>
               <p class="texto_modal"><strong>Nombre Producto:</strong> Cepillo Secador</p>
@@ -18,13 +18,33 @@
               <p class="texto_modal"><strong>Proveedor:</strong> <a href="https://edishop.imporsuit.com" target="_blank">https://edishop.imporsuit.com</a></p>
               <p class="texto_modal"><a href="tel:+593998011578">+593998011578</a></p>
             </div>
-            <div class="col-md-6">
-              <img src="https://example.com/image.jpg" class="img-fluid rounded" alt="Product Image">
+            <div class="col-12">
+              <div id="productCarousel" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="https://example.com/image1.jpg" class="d-block w-100 fixed-size-img" alt="Product Image 1">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="https://example.com/image2.jpg" class="d-block w-100 fixed-size-img" alt="Product Image 2">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="https://example.com/image3.jpg" class="d-block w-100 fixed-size-img" alt="Product Image 3">
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#productCarousel" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#productCarousel" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
             </div>
           </div>
           <hr>
-          <h3 class="mb-3" style="text-decoration:underline;"><strong>Descripción</strong></h3>
-          <p class="texto_modal">Cepillo Secador</p>
+          <h6 class="mb-3">Descripción</h6>
+          <p>Cepillo Secador</p>
         </div>
       </div>
       <div class="modal-footer">
