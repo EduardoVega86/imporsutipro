d417f757153710cf3d8323179842b25f4cb80c91
Jeimy Jara <jeimyjara@hotmail.com>
2024-06-06 10:28:45 -0500

    Eliminar categoria

--- Models/ProductosModel.php
+++ Models/ProductosModel.php
@@ -81,7 +81,7 @@ class ProductosModel extends Query
     {
         // codigo para eliminar categoria
         $response = $this->initialResponse();
-
+        echo $plataforma;
         $sql = "DELETE FROM lineas WHERE id_linea = ? AND id_plataforma = ?";
         $data = [$id, $plataforma];
         $eliminar_categoria = $this->delete($sql, $data);
