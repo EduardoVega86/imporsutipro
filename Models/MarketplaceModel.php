04863dec1b146d618214b50600e2a7e4660a46ca
User <User@DESKTOP-9FVJ3SL>
2024-06-07 17:59:00 -0500

    producto

--- Models/MarketplaceModel.php
+++ Models/MarketplaceModel.php
@@ -17,8 +17,9 @@ class ProductosModel extends Query
     public function agregarMarketplace($codigo_producto,  $plataforma)
     {
         $response = $this->initialResponse();
-        $sql_update="update productos set drogshipin=1 where id_producto=$codigo_producto";
-        $actualizar_stock= $this->simple_select($sql_update);
+        $sql_update="update productos set drogshipin=? where id_producto=?";
+        $data_update=[$codigo_producto, $plataforma];
+        $actualizar_stock= $this->simple_select($sql_update, $data_update);
  if($actualizar_stock==1){
        $response['status'] = 200;
        $response['title'] = 'Peticion exitosa';
@@ -31,331 +32,27 @@ class ProductosModel extends Query
         return $response;
     }
 
-    public function agregarTmp($id_producto, $cantidad, $precio,  $plataforma)
+    public function agregarTmp($id_producto, $cantidad, $precio,  $plataforma, $sku)
     {
     $timestamp = time() . '_' . session_id();   
-
-
-
-        $sql = "SELECT * FROM productos p inner join inventario_bodegas ib on p.codigo_producto = ib.sku WHERE p.id_producto = $id AND p.id_plataforma = $plataforma";
-        return $this->select($sql);
-    }
-
-    public function guardar_imagen_productos()
-    {
-        $response = $this->initialResponse();
-        $target_dir = "public/img/productos";
-        $target_file = $target_dir . basename($_FILES["file"]["name"]);
-        $uploadOk = 1;
-        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-        $check = getimagesize($_FILES["file"]["tmp_name"]);
-        if ($check !== false) {
-            $uploadOk = 1;
-        } else {
-            $response['status'] = 500;
-            $response['title'] = 'Error';
-            $response['message'] = 'El archivo no es una imagen';
-            $uploadOk = 0;
-        }
-        if ($_FILES["file"]["size"] > 500000) {
-            $response['status'] = 500;
-            $response['title'] = 'Error';
-            $response['message'] = 'El archivo es muy grande';
-            $uploadOk = 0;
-        }
-        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-            $response['status'] = 500;
-            $response['title'] = 'Error';
-            $response['message'] = 'Solo se permiten archivos JPG, JPEG, PNG';
-            $uploadOk = 0;
-        }
-        if ($uploadOk == 0) {
-            $response['status'] = 500;
-            $response['title'] = 'Error';
-            $response['message'] = 'Error al subir la imagen';
-        } else {
-            if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
-                $response['status'] = 200;
-                $response['title'] = 'Peticion exitosa';
-                $response['message'] = 'Imagen subida correctamente';
-                $response['data'] = $target_file;
-            } else {
-                $response['status'] = 500;
-                $response['title'] = 'Error';
-                $response['message'] = 'Error al subir la imagen';
-            }
-        }
-        return $response;
-    }
-
-    public function obtener_productos_bodegas($id_bodega, $plataforma)
-    {
-        $sql = "SELECT * FROM inventario_bodegas WHERE id_bodega = $id_bodega and id_plataforma = $plataforma";
-        return $this->select($sql);
-    }
-
-    ///categorias
-
-    public function agregarCategoria($nombre_linea, $descripcion_linea, $estado_linea, $date_added, $online, $imagen, $tipo, $padre, $plataforma)
-    {
-        // codigo para agregar categoria
-        $response = $this->initialResponse();
-
-        $sql = "INSERT INTO lineas (nombre_linea, descripcion_linea, estado_linea, date_added, online, imagen, tipo, padre, id_plataforma) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        $data = [$nombre_linea, $descripcion_linea, $estado_linea, $date_added, $online, $imagen, $tipo, $padre, $plataforma];
-        $insertar_categoria = $this->insert($sql, $data);
-        if ($insertar_categoria == 1) {
-            $response['status'] = 200;
-            $response['title'] = 'Peticion exitosa';
-            $response['message'] = 'Categoria agregada correctamente';
-        } else {
-            $response['status'] = 500;
-            $response['title'] = 'Error';
-            $response['message'] = 'Error al agregar la categoria';
-        }
-        return $response;
-    }
-
-
-    public function cargarCategorias($plataforma)
-    {
-        $sql = "SELECT * FROM lineas WHERE id_plataforma = $plataforma";
-        return $this->select($sql);
-    }
-
-    public function editarCategoria($id, $nombre_linea, $descripcion_linea, $estado_linea, $date_added, $online, $imagen, $tipo, $padre, $plataforma)
-    {
-        // codigo para editar categoria
-        $response = $this->initialResponse();
-
-        $sql = "UPDATE lineas SET nombre_linea = ?, descripcion_linea = ?, estado_linea = ?, date_added = ?, online = ?, imagen = ?, tipo = ?, padre = ? WHERE id_linea = ? AND id_plataforma = ?";
-        $data = [$nombre_linea, $descripcion_linea, $estado_linea, $date_added, $online, $imagen, $tipo, $padre, $id, $plataforma];
-        $editar_categoria = $this->update($sql, $data);
-        if ($editar_categoria == 1) {
-            $response['status'] = 200;
-            $response['title'] = 'Peticion exitosa';
-            $response['message'] = 'Categoria editada correctamente';
-        } else {
-            $response['status'] = 500;
-            $response['title'] = 'Error';
-            $response['message'] = 'Error al editar la categoria';
-        }
-        return $response;
-    }
-
-    public function eliminarCategoria($id, $plataforma)
-    {
-        // codigo para eliminar categoria
-        $response = $this->initialResponse();
-
-        $sql = "DELETE FROM lineas WHERE id_linea = ? AND id_plataforma = ?";
-        $data = [$id, $plataforma];
-        $eliminar_categoria = $this->delete($sql, $data);
-        if ($eliminar_categoria == 1) {
-            $response['status'] = 200;
-            $response['title'] = 'Peticion exitosa';
-            $response['message'] = 'Categoria eliminada correctamente';
-        } else {
-            $response['status'] = 500;
-            $response['title'] = 'Error';
-            $response['message'] = 'Error al eliminar la categoria';
-        }
-        return $response;
-    }
-
-
-    public function listarCategoria($id, $plataforma)
-    {
-        $sql = "SELECT * FROM lineas WHERE id_linea = $id AND id_plataforma = $plataforma";
-        return $this->select($sql);
-    }
-
-    public function guardar_imagen_categorias($imagen, $id_categoria, $plataforma)
-    {
-        $response = $this->initialResponse();
-        $target_dir = "public/img/categorias";
-        $target_file = $target_dir . basename($_FILES["file"]["name"]);
-        $uploadOk = 1;
-        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-        $check = getimagesize($_FILES["file"]["tmp_name"]);
-        if ($check !== false) {
-            $uploadOk = 1;
-        } else {
-            $response['status'] = 500;
-            $response['title'] = 'Error';
-            $response['message'] = 'El archivo no es una imagen';
-            $uploadOk = 0;
-        }
-        if ($_FILES["file"]["size"] > 500000) {
-            $response['status'] = 500;
-            $response['title'] = 'Error';
-            $response['message'] = 'El archivo es muy grande';
-            $uploadOk = 0;
-        }
-        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-            $response['status'] = 500;
-            $response['title'] = 'Error';
-            $response['message'] = 'Solo se permiten archivos JPG, JPEG, PNG';
-            $uploadOk = 0;
-        }
-        if ($uploadOk == 0) {
-            $response['status'] = 500;
-            $response['title'] = 'Error';
-            $response['message'] = 'Error al subir la imagen';
-        } else {
-            if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
-                $response['status'] = 200;
-                $response['title'] = 'Peticion exitosa';
-                $response['message'] = 'Imagen subida correctamente';
-                $response['data'] = $target_file;
-
-                $sql = "UPDATE lineas SET imagen = ? WHERE id_linea = ? AND id_plataforma = ?";
-                $data = [$target_file, $id_categoria, $plataforma];
-                $editar_imagen = $this->update($sql, $data);
-                if ($editar_imagen == 1) {
-                    $response['status'] = 200;
-                    $response['title'] = 'Peticion exitosa';
-                    $response['message'] = 'Imagen subida correctamente';
-                } else {
-                    $response['status'] = 500;
-                    $response['title'] = 'Error';
-                    $response['message'] = 'Error al subir la imagen';
-                }
-            } else {
-                $response['status'] = 500;
-                $response['title'] = 'Error';
-                $response['message'] = 'Error al subir la imagen';
-            }
-        }
-        return $response;
-    }
-
-    ///bodegas
-
-    public function agregarBodega($nombre, $direccion, $telefono, $ciudad, $provincia, $contacto, $telefono_contacto, $numerocasa, $referencia, $plataforma, $longitud, $latitud)
-    {
-        // codigo para agregar categoria
-        $response = $this->initialResponse();
-
-        $sql = "INSERT INTO `bodega` (`nombre`,`id_empresa`,`longitud`,`latitud`,`direccion`,`num_casa`,`referencia`,`responsable`,`contacto`,`localidad`,`provincia`,`id_plataforma`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        $data = [$nombre, $plataforma, $longitud, $latitud, $direccion, $numerocasa, $referencia, $contacto, $telefono_contacto, $ciudad, $provincia, $plataforma];
-        $insertar_categoria = $this->insert($sql, $data);
-        if ($insertar_categoria == 1) {
-            $response['status'] = 200;
-            $response['title'] = 'Peticion exitosa';
-            $response['message'] = 'Categoria agregada correctamente';
-        } else {
-            $response['status'] = 500;
-            $response['title'] = 'Error';
-            $response['message'] = $insertar_categoria['message'];
-        }
-        return $response;
-    }
-
-    public function obtenerBodega($id, $plataforma)
-    {
-        $sql = "SELECT * FROM bodega WHERE id= $id AND id_plataforma = $plataforma";
-        return $this->select($sql);
-    }
-
-    public function editarBodega($id, $nombre, $direccion, $telefono, $ciudad, $provincia, $contacto, $telefono_contacto, $numerocasa, $referencia, $plataforma, $longitud, $latitud)
-    {
-        // codigo para editar categoria
-        $response = $this->initialResponse();
-
-        $sql = "UPDATE `bodega` SET `nombre` = ?, `longitud` = ?, `latitud` = ?, `direccion` = ?, `num_casa` = ?, `referencia` = ?, `responsable` = ?, `contacto` = ?, `localidad` = ?, `provincia` = ? WHERE `id` = ? AND `id_plataforma` = ?";
-        $data = [$nombre, $longitud, $latitud, $direccion, $numerocasa, $referencia, $contacto, $telefono_contacto, $ciudad, $provincia, $id, $plataforma];
-        $editar_categoria = $this->update($sql, $data);
-        if ($editar_categoria == 1) {
-            $response['status'] = 200;
-            $response['title'] = 'Peticion exitosa';
-            $response['message'] = 'Categoria editada correctamente';
-        } else {
-            $response['status'] = 500;
-            $response['title'] = 'Error';
-            $response['message'] = $editar_categoria['message'];
-        }
-        return $response;
-    }
-
-    public function eliminarBodega($id, $plataforma)
-    {
-        // codigo para eliminar categoria
-        $response = $this->initialResponse();
-
-        $sql = "DELETE FROM bodega WHERE id_bodega = ? AND id_plataforma = ?";
-        $data = [$id, $plataforma];
-        $eliminar_categoria = $this->delete($sql, $data);
-        if ($eliminar_categoria == 1) {
-            $response['status'] = 200;
-            $response['title'] = 'Peticion exitosa';
-            $response['message'] = 'Categoria eliminada correctamente';
-        } else {
-            $response['status'] = 500;
-            $response['title'] = 'Error';
-            $response['message'] = $eliminar_categoria['message'];
-        }
-        return $response;
-    }
-
-
-
-
-    public function listarBodegas($plataforma)
-    {
-        $sql = "SELECT * FROM bodega WHERE id_plataforma in ('$plataforma') or global = 1";
-        return $this->select($sql);
-    }
-
-
-
-    ///caracteristicas
-
-    public function agregarCaracteristica($variedad, $id_atributo, $plataforma)
-    {
-        $response = $this->initialResponse();
-        $sql = "INSERT INTO variedades (variedad, id_atributo, id_plataforma) VALUES (?, ?, ?)";
-        $data = [$variedad, $id_atributo, $plataforma];
+        $sql = "INSERT INTO `tmp_cotizacion` (`id_producto`, `cantidad_tmp`, `precio_tmp`, `desc_tmp`, `session_id`, `id_plataforma`, `sku`) VALUES ('1', '1', '1', '1', '1', '1', '1');";
+        $data = [$id_producto, $cantidad, $precio, $timestamp, $plataforma, $sku];
         $insertar_caracteristica = $this->insert($sql, $data);
         if ($insertar_caracteristica == 1) {
             $response['status'] = 200;
             $response['title'] = 'Peticion exitosa';
-            $response['message'] = 'Caracteristica agregada correctamente';
+            $response['message'] = 'Producto agregado al carrito';
         } else {
             $response['status'] = 500;
             $response['title'] = 'Error';
             $response['message'] = 'Error al agregar la caracteristica';
         }
         return $response;
+        
     }
 
-    public function listarCaracteristicas($plataforma)
-    {
-        $sql = "SELECT * FROM variedades WHERE id_plataforma = $plataforma";
-        return $this->select($sql);
-    }
+    
+  
 
-    public function listarAtributos($id, $plataforma)
-    {
-        $sql = "SELECT * FROM atributos WHERE id_plataforma in (0, $plataforma)";
-        return $this->select($sql);
-    }
-
-    public function eliminarCaracteristica($id, $plataforma)
-    {
-        $response = $this->initialResponse();
-        $sql = "DELETE FROM variedades WHERE id_variedad = ? AND id_plataforma = ?";
-        $data = [$id, $plataforma];
-        $eliminar_caracteristica = $this->delete($sql, $data);
-        if ($eliminar_caracteristica == 1) {
-            $response['status'] = 200;
-            $response['title'] = 'Peticion exitosa';
-            $response['message'] = 'Caracteristica eliminada correctamente';
-        } else {
-            $response['status'] = 500;
-            $response['title'] = 'Error';
-            $response['message'] = 'Error al eliminar la caracteristica';
-        }
-        return $response;
-    }
+ 
 }
