a05fd19ef065cfa936782b70b2f3b75bd3d09cb4
Jeimy Jara <jeimyjara@hotmail.com>
2024-06-07 15:43:53 -0500

    productos

--- Models/ProductosModel.php
+++ Models/ProductosModel.php
@@ -21,8 +21,6 @@ class ProductosModel extends Query
         $data = [$codigo_producto, $nombre_producto, $descripcion_producto, $id_linea_producto, $inv_producto, $producto_variable, $costo_producto, $aplica_iva, $estado_producto, $date_added, $image_path, $id_imp_producto, $pagina_web, $formato, $drogshipin, $destacado, $plataforma];
         $insertar_producto = $this->insert($sql, $data);
 
-        print_r($insertar_producto);
-
         $sql_id = "SELECT id_producto FROM productos WHERE codigo_producto = $codigo_producto AND id_plataforma = $plataforma";
         $id_producto = $this->select($sql_id);
         $id_producto = $id_producto[0]['id_producto'];
@@ -58,6 +56,8 @@ class ProductosModel extends Query
             $response['title'] = 'Error';
             $response['message'] = 'Error al agregar el producto';
         }
+
+        return $response;
     }
 
     public function guardar_imagen_productos()
