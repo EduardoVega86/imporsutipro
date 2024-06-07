c009e2fc65c90ddce9538d91076e70b239b79943
Jeimy Jara <jeimyjara@hotmail.com>
2024-06-07 14:09:30 -0500

    productos

--- Controllers/Pedidos.php
+++ Controllers/Pedidos.php
@@ -51,79 +51,8 @@ class Pedidos extends Controller
     ///Funciones
     public function nuevo_pedido()
     {
-        $fecha = date("Y-m-d H:i:s");
-        $id_cliente = 1;
-        $id_vendedor = 1;
-        $condiciones = 1;
-        $monto_factura = $_POST['monto_factura'];
-        $estado_factura = 1;
-        $id_user_factura = 1;
-        $validez = 3;
-        $id_sucursal = 1;
-        $nombre = $_POST['nombre'];
-        $telefono = $_POST['telefono'];
-        $provincia = $_POST['provincia'];
-        $calle_principal = $_POST['calle_principal'];
-        $ciudad = $_POST['ciudad'];
-        $calle_secundaria = $_POST['calle_secundaria'];
-        $referencia = $_POST['referencia'];
-        $observacion = $_POST['observacion'];
-        $guia_enviada = 0;
-        $transporte = NULL;
-        $identificacion = NULL;
-        $celular = $_POST['telefono'];
-        $cod = NULL;
-        $valor_segura = NULL;
-        $dropshipping = $_POST['dropshipping'];
-        $tienda = $_POST['tienda'];
-        $importado = NULL;
-        $plataforma_importa = NULL;
-        $estado_guia_sistema = 1;
-        $id_factura_origen = NULL;
-        $impreso = NULL;
-        $facturada = NULL;
-        $factura_numero = NULL;
-        $numero_guia = NULL;
-        $anulado = 0;
-
-        $response =  $this->model->generarPedido(
-            $fecha,
-            $id_cliente,
-            $id_vendedor,
-            $condiciones,
-            $monto_factura,
-            $estado_factura,
-            $id_user_factura,
-            $validez,
-            $id_sucursal,
-            $nombre,
-            $telefono,
-            $provincia,
-            $calle_principal,
-            $ciudad,
-            $calle_secundaria,
-            $referencia,
-            $observacion,
-            $guia_enviada,
-            $transporte,
-            $identificacion,
-            $celular,
-            $cod,
-            $valor_segura,
-            $dropshipping,
-            $tienda,
-            $importado,
-            $plataforma_importa,
-            $estado_guia_sistema,
-            $id_factura_origen,
-            $impreso,
-            $facturada,
-            $factura_numero,
-            $numero_guia,
-            $anulado,
-            $id_plataforma = $_SESSION['id_plataforma']
-        );
-        echo json_encode($response);
+        $fecha_factura = date("Y-m-d H:i:s");
+        $id_usuario = $_SESSION['id'];
     }
 
     public function obtener_guias($filtro = "")
@@ -131,5 +60,10 @@ class Pedidos extends Controller
 
         $data = $this->model->cargarGuias($filtro);
         echo json_encode($data);
+    }
+
+    public function info()
+    {
+        print_r($_SESSION);
     }
 }
