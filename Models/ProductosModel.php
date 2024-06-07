f266363530e66067d72762e33a617b1e029e5ccf
Jeimy Jara <jeimyjara@hotmail.com>
2024-06-07 16:49:36 -0500

    productos

--- Models/ProductosModel.php
+++ Models/ProductosModel.php
@@ -64,7 +64,7 @@ class ProductosModel extends Query
 
     public function obtenerProducto($id, $plataforma)
     {
-        $sql = "SELECT * FROM productos p inner join inventario_bodegas ib on p.codigo_producto = ib.sku WHERE id_producto = $id AND id_plataforma = $plataforma";
+        $sql = "SELECT * FROM productos p inner join inventario_bodegas ib on p.codigo_producto = ib.sku WHERE p.id_producto = $id AND p.id_plataforma = $plataforma";
         return $this->select($sql);
     }
 
