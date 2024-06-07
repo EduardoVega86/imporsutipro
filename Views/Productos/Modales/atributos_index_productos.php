5500c4558562defd15caf721d5b33854c69a67b8
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-07 11:57:02 -0500

    agregar_index

--- Views/Productos/Modales/atributos_index_productos.php
+++ Views/Productos/Modales/atributos_index_productos.php
@@ -16,7 +16,7 @@
 </style>
 
 <div class="modal fade" id="atributosModal" tabindex="-1" aria-labelledby="atributosModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog moidal-lg">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="atributosModalLabel">CARACTERÍSTICAS DE PRODUCTOS</h5>
