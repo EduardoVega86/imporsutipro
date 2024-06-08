b390d8d36d77b541175b409353c8b8adf8ded85c
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-08 11:54:18 -0500

    agregar_index

--- Views/Productos/js/marketplace.js
+++ Views/Productos/js/marketplace.js
@@ -1,32 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const productsPerPage = 10;
-    let currentPage = 1;
-    let products = [];
+document.addEventListener("DOMContentLoaded", function () {
+  const productsPerPage = 10;
+  let currentPage = 1;
+  let products = [];
 
-    const cardContainer = document.getElementById('card-container');
-    const pagination = document.getElementById('pagination');
+  const cardContainer = document.getElementById("card-container");
+  const pagination = document.getElementById("pagination");
 
-    async function fetchProducts() {
-        try {
-            const response = await fetch('' + SERVERURL + 'marketplace/obtener_productos');
-            products = await response.json();
-            displayProducts(products, currentPage, productsPerPage);
-            createPagination(products.length, productsPerPage);
-        } catch (error) {
-            console.error('Error al obtener los productos:', error);
-        }
+  async function fetchProducts() {
+    try {
+      const response = await fetch(
+        "" + SERVERURL + "marketplace/obtener_productos"
+      );
+      products = await response.json();
+      displayProducts(products, currentPage, productsPerPage);
+      createPagination(products.length, productsPerPage);
+    } catch (error) {
+      console.error("Error al obtener los productos:", error);
     }
+  }
 
-    function displayProducts(products, page = 1, perPage = productsPerPage) {
-        cardContainer.innerHTML = '';
-        const start = (page - 1) * perPage;
-        const end = start + perPage;
-        const paginatedProducts = products.slice(start, end);
+  function displayProducts(products, page = 1, perPage = productsPerPage) {
+    cardContainer.innerHTML = "";
+    const start = (page - 1) * perPage;
+    const end = start + perPage;
+    const paginatedProducts = products.slice(start, end);
 
-        paginatedProducts.forEach(product => {
-            const card = document.createElement('div');
-            card.className = 'card card-custom';
-            card.innerHTML = `
+    paginatedProducts.forEach((product) => {
+      const card = document.createElement("div");
+      card.className = "card card-custom";
+      card.innerHTML = `
             <img src="${product.img}" class="card-img-top" alt="Product Image">
             <div class="card-body text-center d-flex flex-column justify-content-between">
                 <div>
@@ -37,136 +39,178 @@ document.addEventListener('DOMContentLoaded', function() {
                 </div>
                 <div>
                     <button class="btn btn-description" onclick="agregarModal_marketplace(${product.id_producto})">Descripción</button>
-                    <button class="btn btn-import">Importar</button>
+                    <button class="btn btn-import" onclick="enviar_cliente(${product.id_producto})">Enviar a cliente</button>
                 </div>
             </div>
         `;
-            cardContainer.appendChild(card);
-        });
-    }
+      cardContainer.appendChild(card);
+    });
+  }
 
-    function createPagination(totalProducts, perPage = productsPerPage) {
-        pagination.innerHTML = '';
-        const totalPages = Math.ceil(totalProducts / perPage);
+  function createPagination(totalProducts, perPage = productsPerPage) {
+    pagination.innerHTML = "";
+    const totalPages = Math.ceil(totalProducts / perPage);
 
-        const previousPageItem = document.createElement('li');
-        previousPageItem.className = 'page-item';
-        previousPageItem.innerHTML = `
+    const previousPageItem = document.createElement("li");
+    previousPageItem.className = "page-item";
+    previousPageItem.innerHTML = `
         <button class="page-link" aria-label="Previous">
             <span aria-hidden="true">&laquo;</span>
         </button>
     `;
-        previousPageItem.addEventListener('click', function() {
-            if (currentPage > 1) {
-                currentPage--;
-                displayProducts(products, currentPage, productsPerPage);
-                createPagination(totalProducts, perPage);
-            }
-        });
-        pagination.appendChild(previousPageItem);
+    previousPageItem.addEventListener("click", function () {
+      if (currentPage > 1) {
+        currentPage--;
+        displayProducts(products, currentPage, productsPerPage);
+        createPagination(totalProducts, perPage);
+      }
+    });
+    pagination.appendChild(previousPageItem);
 
-        for (let i = 1; i <= totalPages; i++) {
-            const pageItem = document.createElement('li');
-            pageItem.className = `page-item ${i === currentPage ? 'active' : ''}`;
-            pageItem.innerHTML = `
+    for (let i = 1; i <= totalPages; i++) {
+      const pageItem = document.createElement("li");
+      pageItem.className = `page-item ${i === currentPage ? "active" : ""}`;
+      pageItem.innerHTML = `
             <button class="page-link">${i}</button>
         `;
-            pageItem.addEventListener('click', function() {
-                currentPage = i;
-                displayProducts(products, currentPage, productsPerPage);
-                createPagination(totalProducts, perPage);
-            });
-            pagination.appendChild(pageItem);
-        }
+      pageItem.addEventListener("click", function () {
+        currentPage = i;
+        displayProducts(products, currentPage, productsPerPage);
+        createPagination(totalProducts, perPage);
+      });
+      pagination.appendChild(pageItem);
+    }
 
-        const nextPageItem = document.createElement('li');
-        nextPageItem.className = 'page-item';
-        nextPageItem.innerHTML = `
+    const nextPageItem = document.createElement("li");
+    nextPageItem.className = "page-item";
+    nextPageItem.innerHTML = `
         <button class="page-link" aria-label="Next">
             <span aria-hidden="true">&raquo;</span>
         </button>
     `;
-        nextPageItem.addEventListener('click', function() {
-            if (currentPage < totalPages) {
-                currentPage++;
-                displayProducts(products, currentPage, productsPerPage);
-                createPagination(totalProducts, perPage);
-            }
-        });
-        pagination.appendChild(nextPageItem);
+    nextPageItem.addEventListener("click", function () {
+      if (currentPage < totalPages) {
+        currentPage++;
+        displayProducts(products, currentPage, productsPerPage);
+        createPagination(totalProducts, perPage);
+      }
+    });
+    pagination.appendChild(nextPageItem);
 
-        updatePaginationButtons(totalPages);
-    }
+    updatePaginationButtons(totalPages);
+  }
 
-    function updatePaginationButtons(totalPages) {
-        const previousPageItem = pagination.querySelector('.page-item:first-child');
-        const nextPageItem = pagination.querySelector('.page-item:last-child');
+  function updatePaginationButtons(totalPages) {
+    const previousPageItem = pagination.querySelector(".page-item:first-child");
+    const nextPageItem = pagination.querySelector(".page-item:last-child");
 
-        previousPageItem.classList.toggle('disabled', currentPage === 1);
-        nextPageItem.classList.toggle('disabled', currentPage === totalPages);
-    }
+    previousPageItem.classList.toggle("disabled", currentPage === 1);
+    nextPageItem.classList.toggle("disabled", currentPage === totalPages);
+  }
 
-    fetchProducts();
+  fetchProducts();
 });
 
+//agregar informacion al modal descripcion marketplace
 function agregarModal_marketplace(id) {
-    $.ajax({
-        type: "POST",
-        url: SERVERURL + "marketplace/obtener_producto/" + id,
-        dataType: 'json',
-        success: function (response) {
-            console.log(response); // Depuración: Mostrar la respuesta en la consola
+  $.ajax({
+    type: "POST",
+    url: SERVERURL + "marketplace/obtener_producto/" + id,
+    dataType: "json",
+    success: function (response) {
+      if (response) {
+        // Obtener el primer objeto de la respuesta
+        const data = response[0];
 
-            if (response) {
-                // Obtener el primer objeto de la respuesta
-                const data = response[0];
+        // Llenar los elementos <span> del modal con los datos recibidos
+        $("#codigo_producto").text(data.codigo_producto);
+        $("#nombre_producto").text(data.nombre_producto);
+        $("#precio_proveedor").text(data.costo_producto);
+        $("#precio_sugerido").text(data.pvp);
+        $("#stock").text(data.saldo_stock);
+        $("#nombre_proveedor").text(data.contacto);
+        $("#telefono_proveedor").text(formatPhoneNumber(data.whatsapp));
+        $("#descripcion").text(data.descripcion_producto);
 
-                // Llenar los elementos <span> del modal con los datos recibidos
-                $('#codigo_producto').text(data.codigo_producto);
-                $('#nombre_producto').text(data.nombre_producto);
-                $('#precio_proveedor').text(data.costo_producto);
-                $('#precio_sugerido').text(data.pvp);
-                $('#stock').text(data.saldo_stock);
-                $('#nombre_proveedor').text(data.contacto);
-                $('#telefono_proveedor').text(formatPhoneNumber(data.whatsapp));
-                $('#descripcion').text(data.descripcion_producto);
+        // Actualizar el enlace con el número de teléfono del proveedor
+        $('a[href^="https://wa.me/"]').attr(
+          "href",
+          "https://wa.me/" + formatPhoneNumber(data.whatsapp)
+        );
 
-                // Actualizar el enlace con el número de teléfono del proveedor
-                $('a[href^="https://wa.me/"]').attr('href', 'https://wa.me/' + formatPhoneNumber(data.whatsapp));
+        // Abrir el modal
+        $("#descripcion_productModal").modal("show");
+      } else {
+        console.error("La respuesta está vacía o tiene un formato incorrecto.");
+      }
+    },
+    error: function (xhr, status, error) {
+      console.error("Error en la solicitud AJAX:", error);
+      alert("Hubo un problema al obtener la información del producto");
+    },
+  });
+}
 
+//enviar cliente
+function enviar_cliente(id) {
+  $.ajax({
+    type: "POST",
+    url: SERVERURL + "marketplace/obtener_producto/" + id,
+    dataType: "json",
+    success: function (response) {
+      if (response) {
+        const data = response[0];
 
-                // Abrir el modal
-                $('#descripcion_productModal').modal('show');
-            } else {
-                console.error("La respuesta está vacía o tiene un formato incorrecto.");
-            }
-        },
-        error: function (xhr, status, error) {
+        // Crear un objeto FormData y agregar los datos
+        const formData = new FormData();
+        formData.append("codigo_producto", data.codigo_producto);
+        formData.append("cantidad", 1);
+        formData.append("precio", data.pvp);
+
+        $.ajax({
+          type: "POST",
+          url: ""+SERVERURL +"marketplace/agregarTmp",
+          data: formData,
+          processData: false,
+          contentType: false,
+          success: function (response) {
+            console.log("Producto agregado temporalmente:", response);
+          },
+          error: function (xhr, status, error) {
             console.error("Error en la solicitud AJAX:", error);
-            alert("Hubo un problema al obtener la información del producto");
-        },
-    });
+            alert("Hubo un problema al agregar el producto temporalmente");
+          },
+        });
+      } else {
+        console.error("La respuesta está vacía o tiene un formato incorrecto.");
+      }
+    },
+    error: function (xhr, status, error) {
+      console.error("Error en la solicitud AJAX:", error);
+      alert("Hubo un problema al obtener la información del producto");
+    },
+  });
+}
 
-    function formatPhoneNumber(number) {
-        // Eliminar caracteres no numéricos excepto el signo +
-        number = number.replace(/[^\d+]/g, '');
-    
-        // Verificar si el número ya tiene el código de país +593
-        if (/^\+593/.test(number)) {
-            // El número ya está correctamente formateado con +593
-            return number;
-        } else if (/^593/.test(number)) {
-            // El número tiene 593 al inicio pero le falta el +
-            return '+' + number;
-        } else {
-            // Si el número comienza con 0, quitarlo
-            if (number.startsWith('0')) {
-                number = number.substring(1);
-            }
-            // Agregar el código de país +593 al inicio del número
-            number = '+593' + number;
-        }
-    
-        return number;
+function formatPhoneNumber(number) {
+  // Eliminar caracteres no numéricos excepto el signo +
+  number = number.replace(/[^\d+]/g, "");
+
+  // Verificar si el número ya tiene el código de país +593
+  if (/^\+593/.test(number)) {
+    // El número ya está correctamente formateado con +593
+    return number;
+  } else if (/^593/.test(number)) {
+    // El número tiene 593 al inicio pero le falta el +
+    return "+" + number;
+  } else {
+    // Si el número comienza con 0, quitarlo
+    if (number.startsWith("0")) {
+      number = number.substring(1);
     }
+    // Agregar el código de país +593 al inicio del número
+    number = "+593" + number;
+  }
+
+  return number;
 }
