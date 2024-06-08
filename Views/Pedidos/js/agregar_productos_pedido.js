5cd3b1f30c2d4a94b39258c290c23c4837573d0e
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-08 16:04:08 -0500

    agregar_index

--- Views/Pedidos/js/agregar_productos_pedido.js
+++ Views/Pedidos/js/agregar_productos_pedido.js
@@ -5,18 +5,14 @@ function getParameterByName(name) {
     const url = new URL(window.location.href);
     return url.searchParams.get(name);
 }
+
 // Obtener el id_producto de la URL
 const id_producto = getParameterByName('id_producto');
 const sku = getParameterByName('sku');
 
 const dataTableNuevosPedidosOptions = {
-    //scrollX: "2000px",
-    /* lengthMenu: [5, 10, 15, 20, 100, 200, 500], */
     columnDefs: [
-        { className: "centered", targets: [0, 1, 2, 3, 4, 5, 6] },
-        /* { orderable: false, targets: [5, 6] }, */
-        /* { searchable: false, targets: [1] } */
-        //{ width: "50%", targets: [0] }
+        { className: "centered", targets: [0, 1, 2, 3, 4, 5, 6] }
     ],
     pageLength: 10,
     destroy: true,
@@ -60,34 +56,42 @@ const listNuevosPedidos = () => {
         data: formData,
         processData: false,  // No procesar los datos
         contentType: false,  // No establecer ningún tipo de contenido
-        success: function(nuevosPedidos) {
-            let content = ``;
-            nuevosPedidos.forEach((nuevoPedido, index) => {
-                content += `
-                    <tr>
-                        <td>${nuevoPedido.image_path}</td>
-                        <td>${nuevoPedido.id_producto}</td>
-                        <td>${nuevoPedido.nombre_producto}</td>
-                        <td>${nuevoPedido.stock_inicial}</td>
-                        <td><input type="number" class="form-control" value="1" min="1" id="cantidad_${index}"></td>
-                        <td>${nuevoPedido.pvp}</td>
-                        <td>
-                            <button class="btn btn-sm btn-success"><i class="fa-solid fa-pencil"></i></button>
-                        </td>
-                    </tr>`;
-            });
-            document.getElementById('tableBody_nuevosPedidos').innerHTML = content;
-            $('#nuevosPedidosModal').modal('show');
+        success: function(response) {
+            console.log("Respuesta del servidor:", response);  // Verificar la respuesta
+
+            // Verificar si la respuesta es un array
+            if (Array.isArray(response)) {
+                let nuevosPedidos = response;
+                let content = ``;
+                nuevosPedidos.forEach((nuevoPedido, index) => {
+                    content += `
+                        <tr>
+                            <td>${nuevoPedido.image_path}</td>
+                            <td>${nuevoPedido.id_producto}</td>
+                            <td>${nuevoPedido.nombre_producto}</td>
+                            <td>${nuevoPedido.stock_inicial}</td>
+                            <td><input type="number" class="form-control" value="1" min="1" id="cantidad_${index}"></td>
+                            <td>${nuevoPedido.pvp}</td>
+                            <td>
+                                <button class="btn btn-sm btn-success"><i class="fa-solid fa-pencil"></i></button>
+                            </td>
+                        </tr>`;
+                });
+                document.getElementById('tableBody_nuevosPedidos').innerHTML = content;
+                $('#nuevosPedidosModal').modal('show');
+            } else {
+                console.error("La respuesta no es un array:", response);
+                alert("Error: La respuesta no tiene el formato esperado.");
+            }
         },
         error: function(jqXHR, textStatus, errorThrown) {
+            console.error("Error en la solicitud AJAX:", textStatus, errorThrown);
             alert(errorThrown);
         }
     });
 };
 
-
-
-//abrir modal
+// Abrir modal
 function buscar_productos_nuevoPedido(){
     $('#nuevosPedidosModal').modal('show');
 }
