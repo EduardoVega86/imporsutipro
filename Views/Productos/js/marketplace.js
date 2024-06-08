5aaf5e4b454705cf8ae1dd7e34550b05d6c0bf25
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-08 11:56:32 -0500

    agregar_index

--- Views/Productos/js/marketplace.js
+++ Views/Productos/js/marketplace.js
@@ -175,6 +175,22 @@ function enviar_cliente(id) {
           contentType: false,
           success: function (response) {
             console.log("Producto agregado temporalmente:", response);
+            if (data.status == 500) {
+                Swal.fire({
+                    icon: 'error',
+                    title: data.title,
+                    text: data.message
+                });
+            } else if (data.status == 200){
+
+                    Swal.fire({
+                        icon: 'success',
+                        title: data.title,
+                        text: data.message
+                    }).then(() => {
+                        window.location.href = '' + SERVERURL + 'Pedidos/nuevo';
+                    });
+            }
           },
           error: function (xhr, status, error) {
             console.error("Error en la solicitud AJAX:", error);
