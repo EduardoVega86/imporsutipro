81adc3f56149ff2b0abaa11e08c886f61f88ff75
User <User@DESKTOP-9FVJ3SL>
2024-06-10 10:49:21 -0500

    producto

--- Models/ProductosModel.php
+++ Models/ProductosModel.php
@@ -103,9 +103,15 @@ class ProductosModel extends Query
         return $response;
     }
 
-    public function obtenerProducto($id, $plataforma)
+    public function obtenerProducto($id, $plataforma, $id_linea)
     {
-        $sql = "SELECT * FROM productos p inner join inventario_bodegas ib on p.codigo_producto = ib.sku WHERE p.id_producto = $id AND p.id_plataforma = $plataforma";
+        if ($id_linea==0){
+          
+            $sql = "SELECT * FROM productos p inner join inventario_bodegas ib on p.codigo_producto = ib.sku WHERE p.id_producto = $id AND p.id_plataforma = $plataforma";
+        }else{
+           $sql = "SELECT * FROM productos p inner join inventario_bodegas ib on p.codigo_producto = ib.sku WHERE p.id_producto = $id AND p.id_plataforma = $plataforma and p.id_linea_producto=$id_linea";  
+        }
+       
         return $this->select($sql);
     }
 
