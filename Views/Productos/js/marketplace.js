ce80cf9c2590546a7a7e6dc493282f0c3798778f
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-10 15:27:27 -0500

    agregar_index

--- Views/Productos/js/marketplace.js
+++ Views/Productos/js/marketplace.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
             <div class="card-body text-center d-flex flex-column justify-content-between">
                 <div>
                     <h5 class="card-title">${product.nombre_producto}</h5>
-                    <p class="card-text">Stock: <strong style="color:green">${product.stock}</strong></p>
+                    <p class="card-text">Stock: <strong style="color:green">9</strong></p>
                     <p class="card-text">Precio Proveedor: <strong>$20</strong></p>
                     <p class="card-text">Precio Sugerido: <strong>$25</strong></p>
                     <p class="card-text">Proveedor: <a href="#">tony</a></p>
