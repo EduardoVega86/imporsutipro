0788504c35e8d1b8905a293fa0673c6659487f99
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-10 12:04:21 -0500

    agregar_index

--- Views/Pedidos/js/agregar_productos_pedido.js
+++ Views/Pedidos/js/agregar_productos_pedido.js
@@ -1,158 +1,167 @@
-$(document).ready(function () {
-    $('#nuevosPedidosModal').on('shown.bs.modal', function () {
-      initDataTableNuevosPedidos();
-    });
-  });
-  
-  let dataTableNuevosPedidos;
-  let dataTableNuevosPedidosIsInitialized = false;
-  
-  function getParameterByName(name) {
-    const url = new URL(window.location.href);
-    return url.searchParams.get(name);
-  }
-  
-  // Obtener el id_producto de la URL
-  const id_producto = getParameterByName("id_producto");
-  const sku = getParameterByName("sku");
-  
-  const dataTableNuevosPedidosOptions = {
-    columnDefs: [
-      { className: "centered", targets: [0, 1, 2, 3, 4, 5, 6] }
-    ],
-    pageLength: 10,
-    destroy: true,
-    language: {
-      lengthMenu: "Mostrar _MENU_ registros por página",
-      zeroRecords: "Ningún pedido encontrado",
-      info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
-      infoEmpty: "Ningún pedido encontrado",
-      infoFiltered: "(filtrados desde _MAX_ registros totales)",
-      search: "Buscar:",
-      autoWidth: false,
-      loadingRecords: "Cargando...",
-      paginate: {
-        first: "Primero",
-        last: "Último",
-        next: "Siguiente",
-        previous: "Anterior",
-      },
+let dataTableNuevosPedidos;
+let dataTableNuevosPedidosIsInitialized = false;
+
+function getParameterByName(name) {
+  const url = new URL(window.location.href);
+  return url.searchParams.get(name);
+}
+// Obtener el id_producto de la URL
+const id_producto = getParameterByName("id_producto");
+const sku = getParameterByName("sku");
+
+const dataTableNuevosPedidosOptions = {
+  //scrollX: "2000px",
+  /* lengthMenu: [5, 10, 15, 20, 100, 200, 500], */
+  columnDefs: [
+    { className: "centered", targets: [0, 1, 2, 3, 4, 5, 6] },
+    /* { orderable: false, targets: [5, 6] }, */
+    /* { searchable: false, targets: [1] } */
+    //{ width: "50%", targets: [0] }
+  ],
+  pageLength: 10,
+  destroy: true,
+  language: {
+    lengthMenu: "Mostrar _MENU_ registros por página",
+    zeroRecords: "Ningún pedido encontrado",
+    info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+    infoEmpty: "Ningún pedido encontrado",
+    infoFiltered: "(filtrados desde _MAX_ registros totales)",
+    search: "Buscar:",
+    loadingRecords: "Cargando...",
+    paginate: {
+      first: "Primero",
+      last: "Último",
+      next: "Siguiente",
+      previous: "Anterior",
     },
-  };
-  
-  const initDataTableNuevosPedidos = async () => {
-    if (dataTableNuevosPedidosIsInitialized) {
-      dataTableNuevosPedidos.destroy();
-    }
-  
-    await listNuevosPedidos();
-  
-    dataTableNuevosPedidos = $("#datatable_nuevosPedidos").DataTable(
-      dataTableNuevosPedidosOptions
-    );
-  
-    dataTableNuevosPedidosIsInitialized = true;
-  };
-  
-  const listNuevosPedidos = () => {
-    let formData = new FormData();
-    formData.append("sku", sku);
-  
+  },
+};
+
+const initDataTableNuevosPedidos = async () => {
+  if (dataTableNuevosPedidosIsInitialized) {
+    dataTableNuevosPedidos.destroy();
+  }
+
+  await listNuevosPedidos();
+
+  dataTableNuevosPedidos = $("#datatable_nuevosPedidos").DataTable(
+    dataTableNuevosPedidosOptions
+  );
+
+  dataTableNuevosPedidosIsInitialized = true;
+};
+
+const listNuevosPedidos = () => {
+  // Crear una instancia de FormData
+  let formData = new FormData();
+  formData.append("sku", sku); // Añadir el SKU al FormData
+
+  $.ajax({
+    url: SERVERURL + "pedidos/buscarProductosBodega/" + id_producto,
+    type: "POST", // Cambiar a POST para enviar FormData
+    data: formData,
+    processData: false, // No procesar los datos
+    contentType: false, // No establecer ningún tipo de contenido
+    success: function (response) {
+      console.log("Respuesta del servidor:", response); // Verificar la respuesta
+
+      // Verificar si la respuesta es un JSON y tiene el formato esperado
+      let nuevosPedidos = response;
+      if (typeof response === "string") {
+        nuevosPedidos = JSON.parse(response);
+      }
+
+      if (Array.isArray(nuevosPedidos)) {
+        let content = ``;
+        nuevosPedidos.forEach((nuevoPedido, index) => {
+          content += `
+                        <tr>
+                            <td>${nuevoPedido.image_path}</td>
+                            <td>${nuevoPedido.id_producto}</td>
+                            <td>${nuevoPedido.nombre_producto}</td>
+                            <td>${nuevoPedido.stock_inicial}</td>
+                            <td><input type="number" class="form-control" value="1" min="1" id="cantidad_${index}"></td>
+                            <td>${nuevoPedido.pvp}</td>
+                            <td>
+                            <button class="btn btn-sm btn-success" onclick="enviar_cliente(${nuevoPedido.id_producto}, ${index})"><i class="fa-solid fa-pencil"></i></button>
+                            </td>
+                        </tr>`;
+        });
+        document.getElementById("tableBody_nuevosPedidos").innerHTML = content;
+      } else {
+        console.error("La respuesta no es un array:", nuevosPedidos);
+        alert("Error: La respuesta no tiene el formato esperado.");
+      }
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      alert(errorThrown);
+    },
+  });
+};
+
+// Abrir modal
+function buscar_productos_nuevoPedido() {
+  $("#nuevosPedidosModal").modal("show");
+}
+
+//enviar cliente
+//enviar cliente
+function enviar_cliente(id, index) {
+    // Obtener el valor del input cantidad correspondiente
+    let cantidad = $(`#cantidad_${index}`).val();
+
     $.ajax({
-      url: SERVERURL + "pedidos/buscarProductosBodega/" + id_producto,
       type: "POST",
-      data: formData,
-      processData: false,
-      contentType: false,
+      url: SERVERURL + "marketplace/obtener_producto/" + id,
+      dataType: "json",
       success: function (response) {
-        let nuevosPedidos = response;
-        if (typeof response === "string") {
-          nuevosPedidos = JSON.parse(response);
-        }
+        if (response) {
+          const data = response[0];
   
-        if (Array.isArray(nuevosPedidos)) {
-          let content = ``;
-          nuevosPedidos.forEach((nuevoPedido, index) => {
-            content += `
-                          <tr>
-                              <td>${nuevoPedido.image_path}</td>
-                              <td>${nuevoPedido.id_producto}</td>
-                              <td>${nuevoPedido.nombre_producto}</td>
-                              <td>${nuevoPedido.stock_inicial}</td>
-                              <td><input type="number" class="form-control" value="1" min="1" id="cantidad_${index}"></td>
-                              <td>${nuevoPedido.pvp}</td>
-                              <td>
-                              <button class="btn btn-sm btn-success" onclick="enviar_cliente(${nuevoPedido.id_producto}, ${index})"><i class="fa-solid fa-pencil"></i></button>
-                              </td>
-                          </tr>`;
+          // Crear un objeto FormData y agregar los datos
+          const formData = new FormData();
+          formData.append("cantidad", cantidad);  // Utilizar la cantidad obtenida
+          formData.append("precio", data.pvp);
+          formData.append("id_producto", data.id_producto);
+          formData.append("sku", data.sku);
+  
+          $.ajax({
+            type: "POST",
+            url: SERVERURL + "marketplace/agregarTmp",
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function (response2) {
+              response2 = JSON.parse(response2);
+              console.log(response2);
+              console.log(response2[0]);
+              if (response2.status == 500) {
+                Swal.fire({
+                  icon: "error",
+                  title: response2.title,
+                  text: response2.message,
+                });
+              } else if (response2.status == 200) {
+                initDataTableNuevoPedido();
+              }
+            },
+            error: function (xhr, status, error) {
+              console.error("Error en la solicitud AJAX:", error);
+              alert("Hubo un problema al agregar el producto temporalmente");
+            },
           });
-          document.getElementById("tableBody_nuevosPedidos").innerHTML = content;
         } else {
-          console.error("La respuesta no es un array:", nuevosPedidos);
-          alert("Error: La respuesta no tiene el formato esperado.");
+          console.error("La respuesta está vacía o tiene un formato incorrecto.");
         }
       },
-      error: function (jqXHR, textStatus, errorThrown) {
-        alert(errorThrown);
+      error: function (xhr, status, error) {
+        console.error("Error en la solicitud AJAX:", error);
+        alert("Hubo un problema al obtener la información del producto");
       },
     });
-  };
-  
-  //enviar cliente
-  function enviar_cliente(id, index) {
-      let cantidad = $(`#cantidad_${index}`).val();
-  
-      $.ajax({
-        type: "POST",
-        url: SERVERURL + "marketplace/obtener_producto/" + id,
-        dataType: "json",
-        success: function (response) {
-          if (response) {
-            const data = response[0];
-  
-            const formData = new FormData();
-            formData.append("cantidad", cantidad);
-            formData.append("precio", data.pvp);
-            formData.append("id_producto", data.id_producto);
-            formData.append("sku", data.sku);
-  
-            $.ajax({
-              type: "POST",
-              url: SERVERURL + "marketplace/agregarTmp",
-              data: formData,
-              processData: false,
-              contentType: false,
-              success: function (response2) {
-                response2 = JSON.parse(response2);
-                console.log(response2);
-                console.log(response2[0]);
-                if (response2.status == 500) {
-                  Swal.fire({
-                    icon: "error",
-                    title: response2.title,
-                    text: response2.message,
-                  });
-                } else if (response2.status == 200) {
-                  initDataTableNuevoPedido();
-                }
-              },
-              error: function (xhr, status, error) {
-                console.error("Error en la solicitud AJAX:", error);
-                alert("Hubo un problema al agregar el producto temporalmente");
-              },
-            });
-          } else {
-            console.error("La respuesta está vacía o tiene un formato incorrecto.");
-          }
-        },
-        error: function (xhr, status, error) {
-          console.error("Error en la solicitud AJAX:", error);
-          alert("Hubo un problema al obtener la información del producto");
-        },
-      });
-  }
-  
-  window.addEventListener("load", function () {
-    buscar_productos_nuevoPedido();
-  });
-  
+}
+
+
+window.addEventListener("load", async () => {
+  await initDataTableNuevosPedidos();
+});
