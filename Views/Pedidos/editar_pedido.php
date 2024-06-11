8533a1dc83c84de28f697b429ebb20ae791e6a4d
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-11 11:58:16 -0500

    agregar_index

---
+++ Views/Pedidos/editar_pedido.php
@@ -0,0 +1,252 @@
+<?php require_once './Views/templates/header.php'; ?>
+<?php // Recuperar los parámetros de la URL
+$id_producto = isset($_GET['id_producto']) ? $_GET['id_producto'] : null;
+$sku = isset($_GET['sku']) ? $_GET['sku'] : null;
+?>
+<style>
+    .section-title {
+        background-color: #f8f9fa;
+        padding: 10px;
+        border-radius: 5px;
+        text-align: center;
+        margin-bottom: 20px;
+    }
+
+    .form-section {
+        background-color: #fff;
+        padding: 20px;
+        border-radius: 5px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        margin-bottom: 20px;
+    }
+
+    .btn-custom {
+        width: 100%;
+    }
+
+    .left-column,
+    .right-column {
+        padding: 20px;
+    }
+
+    .img-container img {
+        max-width: 100px;
+    }
+
+    .transportadora {
+        transition: transform 0.3s ease, box-shadow 0.3s ease;
+        border-radius: 10px;
+        overflow: hidden;
+        padding: 10px;
+        background-color: #f8f9fa;
+        display: inline-block;
+    }
+
+    .transportadora img {
+        max-width: 100px;
+        transition: filter 0.3s ease;
+        filter: grayscale(100%);
+    }
+
+    .transportadora:hover {
+        transform: scale(1.1);
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    }
+
+    .transportadora:hover img {
+        filter: grayscale(0%);
+    }
+
+    /* tabla */
+    .table-striped tbody tr:nth-of-type(odd) {
+        background-color: rgba(0, 0, 0, .05);
+    }
+
+    .table-hover tbody tr:hover {
+        background-color: rgba(0, 0, 0, .075);
+    }
+
+    .table thead th {
+        background-color: #171931;
+        color: white;
+    }
+
+    .table th,
+    .table td {
+        text-align: center;
+        vertical-align: middle;
+    }
+
+    .centered {
+        text-align: center !important;
+        vertical-align: middle !important;
+    }
+</style>
+<?php require_once './Views/Pedidos/Modales/agregar_productos_pedido.php'; ?>
+<div class="custom-container-fluid mt-4">
+    <div class="row">
+        <div class="col">
+            <h2 class="section-title">Generar Guías</h2>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6 left-column d-flex flex-column">
+            <div class="form-section">
+                <form>
+                    <div class="row g-3 align-items-center mb-3">
+                        <div class="col-auto">
+                            <label for="cantidad" class="col-form-label">Cant:</label>
+                        </div>
+                        <div class="col-auto">
+                            <input type="number" id="cantidad" class="form-control" value="1" min="1">
+                        </div>
+                        <div class="col-auto">
+                            <label for="codigo" class="col-form-label">Código:</label>
+                        </div>
+                        <div class="col-auto">
+                            <input type="text" id="codigo" class="form-control">
+                        </div>
+                        <div class="col-auto">
+                            <button type="button" class="btn btn-primary" onclick="buscar_productos_nuevoPedido()">Buscar</button>
+                        </div>
+
+                    </div>
+                </form>
+            </div>
+            <div class="table-responsive">
+                <!-- <table class="table table-bordered table-striped table-hover"> -->
+                <table id="datatable_nuevoPedido" class="table table-striped">
+                    <!-- <caption>
+                    DataTable.js Demo
+                </caption> -->
+                    <thead>
+                        <tr>
+                            <th class="centered">Cod</th>
+                            <th class="centered">Cant.</th>
+                            <th class="centered">Descripción</th>
+                            <th class="centered">Precio</th>
+                            <th class="centered">Desc %</th>
+                            <th class="centered">Total</th>
+                            <th class="centered">Acción</th>
+                        </tr>
+                    </thead>
+                    <tbody id="tableBody_nuevoPedido"></tbody>
+                </table>
+                <div class="text-end">
+                    <span>Total: $<span id="monto_total" class="monto">
+
+                        </span> </span>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6 right-column">
+            <div class="form-section mb-4">
+                <h5>Datos Destinatario</h5>
+                <form id="datos_destinatario">
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <label for="nombre" class="form-label">Nombre y Apellido</label>
+                            <input type="text" class="form-control" id="nombre" placeholder="Nombre y Apellido">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="telefono" class="form-label">Teléfono</label>
+                            <input type="text" class="form-control" id="telefono" placeholder="Teléfono">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <label for="provincia" class="form-label">Provincia</label>
+                            <select id="provincia" class="form-select">
+                                <option selected>Selecciona una opción</option>
+                                <!-- Agregar opciones aquí -->
+                            </select>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="ciudad" class="form-label">Ciudad</label>
+                            <select id="ciudad" class="form-select">
+                                <option selected>Selecciona una opción</option>
+                                <!-- Agregar opciones aquí -->
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <label for="calle_principal" class="form-label">Calle Principal</label>
+                            <input type="text" class="form-control" id="calle_principal" placeholder="Calle Principal">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="calle_secundaria" class="form-label">Calle Secundaria</label>
+                            <input type="text" class="form-control" id="calle_secundaria" placeholder="Calle Secundaria">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <label for="numero_casa" class="form-label">Número de Casa</label>
+                            <input type="text" class="form-control" id="numero_casa" placeholder="Número de Casa">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="referencia" class="form-label">Referencia</label>
+                            <input type="text" class="form-control" id="referencia" placeholder="Referencia">
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="observaciones" class="form-label">Observaciones para la entrega</label>
+                        <input type="text" class="form-control" id="observacion" placeholder="Referencias Adicionales (Opcional)">
+                    </div>
+                </form>
+            </div>
+
+            <div class="form-section">
+                <h5>Generar Guías</h5>
+                <div class="d-flex justify-content-around mb-4">
+                    <!-- Agregar imágenes o iconos según sea necesario -->
+                    <div class="img-container text-center transportadora">
+                        <img src="<?php echo SERVERURL;?>/public/img/SERVIENTREGA.jpg" alt="Servientrega">
+                    </div>
+                    <div class="img-container text-center transportadora">
+                        <img src="<?php echo SERVERURL;?>/public/img/LAAR.jpg" alt="Laborcourier">
+                    </div>
+                    <div class="img-container text-center transportadora">
+                        <img src="<?php echo SERVERURL;?>/public/img/SPEED.png" alt="Speed">
+                    </div>
+                    <div class="img-container text-center transportadora">
+                        <img src="<?php echo SERVERURL;?>/public/img/GINTRACOM.jpg" alt="Gintracom">
+                    </div>
+                </div>
+                <form>
+                    <div class="mb-3">
+                        <label for="recaudo" class="form-label">Recaudo</label>
+                        <select id="recaudo" class="form-select">
+                            <option value="1">Con Recaudo</option>
+                            <option value="0">Sin Recaudo</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <!-- <input class="form-check-input" type="checkbox" id="asegurar_mercaderia">
+                        <label class="form-check-label" for="asegurar_mercaderia">
+                            Deseo asegurar la mercadería
+                        </label> -->
+                        <label class="form-check-label" for="extras">extra</label>
+                        <select class="form-control">
+                            <option>Selecciona un Extras</option>
+                            <option value="1">Autogestion</option>
+                            <option value="2">Call center</option>
+                        </select>
+                    </div>
+                </form>
+                <div class="d-flex justify-content-between">
+                    <button type="button" id="guardarPedidoBtn" class="btn btn-success btn-custom" onclick="agregar_nuevoPedido()">Guardar Pedido</button>
+                    <button type="button" id="generarGuiaBtn" class="btn btn-danger btn-custom">Generar Guía</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="<?php echo SERVERURL ?>/Views/Pedidos/js/editar_pedido.js"></script>
+<script src="<?php echo SERVERURL ?>/Views/Pedidos/js/agregar_productos_pedido.js"></script>
+<?php require_once './Views/templates/footer.php'; ?>
---
+++ Views/Pedidos/js/editar_pedido.js
@@ -0,0 +1,411 @@
+let dataTableNuevoPedido;
+let dataTableNuevoPedidoIsInitialized = false;
+let eliminado = false;
+
+const dataTableNuevoPedidoOptions = {
+  paging: false,
+  searching: false,
+  info: false,
+  lengthChange: false,
+  destroy: true,
+  autoWidth: false,
+  columnDefs: [{ className: "centered", targets: [0, 1, 2, 3, 4, 5, 6] }],
+  /* dom: '<"flex justify-between items-center mb-4"lBf<"text-center mt-4">r>t<"flex justify-between items-center"ip>', */
+  language: {
+    lengthMenu: "Mostrar _MENU_ registros por página",
+    zeroRecords: "Ningún usuario encontrado",
+    info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+    infoEmpty: "Ningún usuario encontrado",
+    infoFiltered: "(filtrados desde _MAX_ registros totales)",
+    search: "Buscar:",
+    loadingRecords: "Cargando...",
+    paginate: {
+      first: "Primero",
+      last: "Último",
+      next: "Siguiente",
+      previous: "Anterior",
+    },
+  },
+};
+
+const initDataTableNuevoPedido = async () => {
+  if (dataTableNuevoPedidoIsInitialized) {
+    dataTableNuevoPedido.destroy();
+  }
+
+  await listNuevoPedido();
+
+  dataTableNuevoPedido = $("#datatable_nuevoPedido").DataTable(
+    dataTableNuevoPedidoOptions
+  );
+
+  dataTableNuevoPedidoIsInitialized = true;
+};
+
+var celular_bodega = "";
+var nombre_bodega = "";
+var ciudad_bodega = "";
+var provincia_bodega = "";
+var direccion_bodega = "";
+var referencia_bodega = "";
+var numeroCasa_bodega = "";
+var id_propietario_bodega = "";
+var id_producto_venta = "";
+var dropshipping = "";
+var contiene = "";
+var costo_producto = 0;
+
+const listNuevoPedido = async () => {
+  try {
+    const response = await fetch("" + SERVERURL + "pedidos/buscarTmp");
+
+    const data = await response.json();
+    console.log(data);
+
+    if (data.tmp[0].id_producto == 0 && eliminado == false) {
+      // If the response is empty, return
+      return;
+    }
+    const nuevosPedidos = data.tmp; // Extract the 'tmp' array from the response
+    const nuevosPedidos_bodega = data.bodega;
+    console.log(nuevosPedidos_bodega);
+    let content = ``;
+    let total = 0;
+    let precio_costo = 0;
+    nuevosPedidos.forEach((nuevoPedido, index) => {
+      if (nuevosPedidos_bodega.length > 0 && nuevosPedidos_bodega[0]) {
+        celular_bodega = nuevosPedidos_bodega[0].contacto;
+        nombre_bodega = nuevosPedidos_bodega[0].nombre;
+        ciudad_bodega = nuevosPedidos_bodega[0].localidad;
+        provincia_bodega = nuevosPedidos_bodega[0].provincia;
+        direccion_bodega = nuevosPedidos_bodega[0].direccion;
+        referencia_bodega = nuevosPedidos_bodega[0].referencia;
+        numeroCasa_bodega = nuevosPedidos_bodega[0].num_casa;
+        id_propietario_bodega = nuevosPedidos_bodega[0].id;
+      }
+      id_producto_venta = nuevoPedido.id_producto;
+      dropshipping = nuevoPedido.drogshipin;
+      costo_producto = nuevoPedido.costo_producto;
+
+      contiene += `${nuevoPedido.nombre_producto} X${nuevoPedido.cantidad_tmp} `;
+
+      precio_costo = parseFloat(nuevoPedido.precio_tmp);
+
+      // Verificar condición
+      if (!validar_direccion()) {
+        return; // Salir de la función si la validación falla
+      }
+
+      const precio = parseFloat(nuevoPedido.precio_tmp);
+      const descuento = parseFloat(nuevoPedido.desc_tmp);
+      const precioFinal = precio - precio * (descuento / 100);
+      total += precioFinal;
+      content += `
+                <tr>
+                    <td>${nuevoPedido.id_tmp}</td>
+                    <td>${nuevoPedido.cantidad_tmp}</td>
+                    <td>${nuevoPedido.nombre_producto}</td>
+                    <td><input type="text" onblur='recalcular("${
+                      nuevoPedido.id_tmp
+                    }", "precio_nuevoPedido_${index}", "descuento_nuevoPedido_${index}")' id="precio_nuevoPedido_${index}" class="form-control prec" value="${precio}"></td>
+                    <td><input type="text" onblur='recalcular("${
+                      nuevoPedido.id_tmp
+                    }", "precio_nuevoPedido_${index}", "descuento_nuevoPedido_${index}")' id="descuento_nuevoPedido_${index}" class="form-control desc" value="${descuento}"></td>
+                    <td><span class='tota' id="precioFinal_nuevoPedido_${index}">${precioFinal.toFixed(
+        2
+      )}</span></td>
+                    <td>
+                        <button class="btn btn-sm btn-danger" onclick="eliminar_nuevoPedido(${
+                          nuevoPedido.id_tmp
+                        })"><i class="fa-solid fa-trash-can"></i></button>
+                    </td>
+                </tr>`;
+    });
+    document.getElementById("monto_total").innerHTML = total.toFixed(2);
+    document.getElementById("tableBody_nuevoPedido").innerHTML = content;
+    if (eliminado == true) {
+      eliminado = false;
+      document.getElementById("monto_total").innerHTML = 0;
+      document.getElementById("tableBody_nuevoPedido").innerHTML = "";
+    }
+  } catch (ex) {
+    alert(ex);
+  }
+};
+
+function recalcular(id, idPrecio, idDescuento) {
+  costo_producto = 0;
+  const precio = parseFloat(document.getElementById(idPrecio).value);
+  const descuento = parseFloat(document.getElementById(idDescuento).value);
+
+  const ffrm = new FormData();
+  ffrm.append("id", id);
+  ffrm.append("precio", precio);
+  ffrm.append("descuento", descuento);
+
+  fetch("" + SERVERURL + "pedidos/actualizarTmp/" + id, {
+    method: "POST",
+    body: ffrm,
+  })
+    .then((response) => response.json())
+    .then(async (data) => {
+      if (data.status == 200) {
+        toastr.success("PRODUCTO ACTUALIZADO CORRECTAMENTE", "NOTIFICACIÓN", {
+          positionClass: "toast-bottom-center",
+        });
+      } else {
+        toastr.error(
+          "EL PRODUCTO NO SE ACTUALIZADO CORRECTAMENTE",
+          "NOTIFICACIÓN",
+          {
+            positionClass: "toast-bottom-center",
+          }
+        );
+      }
+      await initDataTableNuevoPedido();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("Hubo un problema al actualizar el producto");
+    });
+}
+
+function validar_direccion() {
+  // Obtener los parámetros de la URL
+  const urlParams = new URLSearchParams(window.location.search);
+  const idProducto = urlParams.get("id_producto");
+  const sku = urlParams.get("sku");
+
+  // Solo realizar la validación si los parámetros están presentes
+  if (idProducto && sku) {
+    if (
+      ciudad_bodega == null ||
+      provincia_bodega == null ||
+      direccion_bodega == null
+    ) {
+      // Bloquear los botones
+      const guardarPedidoBtn = document.getElementById("guardarPedidoBtn");
+      const generarGuiaBtn = document.getElementById("generarGuiaBtn");
+
+      guardarPedidoBtn.disabled = true;
+      generarGuiaBtn.disabled = true;
+
+      // Crear el tooltip
+      toastr.error(
+        "Esta bodega no contiene datos de dirección y no puede generar guias",
+        "NOTIFICACIÓN",
+        {
+          positionClass: "toast-bottom-center",
+        }
+      );
+
+      return false; // Retorna falso para indicar que la validación falló
+    }
+  }
+  return true; // Retorna verdadero si la validación pasa o los parámetros no están presentes
+}
+
+function eliminar_nuevoPedido(id) {
+  eliminado = true;
+  $.ajax({
+    type: "POST",
+    url: SERVERURL + "pedidos/eliminarTmp/" + id,
+    success: function (response) {
+      // Mostrar alerta de éxito
+      if (response.status == 500) {
+        toastr.error(
+          "EL PRODUCTO NO SE ELIMINADO CORRECTAMENTE",
+          "NOTIFICACIÓN",
+          { positionClass: "toast-bottom-center" }
+        );
+      } else if (response.status == 200) {
+        toastr.success("PRODUCTO ELIMINADO CORRECTAMENTE", "NOTIFICACIÓN", {
+          positionClass: "toast-bottom-center",
+        });
+      }
+
+      // Recargar la DataTable
+      initDataTableNuevoPedido();
+    },
+    error: function (xhr, status, error) {
+      console.error("Error en la solicitud AJAX:", error);
+      alert("Hubo un problema al eliminar la categoría");
+    },
+  });
+}
+
+window.addEventListener("load", async () => {
+  await initDataTableNuevoPedido();
+});
+
+//cargar selelct ciudades y provincias
+$(document).ready(function () {
+  // Inicializar Select2 en los selects
+  $("#provincia").select2({
+    placeholder: "Selecciona una opción",
+    allowClear: true,
+  });
+
+  $("#ciudad").select2({
+    placeholder: "Selecciona una opción",
+    allowClear: true,
+  });
+
+  cargarProvincias(); // Llamar a cargarProvincias cuando la página esté lista
+
+  // Llamar a cargarCiudades cuando se seleccione una provincia
+  $("#provincia").on("change", cargarCiudades);
+});
+
+// Función para cargar provincias
+function cargarProvincias() {
+  $.ajax({
+    url: "" + SERVERURL + "Ubicaciones/obtenerProvincias", // Reemplaza con la ruta correcta a tu controlador
+    method: "GET",
+    success: function (response) {
+      let provincias = JSON.parse(response);
+      let provinciaSelect = $("#provincia");
+      provinciaSelect.empty();
+      provinciaSelect.append('<option value="">Provincia *</option>'); // Añadir opción por defecto
+
+      provincias.forEach(function (provincia) {
+        provinciaSelect.append(
+          `<option value="${provincia.codigo_provincia}">${provincia.provincia}</option>`
+        );
+      });
+
+      // Refrescar Select2 para que muestre las nuevas opciones
+      provinciaSelect.trigger("change.select2");
+    },
+    error: function (error) {
+      console.log("Error al cargar provincias:", error);
+    },
+  });
+}
+
+// Función para cargar ciudades según la provincia seleccionada
+function cargarCiudades() {
+  let provinciaId = $("#provincia").val();
+  if (provinciaId) {
+    $.ajax({
+      url: SERVERURL + "Ubicaciones/obtenerCiudades/" + provinciaId, // Reemplaza con la ruta correcta a tu controlador
+      method: "GET",
+      success: function (response) {
+        let ciudades = JSON.parse(response);
+        console.log("Ciudades recibidas:", ciudades); // Verificar los datos en la consola del navegador
+        let ciudadSelect = $("#ciudad");
+        ciudadSelect.empty();
+        ciudadSelect.append('<option value="">Ciudad *</option>'); // Añadir opción por defecto
+
+        ciudades.forEach(function (ciudad) {
+          ciudadSelect.append(
+            `<option value="${ciudad.id_cotizacion}">${ciudad.ciudad}</option>`
+          );
+        });
+
+        // Refrescar Select2 para que muestre las nuevas opciones
+        ciudadSelect.trigger("change.select2");
+
+        ciudadSelect.prop("disabled", false); // Habilitar el select de ciudades
+      },
+      error: function (error) {
+        console.log("Error al cargar ciudades:", error);
+      },
+    });
+  } else {
+    $("#ciudad")
+      .empty()
+      .append('<option value="">Ciudad *</option>')
+      .prop("disabled", true)
+      .trigger("change.select2"); // Refrescar Select2 para mostrar el estado deshabilitado
+  }
+}
+
+//agregar funcion pedido
+function agregar_nuevoPedido() {
+  // Evita que el formulario se envíe de la forma tradicional
+  event.preventDefault();
+
+  // Crea un objeto FormData
+  var formData = new FormData();
+  var montoTotal = document.getElementById("monto_total").innerText;
+  formData.append("total_venta", montoTotal);
+  formData.append("nombre", $("#nombre").val());
+  formData.append("telefono", $("#telefono").val());
+  formData.append("calle_principal", $("#calle_principal").val());
+  formData.append("calle_secundaria", $("#calle_secundaria").val());
+  formData.append("referencia", $("#referencia").val());
+  formData.append("ciudad", $("#ciudad").val());
+  formData.append("provincia", $("#provincia").val());
+  formData.append("identificacion", 0);
+  formData.append("observacion", $("#observacion").val());
+  formData.append("transporte", 0);
+  formData.append("celular", $("#telefono").val()); // Asegúrate de obtener el valor correcto
+  formData.append("id_producto_venta", id_producto_venta);
+  formData.append("dropshipping", dropshipping);
+  formData.append("importado", 0);
+  formData.append("id_propietario", id_propietario_bodega);
+  formData.append("identificacionO", 0);
+  formData.append("celularO", celular_bodega);
+  formData.append("nombreO", nombre_bodega); // Corregir nombre de variable
+  formData.append("ciudadO", ciudad_bodega);
+  formData.append("provinciaO", provincia_bodega);
+  formData.append("direccionO", direccion_bodega);
+  formData.append("referenciaO", referencia_bodega); // Corregir nombre de variable
+  formData.append("numeroCasaO", numeroCasa_bodega);
+  formData.append("valor_seguro", 0); // Corregir nombre de variable
+  formData.append("no_piezas", 1);
+  formData.append("contiene", contiene);
+  formData.append("costo_flete", 0);
+  formData.append("costo_producto", costo_producto);
+  formData.append("comentario", "Enviado por x");
+  formData.append("id_transporte", 0);
+
+  // Realiza la solicitud AJAX
+  $.ajax({
+    url: "" + SERVERURL + "/pedidos/nuevo_pedido",
+    type: "POST",
+    data: formData,
+    processData: false,
+    contentType: false,
+    success: function (response) {
+      response = JSON.parse(response);
+      if (response.status == 500) {
+        Swal.fire({
+          icon: "error",
+          title: response.title,
+          text: response.message,
+        });
+      } else if (response.status == 200) {
+        Swal.fire({
+          icon: "success",
+          title: response.title,
+          text: response.message,
+          showConfirmButton: false,
+          timer: 2000,
+        }).then(() => {
+          vaciarTmpPedidos();
+          window.location.href = "" + SERVERURL + "Pedidos";
+        });
+      }
+    },
+    error: function (error) {
+      alert("Hubo un error al agregar el producto");
+      console.log(error);
+    },
+  });
+}
+
+// Función para vaciar temporalmente los pedidos
+const vaciarTmpPedidos = async () => {
+  try {
+    const response = await fetch("" + SERVERURL + "marketplace/vaciarTmp");
+    if (!response.ok) {
+      throw new Error("Error al vaciar los pedidos temporales");
+    }
+    const data = await response.json();
+    console.log("Respuesta de vaciarTmp:", data);
+  } catch (error) {
+    console.error("Error al hacer la solicitud:", error);
+  }
+};
