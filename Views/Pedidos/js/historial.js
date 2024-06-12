43cd395b47d8b40945a67dd9777d8e96286c81c2
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-12 12:26:46 -0500

    agregar_index

--- Views/Pedidos/js/historial.js
+++ Views/Pedidos/js/historial.js
@@ -80,7 +80,7 @@ const listHistorialPedidos = async () => {
                     </td>
                     <td>${historialPedido.impreso}</td>
                     <td>
-                        <button class="btn btn-sm btn-primary" onclick="boton_editarPedido(${historialPedido.numero_factura})"><i class="fa-solid fa-pencil"></i></button>
+                        <button class="btn btn-sm btn-primary" onclick="boton_editarPedido(${historialPedido.id_factura})"><i class="fa-solid fa-pencil"></i></button>
                         <button class="btn btn-sm btn-danger"><i class="fa-solid fa-trash-can"></i></button>
                     </td>
                 </tr>`;
