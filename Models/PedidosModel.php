8513933804f9123bef70976eb06d2abd61176e02
Jeimy Jara <jeimyjara@hotmail.com>
2024-06-12 16:43:02 -0500

    XD

--- Models/PedidosModel.php
+++ Models/PedidosModel.php
@@ -257,7 +257,7 @@ class PedidosModel extends Query
 
     public function pedidos()
     {
-        $sql = "SELECT *, (SELECT ciudad FROM ciudad_cotizacion where id_cotizacion = ciudad_cot) as ciudad,(SELECT provincia FROM ciudad_cotizacion where id_cotizacion = ciudad_cot) as provinciaa. (SELECT url_imporsuit from plataformas where id_plataforma = id_propietario) as plataforma FROM facturas_cot WHERE anulada = 0 AND (TRIM(numero_guia) = '' OR numero_guia IS NULL OR numero_guia = '0') ORDER BY numero_factura DESC;";
+        $sql = "SELECT *, (SELECT ciudad FROM ciudad_cotizacion where id_cotizacion = ciudad_cot) as ciudad,(SELECT provincia FROM ciudad_cotizacion where id_cotizacion = ciudad_cot) as provinciaa,(SELECT url_imporsuit from plataformas where id_plataforma = id_propietario) as plataforma FROM facturas_cot WHERE anulada = 0 AND (TRIM(numero_guia) = '' OR numero_guia IS NULL OR numero_guia = '0') ORDER BY numero_factura DESC;";
         return $this->select($sql);
     }
 
