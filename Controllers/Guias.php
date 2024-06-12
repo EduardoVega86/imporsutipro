f6416318a364dfae771b5831e5e7e185006685aa
Jeimy Jara <jeimyjara@hotmail.com>
2024-06-11 19:13:30 -0500

    productos

--- Controllers/Guias.php
+++ Controllers/Guias.php
@@ -52,7 +52,7 @@ class Guias extends Controller
         $numero_factura = $_POST['numero_factura'];
 
         $datos = $this->model->generarLaar($nombreOrigen, $ciudadOrigen, $direccionOrigen, $telefonoOrigen, $referenciaOrigen, $celularOrigen, $nombreDestino, $ciudadDestino, $direccionDestino, $telefonoDestino, $celularDestino, $referenciaDestino, $postal, $identificacion, $contiene, $peso, $valor_seguro, $valor_declarado, $tamanio, $cod, $costoflete, $costo_producto, $tipo_cobro, $comentario, $fecha, $extras);
-
+        $datos = json_decode($datos, true);
         if (empty($datos["guia"])) {
             $this->model->actualizarGuia($numero_factura, $datos["guia"]);
         }
