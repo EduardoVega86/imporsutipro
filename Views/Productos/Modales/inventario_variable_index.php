9d84243830cd68d009d07e8fec57679b5a03d073
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-14 10:39:56 -0500

    agregar_index

--- Views/Productos/Modales/inventario_variable_index.php
+++ Views/Productos/Modales/inventario_variable_index.php
@@ -22,37 +22,37 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <div class="tab-pane fade" id="inventario-variable" role="tabpanel" aria-labelledby="inventario-variable-tab">
-                    <div class="d-flex">
-                        <input type="hidden" id="id_productoVariable" name="id_productoVariable" value="0">
-                        <table id="datatable_inventarioVariable" class="table table-striped w-50">
-                            <thead>
-                                <tr>
-                                    <th class="centered">Atributo</th>
-                                    <th class="centered">Valor</th>
-                                    <th class="centered"></th>
-                                </tr>
-                            </thead>
-                            <tbody id="tableBody_inventarioVariable"></tbody>
-                        </table>
 
-                        <table id="datatable_detalleInventario" class="table table-bordered table-striped table-hover w-50">
-                            <thead>
-                                <tr>
-                                    <th class="text-nowrap">Atribuo</th>
-                                    <th class="text-nowrap">SKU</th>
-                                    <th class="text-nowrap">P. Proveedor</th>
-                                    <th class="text-nowrap">P. de Venta</th>
-                                    <th class="text-nowrap">P. Referencial</th>
-                                    <th class="text-nowrap">Bodega</th>
-                                    <th class="text-nowrap">Stock inicial</th>
-                                    <th class="text-nowrap">ID variable</th>
-                                </tr>
-                            </thead>
-                            <tbody id="tableBody_detalleInventario"></tbody>
-                        </table>
-                    </div>
+                <div class="d-flex">
+                    <input type="hidden" id="id_productoVariable" name="id_productoVariable" value="0">
+                    <table id="datatable_inventarioVariable" class="table table-striped w-50">
+                        <thead>
+                            <tr>
+                                <th class="centered">Atributo</th>
+                                <th class="centered">Valor</th>
+                                <th class="centered"></th>
+                            </tr>
+                        </thead>
+                        <tbody id="tableBody_inventarioVariable"></tbody>
+                    </table>
+
+                    <table id="datatable_detalleInventario" class="table table-bordered table-striped table-hover w-50">
+                        <thead>
+                            <tr>
+                                <th class="text-nowrap">Atribuo</th>
+                                <th class="text-nowrap">SKU</th>
+                                <th class="text-nowrap">P. Proveedor</th>
+                                <th class="text-nowrap">P. de Venta</th>
+                                <th class="text-nowrap">P. Referencial</th>
+                                <th class="text-nowrap">Bodega</th>
+                                <th class="text-nowrap">Stock inicial</th>
+                                <th class="text-nowrap">ID variable</th>
+                            </tr>
+                        </thead>
+                        <tbody id="tableBody_detalleInventario"></tbody>
+                    </table>
                 </div>
+
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
