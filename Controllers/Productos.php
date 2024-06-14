c68cc1332fee8b1fc898b22b7703b6dad42c0f56
Jeimy Jara <jeimyjara@hotmail.com>
2024-06-14 08:49:02 -0500

    productos

--- Controllers/Productos.php
+++ Controllers/Productos.php
@@ -191,7 +191,7 @@ class Productos extends Controller
     public function guardar_imagen_productos()
     {
 
-        $response = $this->model->guardar_imagen_categorias($_FILES['imagen'], $_POST['id_producto'], $_SESSION['id_plataforma']);
+        $response = $this->model->guardar_imagen_productos($_FILES['imagen'], $_POST['id_producto'], $_SESSION['id_plataforma']);
         echo json_encode($response);
     }
 
