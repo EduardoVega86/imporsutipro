52f3bd6fa43f5ee681cf32f6e2681ef637037e60
Jeimy Jara <jeimyjara@hotmail.com>
2024-06-14 11:32:08 -0500

    productos

---
+++ Controllers/Manifiestos.php
@@ -0,0 +1,22 @@
+
+<?php
+class Manifiestos extends Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        if (!$this->isAuth())
+            header("Location:  " . SERVERURL . "login");
+        /* if (!$this->hasPermission(2)) {
+            header("Location: /dashboard");
+        } */
+    }
+
+    public function generar()
+    {
+        $arreglo = $_POST['facturas'];
+        $arreglo = json_decode($arreglo, true);
+        $this->model->generarManifiesto($arreglo);
+    }
+}
---
+++ Models/ManifiestosModel.php
@@ -0,0 +1,14 @@
+<?php
+
+class ManifiestosModel extends Query
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function generarManifiesto($arreglo)
+    {
+        print_r($arreglo);
+    }
+}
