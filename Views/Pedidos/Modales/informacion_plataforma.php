5320664d16aa6932e0581d597b270b7afc7884f7
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-15 22:13:08 -0500

    agregar_index

--- Views/Pedidos/Modales/informacion_plataforma.php
+++ Views/Pedidos/Modales/informacion_plataforma.php
@@ -39,7 +39,7 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="infoTiendaModalLabel"><i class="fas fa-edit"></i> Nueva Categoría</h5>
+                <h5 class="modal-title" id="infoTiendaModalLabel"><i class="fas fa-edit"></i> Información de la Tienda</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="card">
