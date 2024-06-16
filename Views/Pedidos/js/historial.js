5005faddb4f5d5a7350021937613b328573415ca
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-15 22:02:53 -0500

    agregar_index

--- Views/Pedidos/js/historial.js
+++ Views/Pedidos/js/historial.js
@@ -97,7 +97,7 @@ const listHistorialPedidos = async () => {
                         <div>telf: ${historialPedido.telefono}</div>
                     </td>
                     <td>${historialPedido.provinciaa}-${ciudad}</td>
-                    <td><a href="#" id="plataformaLink" onclick="abrirModal_infoTienda("${historialPedido.plataforma}")">${plataforma}</a></td>
+                    <td><a id="plataformaLink" onclick="abrirModal_infoTienda("${historialPedido.plataforma}")">${plataforma}</a></td>
                     <td>${transporte_content}</td>
                     <td>
                         <a class="w-100" href="https://wa.me/${formatPhoneNumber(
