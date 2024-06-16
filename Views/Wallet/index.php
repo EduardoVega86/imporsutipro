afd9919d10dbb28b59537c637a3c7ba09e974538
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-16 16:17:06 -0500

    agregar_index

---
+++ Views/Wallet/index.php
@@ -0,0 +1,112 @@
+<?php require_once './Views/templates/header.php'; ?>
+
+<style>
+    .table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    .table th,
+    .table td {
+        text-align: center;
+        vertical-align: middle;
+        border: 1px solid #ddd;
+        /* Añadir borde a celdas */
+    }
+
+    .table-striped tbody tr:nth-of-type(odd) {
+        background-color: rgba(0, 0, 0, .05);
+    }
+
+    .table-hover tbody tr:hover {
+        background-color: rgba(0, 0, 0, .075);
+    }
+
+    .table thead th {
+        background-color: #171931;
+        color: white;
+    }
+
+    .centered {
+        text-align: center !important;
+        vertical-align: middle !important;
+    }
+
+    /* Diseños de estados guias */
+    .badge_danger {
+        background-color: red;
+        color: white;
+        padding: 4px;
+        border-radius: 0.3rem;
+    }
+
+    .badge_purple {
+        background-color: #804BD1;
+        color: white;
+        padding: 4px;
+        border-radius: 0.3rem;
+    }
+
+    .badge_warning {
+        background-color: #F2CC0E;
+        color: white;
+        padding: 4px;
+        border-radius: 0.3rem;
+    }
+
+    .badge_green {
+        background-color: #59D343;
+        color: white;
+        padding: 4px;
+        border-radius: 0.3rem;
+    }
+</style>
+
+<style>
+    .filtros_producos {
+        display: flex;
+        flex-direction: row;
+    }
+
+    @media (max-width: 768px) {
+        .filtros_producos {
+            flex-direction: column;
+        }
+    }
+
+    .link-like {
+            color: blue;
+            text-decoration: underline;
+            cursor: pointer;
+        }
+        .link-like:hover {
+            color: darkblue;
+        }
+</style>
+<?php require_once './Views/Pedidos/Modales/informacion_plataforma.php'; ?>
+<div class="custom-container-fluid">
+    <div class="container mt-5" style="max-width: 1600px;">
+        <h2 class="text-center mb-4">Dellate</h2>
+
+        <div class="table-responsive">
+            <!-- <table class="table table-bordered table-striped table-hover"> -->
+            <table id="datatable_detalleWallet" class="table table-striped">
+
+                <thead>
+                    <tr>
+                        <th class="centered">Tienda</th>
+                        <th class="centered">Total Venta</th>
+                        <th class="centered">Total Utilidad</th>
+                        <th class="centered">Saldo Pendiente</th>
+                        <th class="centered">Guías Pendientes</th>
+                        <th class="centered">Excel</th>
+                        <th class="centered">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody id="tableBody_detalleWallet"></tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<script src="<?php echo SERVERURL ?>/Views/Pedidos/js/guias.js"></script>
+<?php require_once './Views/templates/footer.php'; ?>
---
+++ Views/Wallet/js/detalle.js
@@ -0,0 +1,74 @@
+let dataTableDetalleWallet;
+let dataTableDetalleWalletIsInitialized = false;
+
+const dataTableDetalleWalletOptions = {
+  columnDefs: [
+    { className: "centered", targets: [1, 2, 3, 4, 5, 6] },
+    { orderable: false, targets: 0 }, //ocultar para columna 0 el ordenar columna
+  ],
+  pageLength: 10,
+  destroy: true,
+  language: {
+    lengthMenu: "Mostrar _MENU_ registros por página",
+    zeroRecords: "Ningún usuario encontrado",
+    info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+    infoEmpty: "Ningún usuario encontrado",
+    infoFiltered: "(filtrados desde _MAX_ registros totales)",
+    search: "Buscar:",
+    loadingRecords: "Cargando...",
+    paginate: {
+      first: "Primero",
+      last: "Último",
+      next: "Siguiente",
+      previous: "Anterior",
+    },
+  },
+};
+
+const initDataTableDetalleWallet = async () => {
+  if (dataTableDetalleWalletIsInitialized) {
+    dataTableDetalleWallet.destroy();
+  }
+
+  await listDetalleWallet();
+
+  dataTableDetalleWallet = $("#datatable_detalleWallet").DataTable(dataTableDetalleWalletOptions);
+
+  dataTableDetalleWalletIsInitialized = true;
+};
+
+const listDetalleWallet = async () => {
+  try {
+    const response = await fetch("" + SERVERURL + "wallet/obtenerDatos");
+    const detallesWallet = await response.json();
+
+    let content = ``;
+
+    detallesWallet.forEach((detalleWallet, index) => {
+        console.log(detalleWallet)
+
+      content += `
+                <tr>
+                    <td>${detalleWallet.numero_factura}</td>
+                    <td>${detalleWallet.fecha_factura}</td>
+                    <td>
+                    <div class="dropdown">
+                    <button class="btn btn-sm btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fa-solid fa-gear"></i>
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <li><span class="dropdown-item" style="cursor: pointer;" onclick="pagar('${detalleWallet.numero_guia}')">Anular</span></li>
+                    </ul>
+                    </div>
+                    </td>
+                </tr>`;
+    });
+    document.getElementById("tableBody_detalleWallet").innerHTML = content;
+  } catch (ex) {
+    alert(ex);
+  }
+};
+
+window.addEventListener("load", async () => {
+  await initDataTableDetalleWallet();
+});
