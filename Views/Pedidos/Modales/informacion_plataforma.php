d2c10121e95603ce751183ff3adcb4edd6352d1a
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-15 22:16:00 -0500

    agregar_index

--- Views/Pedidos/Modales/informacion_plataforma.php
+++ Views/Pedidos/Modales/informacion_plataforma.php
@@ -36,40 +36,40 @@
     }
 </style>
 <div class="modal fade" id="infoTiendaModal" tabindex="-1" aria-labelledby="infoTiendaModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="infoTiendaModalLabel"><i class="fas fa-edit"></i> Información de la Tienda</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <div class="card">
-                    <div class="card-header">
-                        Información de la tienda
-                    </div>
-                    <div class="card-body">
-                        <div class="form-group">
-                            <label for="nombreTienda">Nombre</label>
-                            <input type="text" class="form-control" id="nombreTienda" value="Edison Henry Echeverría Pozo" readonly>
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="infoTiendaModalLabel"><i class="fas fa-edit"></i> Información de la Tienda</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="card">
+                        <div class="card-header">
+                            Información de la tienda
                         </div>
-                        <div class="form-group">
-                            <label for="telefonoTienda">Teléfono</label>
-                            <input type="text" class="form-control" id="telefonoTienda" value="593998011578" readonly>
-                        </div>
-                        <div class="form-group">
-                            <label for="correoTienda">Correo</label>
-                            <input type="email" class="form-control" id="correoTienda" value="eheppersonal@gmail.com" readonly>
-                        </div>
-                        <div class="form-group">
-                            <label for="enlaceTienda">Enlace</label>
-                            <input type="url" class="form-control" id="enlaceTienda" value="https://edishop.imporsuit.com" readonly>
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="nombreTienda">Nombre</label>
+                                <input type="text" class="form-control" id="nombreTienda" value="Edison Henry Echeverría Pozo" readonly>
+                            </div>
+                            <div class="form-group">
+                                <label for="telefonoTienda">Teléfono</label>
+                                <input type="text" class="form-control" id="telefonoTienda" value="593998011578" readonly>
+                            </div>
+                            <div class="form-group">
+                                <label for="correoTienda">Correo</label>
+                                <input type="email" class="form-control" id="correoTienda" value="eheppersonal@gmail.com" readonly>
+                            </div>
+                            <div class="form-group">
+                                <label for="enlaceTienda">Enlace</label>
+                                <input type="url" class="form-control" id="enlaceTienda" value="https://edishop.imporsuit.com" readonly>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
