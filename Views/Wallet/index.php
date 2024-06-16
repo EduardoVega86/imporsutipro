162d691ca629c00078cdcebd963bea96fde271ec
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-16 16:25:52 -0500

    agregar_index

--- Views/Wallet/index.php
+++ Views/Wallet/index.php
@@ -83,7 +83,7 @@
             color: darkblue;
         }
 </style>
-<?php require_once './Views/Pedidos/Modales/informacion_plataforma.php'; ?>
+
 <div class="custom-container-fluid">
     <div class="container mt-5" style="max-width: 1600px;">
         <h2 class="text-center mb-4">Dellate</h2>
@@ -108,5 +108,5 @@
         </div>
     </div>
 </div>
-<script src="<?php echo SERVERURL ?>/Views/Pedidos/js/guias.js"></script>
+<script src="<?php echo SERVERURL ?>/Views/Wallet/js/detalle.js"></script>
 <?php require_once './Views/templates/footer.php'; ?>
