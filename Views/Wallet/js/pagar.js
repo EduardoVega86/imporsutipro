f0e0286e675560e87e0deb8ae2774f601c3fb02c
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-17 08:28:50 -0500

    agregar_index

--- Views/Wallet/js/pagar.js
+++ Views/Wallet/js/pagar.js
@@ -8,11 +8,7 @@ const tienda = url.searchParams.get("tienda");
 var pagos_global;
 
 // Añadimos un evento que se ejecuta cuando el DOM ha sido completamente cargado
-document.addEventListener("DOMContentLoaded", async function () {
-    cargarDashboard_wallet();
-    await initDataTablePagos();
-  });
-  
+document.addEventListener("DOMContentLoaded", function () {});
 
 $(document).ready(function () {
   $("#regresar").click(function () {
@@ -20,40 +16,37 @@ $(document).ready(function () {
   });
 });
 
-async function cargarDashboard_wallet() {
-    let formData = new FormData();
-    formData.append("tienda", tienda);
-  
-    try {
-      let response = await fetch(SERVERURL + "wallet/obtenerDetalles", {
-        method: "POST",
-        body: formData
-      });
-  
-      if (!response.ok) {
-        throw new Error('Network response was not ok ' + response.statusText);
-      }
-  
-      let data = await response.json();
-  
-      pagos_global = data.pagos;
+function cargarDashboard_wallet() {
+  let formData = new FormData();
+  formData.append("tienda", tienda);
+
+  $.ajax({
+    url: SERVERURL + "wallet/obtenerDetalles",
+    type: "POST",
+    data: formData,
+    processData: false, // No procesar los datos
+    contentType: false, // No establecer ningún tipo de contenido
+    success: function (response) {
+      response = JSON.parse(response);
+
+      pagos_global = response.pagos;
       $("#image_tienda").attr(
         "src",
         SERVERURL + "public/img/profile_wallet.png"
       );
       $("#tienda_span").text(tienda);
-  
-      $("#totalVentas_wallet").text(data.ventas);
-      $("#utilidadGenerada_wallet").text(data.utilidad);
-      $("#descuentoDevolucion_wallet").text(data.devoluciones);
-      $("#retirosAcreditados_wallet").text(data.abonos_registrados);
-      $("#saldoBilletera_wallet").text(data.saldo);
-  
-    } catch (error) {
-      alert('Error: ' + error.message);
-    }
-  }
-  
+
+      $("#totalVentas_wallet").text(response.ventas);
+      $("#utilidadGenerada_wallet").text(response.utilidad);
+      $("#descuentoDevolucion_wallet").text(response.devoluciones);
+      $("#retirosAcreditados_wallet").text(response.abonos_registrados);
+      $("#saldoBilletera_wallet").text(response.saldo);
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      alert(errorThrown);
+    },
+  });
+}
 
 // TABLAS FACTURAS
 let filtro_facturas = "todas";
@@ -184,7 +177,6 @@ window.addEventListener("load", async () => {
   await initDataTableFacturas();
 });
 
-
 //TABLA DE PAGOS
 let dataTablePagos;
 let dataTablePagosIsInitialized = false;
@@ -227,18 +219,18 @@ const initDataTablePagos = async () => {
 
 const listPagos = async () => {
   try {
-    const pagos =  pagos_global;
+    const pagos = pagos_global;
     let content = ``;
-    let tipo ="";
-    console.log(pagos)
+    let tipo = "";
+    console.log(pagos);
     pagos.forEach((pago, index) => {
-        console.log("pago1"+pago.fecha);
+      console.log("pago1" + pago.fecha);
 
-        if (pago.recargo == 0){
-            tipo= "Pago de Billetera";
-        }else{
-            tipo= "Recargo de Billetera";
-        }
+      if (pago.recargo == 0) {
+        tipo = "Pago de Billetera";
+      } else {
+        tipo = "Recargo de Billetera";
+      }
       content += `
                 <tr>
                     <td>${pago.numero_documento}</td>
@@ -254,3 +246,8 @@ const listPagos = async () => {
     alert(ex);
   }
 };
+
+window.addEventListener("load", async () => {
+  cargarDashboard_wallet();
+  await initDataTablePagos();
+});
