a559379f2c3e5851812d70cf13a5fffdb2f49b8d
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-17 08:33:14 -0500

    agregar_index

--- Views/Wallet/js/pagar.js
+++ Views/Wallet/js/pagar.js
@@ -8,10 +8,18 @@ const tienda = url.searchParams.get("tienda");
 var pagos_global;
 
 // Añadimos un evento que se ejecuta cuando el DOM ha sido completamente cargado
-document.addEventListener("DOMContentLoaded", function () {
-  cargarDashboard_wallet();
-  initDataTablePagos();
-});
+document.addEventListener("DOMContentLoaded", async function () {
+    try {
+      // Ejecutar cargarDashboard_wallet primero
+      cargarDashboard_wallet();
+      
+      // Luego ejecutar initDataTablePagos
+      await initDataTablePagos();
+    } catch (error) {
+      // Manejar cualquier error que ocurra en initDataTablePagos
+      console.error("Error al inicializar la tabla de pagos:", error);
+    }
+  });
 
 $(document).ready(function () {
   $("#regresar").click(function () {
@@ -250,5 +258,3 @@ const listPagos = async () => {
     alert(ex);
   }
 };
-
-
