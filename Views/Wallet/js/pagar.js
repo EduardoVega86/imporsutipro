ce043085232902e551947cf7d93240cef65eeba7
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-16 19:43:05 -0500

    agregar_index

--- Views/Wallet/js/pagar.js
+++ Views/Wallet/js/pagar.js
@@ -137,7 +137,7 @@ const listFacturas = async () => {
                     </td>
 
                     <td><button class="icon-button"><i class="fas fa-eye"></i></button></td>
-                    <td><button class="icon-button" style="background-color: green;"><i class="fa-solid fa-file-pen"></i></button></td>
+                    <td><button class="icon-button" style="background-color: grenn;"><i class="fa-solid fa-file-pen"></i></button></td>
                     <td><button class="icon-button" style="background-color: #FCBF00;"><i class="fa-solid fa-rotate-left"></i></button></td>
                     <td></td>
                     <td></td>
