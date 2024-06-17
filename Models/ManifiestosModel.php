bc480a7be7b881168f6fba6daa7c9735be308d37
User <User@DESKTOP-9FVJ3SL>
2024-06-17 14:43:46 -0500

    producto

--- Models/ManifiestosModel.php
+++ Models/ManifiestosModel.php
@@ -61,13 +61,17 @@ class ManifiestosModel extends Query
         file_put_contents($combinedPdfPath, $dompdf->output());
 
         // Devolver la respuesta
-        $response = [
+         $new_url = str_replace("/home/imporsuitpro/public_html/new", "", $combinedPdfPath);
+            $new_url = "https://new.imporsuitpro.com" . $new_url;
+            
+        $reponse = [
             "url" => $combinedPdfPath,
-            "status" => "200"
+                "download" => $new_url,
+                "status" => "200"
         ];
         
 
-        return $response;
+        return $reponse;
     }
 
     public function generarManifiesto($arreglo)
