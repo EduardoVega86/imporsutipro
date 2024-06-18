4e08e1dc231acc5f7b70f7f249a15013a31fe266
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-18 16:21:15 -0500

    agregar_index

--- Views/Dashboard/dashboard.php
+++ Views/Dashboard/dashboard.php
@@ -38,7 +38,7 @@
         </div>
 
         <div class="slider-container">
-            <img src="<?php echo BANNER_INICIO; ?>" alt="Slider">
+            <img src="https://tiendas.imporsuitpro.com/imgs/logo.png" alt="Slider">
         </div>
     </div>
     <div class="tablas_estaditicas">
