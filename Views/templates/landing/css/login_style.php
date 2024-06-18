6f10e1cbaf715b87fce542e6067a11fda1486591
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-18 09:52:47 -0500

    agregar_index

--- Views/templates/landing/css/login_style.php
+++ Views/templates/landing/css/login_style.php
@@ -1,6 +1,6 @@
 <style>
     body {
-        background-color: #171931;
+        background-color: <?php echo COLOR_FONDO; ?>;
         color: #fff;
         background-size: cover;
         background-position: center;
