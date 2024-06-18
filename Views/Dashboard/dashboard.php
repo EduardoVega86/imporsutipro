2b0498812edaa07d10936d0ecb74183a12b8f50f
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-18 10:59:12 -0500

    agregar_index

--- Views/Dashboard/dashboard.php
+++ Views/Dashboard/dashboard.php
@@ -1,251 +1,139 @@
 <?php require_once './Views/templates/header.php'; ?>
-<style>
-    body {
-        background-color: #f8f9fa;
-        font-family: Arial, sans-serif;
-        margin: 0;
-        padding: 0;
-        height: 100vh;
-        overflow-y: auto;
-    }
+<?php require_once './Views/Dashboard/css/dashboard_style.php'; ?>
 
-    .header {
-        text-align: center;
-        margin: 20px 0;
-    }
+<div class="custom-container-fluid">
 
-    .stats-container {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-around;
-        margin-bottom: 20px;
-        width: 25%;
-    }
-
-    .stat-box {
-        background: #fff;
-        border-radius: 10px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        padding: 20px;
-        margin: 10px;
-        text-align: center;
-        flex: 1 1 calc(25% - 40px);
-        width: 50%;
-    }
-
-    .stat-box h3 {
-        margin-top: 10px;
-        font-size: 24px;
-    }
-
-    .slider-container {
-        width: 75%;
-        margin-bottom: 20px;
-    }
-
-    .slider-container img {
-        width: 100%;
-        border-radius: 10px;
-    }
-
-    .content-container {
-        display: flex;
-        justify-content: space-between;
-        width: 100%;
-    }
-
-    .content-box {
-        background: #fff;
-        border-radius: 10px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        padding: 20px;
-        margin: 10px;
-        flex: 1 1 calc(50% - 40px);
-        max-width: calc(50% - 40px);
-    }
-
-    .table-responsive {
-        max-height: 200px;
-        overflow-y: auto;
-    }
-
-    .table thead th {
-        background: #343a40;
-        color: #fff;
-    }
-
-    /* secciones principales  */
-    .banner_estadisticas {
-        display: flex;
-        flex-direction: row;
-    }
-
-    .tablas_estaditicas .content-container {
-        display: flex;
-        flex-direction: row;
-    }
-
-    /* responsive */
-    @media (max-width: 768px) {
-        .banner_estadisticas {
-            flex-direction: column-reverse !important;
-        }
-
-        .tablas_estaditicas .content-container {
-            flex-direction: column !important;
-        }
-
-        .stats-container {
-            width: 100%;
-        }
-
-        .slider-container {
-            width: 100%;
-        }
-
-        .content-box {
-            width: 100%;
-            max-width: calc(105% - 40px);
-        }
-
-    }
-</style>
-</head>
-
-<body>
-    <div class="custom-container-fluid">
-
-        <div class="banner_estadisticas">
-            <div class="stats-container">
-                <div class="d-flex flex-row">
-                    <div class="stat-box">
-                        <h3>5.00</h3>
-                        <p>Total Ventas</p>
-                    </div>
-                    <div class="stat-box">
-                        <h3>$ 299.98</h3>
-                        <p>Total Pedidos</p>
-                    </div>
+    <div class="banner_estadisticas">
+        <div class="stats-container">
+            <div class="d-flex flex-row">
+                <div class="stat-box">
+                    <h3>5.00</h3>
+                    <p>Total Ventas</p>
                 </div>
-                <div class="d-flex flex-row">
-                    <div class="stat-box">
-                        <h3>2.00</h3>
-                        <p>Total Guias</p>
-                    </div>
-                    <div class="stat-box">
-                        <h3>0.00</h3>
-                        <p>Total Recaudo</p>
-                    </div>
-                </div>
-                <div class="d-flex flex-row">
-                    <div class="stat-box">
-                        <h3>0.00</h3>
-                        <p>Total Fletes</p>
-                    </div>
-                    <div class="stat-box">
-                        <h3>0.00</h3>
-                        <p>Devoluciones</p>
-                    </div>
+                <div class="stat-box">
+                    <h3>$ 299.98</h3>
+                    <p>Total Pedidos</p>
                 </div>
             </div>
-
-            <div class="slider-container">
-                <img src="https://tiendas.imporsuitpro.com/imgs/logo.png" alt="Slider">
+            <div class="d-flex flex-row">
+                <div class="stat-box">
+                    <h3>2.00</h3>
+                    <p>Total Guias</p>
+                </div>
+                <div class="stat-box">
+                    <h3>0.00</h3>
+                    <p>Total Recaudo</p>
+                </div>
+            </div>
+            <div class="d-flex flex-row">
+                <div class="stat-box">
+                    <h3>0.00</h3>
+                    <p>Total Fletes</p>
+                </div>
+                <div class="stat-box">
+                    <h3>0.00</h3>
+                    <p>Devoluciones</p>
+                </div>
             </div>
         </div>
-        <div class="tablas_estaditicas">
-            <div class="content-container">
-                <div class="content-box">
-                    <h4>Últimos Pedidos</h4>
-                    <div class="table-responsive">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>No. Pedido</th>
-                                    <th>Fecha</th>
-                                    <th>Monto</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>COT-005757</td>
-                                    <td>01-06-2024</td>
-                                    <td>$44.99</td>
-                                </tr>
-                                <tr>
-                                    <td>COT-005756</td>
-                                    <td>01-06-2024</td>
-                                    <td>$45.00</td>
-                                </tr>
-                                <tr>
-                                    <td>COT-005755</td>
-                                    <td>01-06-2024</td>
-                                    <td>$49.99</td>
-                                </tr>
-                                <tr>
-                                    <td>COT-005754</td>
-                                    <td>01-06-2024</td>
-                                    <td>$80.00</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <button class="btn btn-primary">Ver todas las Ventas</button>
-                </div>
-                <div class="content-box">
-                    <h4>Ventas del Último Mes</h4>
-                    <canvas id="salesChart"></canvas>
-                </div>
-                <div class="content-box">
-                    <h4>Visitas</h4>
+
+        <div class="slider-container">
+            <img src="https://tiendas.imporsuitpro.com/imgs/logo.png" alt="Slider">
+        </div>
+    </div>
+    <div class="tablas_estaditicas">
+        <div class="content-container">
+            <div class="content-box">
+                <h4>Últimos Pedidos</h4>
+                <div class="table-responsive">
                     <table class="table">
                         <thead>
                             <tr>
-                                <th>Pagina</th>
-                                <th>Visitas</th>
+                                <th>No. Pedido</th>
+                                <th>Fecha</th>
+                                <th>Monto</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <td>Catalogo</td>
-                                <td>20000</td>
+                                <td>COT-005757</td>
+                                <td>01-06-2024</td>
+                                <td>$44.99</td>
                             </tr>
                             <tr>
-                                <td>Productos</td>
-                                <td>20000</td>
+                                <td>COT-005756</td>
+                                <td>01-06-2024</td>
+                                <td>$45.00</td>
+                            </tr>
+                            <tr>
+                                <td>COT-005755</td>
+                                <td>01-06-2024</td>
+                                <td>$49.99</td>
+                            </tr>
+                            <tr>
+                                <td>COT-005754</td>
+                                <td>01-06-2024</td>
+                                <td>$80.00</td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
+                <button class="btn btn-primary">Ver todas las Ventas</button>
+            </div>
+            <div class="content-box">
+                <h4>Ventas del Último Mes</h4>
+                <canvas id="salesChart"></canvas>
+            </div>
+            <div class="content-box">
+                <h4>Visitas</h4>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Pagina</th>
+                            <th>Visitas</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Catalogo</td>
+                            <td>20000</td>
+                        </tr>
+                        <tr>
+                            <td>Productos</td>
+                            <td>20000</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
-
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script>
-        var ctx = document.getElementById('salesChart').getContext('2d');
-        var salesChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: ['1 May', '2 May', '3 May', '4 May', '5 May', '6 May', '7 May', '8 May', '9 May', '10 May', '11 May', '12 May', '13 May', '14 May', '15 May', '16 May', '17 May', '18 May', '19 May', '20 May', '21 May', '22 May', '23 May', '24 May', '25 May', '26 May', '27 May', '28 May', '29 May', '30 May', '31 May'],
-                datasets: [{
-                    label: 'Ventas este mes',
-                    data: [0, 1000, 500, 1200, 1800, 2300, 300, 600, 800, 500, 700, 2000, 100, 0, 300, 500, 400, 300, 200, 1000, 200, 300, 500, 600, 300, 500, 700, 800, 600, 1000, 0],
-                    backgroundColor: 'rgba(0, 123, 255, 0.5)',
-                    borderColor: 'rgba(0, 123, 255, 1)',
-                    borderWidth: 1
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    var ctx = document.getElementById('salesChart').getContext('2d');
+    var salesChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['1 May', '2 May', '3 May', '4 May', '5 May', '6 May', '7 May', '8 May', '9 May', '10 May', '11 May', '12 May', '13 May', '14 May', '15 May', '16 May', '17 May', '18 May', '19 May', '20 May', '21 May', '22 May', '23 May', '24 May', '25 May', '26 May', '27 May', '28 May', '29 May', '30 May', '31 May'],
+            datasets: [{
+                label: 'Ventas este mes',
+                data: [0, 1000, 500, 1200, 1800, 2300, 300, 600, 800, 500, 700, 2000, 100, 0, 300, 500, 400, 300, 200, 1000, 200, 300, 500, 600, 300, 500, 700, 800, 600, 1000, 0],
+                backgroundColor: 'rgba(0, 123, 255, 0.5)',
+                borderColor: 'rgba(0, 123, 255, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
                 }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
             }
-        });
-    </script>
-    <?php require_once './Views/templates/footer.php'; ?>
+        }
+    });
+</script>
+<?php require_once './Views/templates/footer.php'; ?>
