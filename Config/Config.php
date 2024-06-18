c1273dc39d0272e252f488822e1140be47f08e24
Jeimy Jara <jeimyjara@hotmail.com>
2024-06-18 12:32:25 -0500

    barcode

--- Config/Config.php
+++ Config/Config.php
@@ -18,6 +18,7 @@ if (ENVIRONMENT == 'development') {
 } else {
 }
 $url_actual = "https://" . $_SERVER['HTTP_HOST'] . "/";
+echo $url_actual;
 $mysqli = new mysqli(HOST, USER, PASSWORD, DB);
 $mysqli->set_charset(CHARSET);
 if ($mysqli->connect_errno) {
