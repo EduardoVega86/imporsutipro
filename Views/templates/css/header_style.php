3b09a8ed194ea1c658353665e1963f73b62e911a
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-18 11:58:42 -0500

    agregar_index

--- Views/templates/css/header_style.php
+++ Views/templates/css/header_style.php
@@ -32,11 +32,11 @@
     }
 
     .menu-icon {
-        color: <?php echo COLOR_LETRAS; ?>;
+        color: <?php echo COLOR_LETRAS; ?> !important;
     }
 
     .menu-icon:hover {
-        color: <?php echo COLOR_LETRA_HOVER; ?>;
+        color: <?php echo COLOR_LETRA_HOVER; ?> !important;
     }
 
     .sidebar .menu {
