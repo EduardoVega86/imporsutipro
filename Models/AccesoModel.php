288f3d484a4d63d074a84fbde857cc0b5db7ecc7
Jeimy Jara <jeimyjara@hotmail.com>
2024-06-19 11:45:37 -0500

    barcode

--- Models/AccesoModel.php
+++ Models/AccesoModel.php
@@ -156,7 +156,8 @@ class AccesoModel extends Query
                 $_SESSION['id'] = $datos_usuario[0]['id_users'];
                 $_SESSION['tienda'] = $nombre_tienda[0]['nombre_tienda'];
                 $_SESSION['matriz'] = $this->obtenerMatriz();
-                $this->crearSubdominio($nombre_tienda[0]['nombre_tienda']);
+                $c = $this->crearSubdominio($nombre_tienda[0]['nombre_tienda']);
+                print_r($c);
             } else {
                 $response = $this->initialResponse();
                 $response['status'] = 401;
