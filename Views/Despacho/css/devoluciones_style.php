280fa816f4acabe803cca56b5fe8fb585796c7b5
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-19 09:22:32 -0500

    agregar_index

--- Views/Despacho/css/devoluciones_style.php
+++ Views/Despacho/css/devoluciones_style.php
@@ -17,7 +17,7 @@
     }
 
     .custom-container-fluid h1 {
-        color: white;
+        color: <?php echo COLOR_LETRAS;?>;
         margin-bottom: 20px;
     }
 
@@ -27,7 +27,7 @@
 
     .form-group label {
         display: block;
-        color: white;
+        color: <?php echo COLOR_LETRAS;?>;
         margin-bottom: 5px;
     }
 
