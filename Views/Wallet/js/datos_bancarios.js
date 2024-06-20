87ca07b85ea7ffa2da1983cb04b06de8d5949de2
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-20 09:58:55 -0500

    agregar_index

--- Views/Wallet/js/datos_bancarios.js
+++ Views/Wallet/js/datos_bancarios.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  //enviar datos bancarios
   $("#datos_bancario").on("submit", function (event) {
     event.preventDefault();
 
@@ -27,9 +28,59 @@ $(document).ready(function () {
             }
           );
         } else if (response.status == 200) {
-          toastr.success("DATOS BANCARIOS SE AGREGARON CORRECTAMENTE", "NOTIFICACIÓN", {
-            positionClass: "toast-bottom-center",
-          });
+          toastr.success(
+            "DATOS BANCARIOS SE AGREGARON CORRECTAMENTE",
+            "NOTIFICACIÓN",
+            {
+              positionClass: "toast-bottom-center",
+            }
+          );
+
+          $("#imagen_categoriaModal").modal("hide");
+          initDataTable();
+        }
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.error(textStatus, errorThrown);
+        alert("Error al guardar los datos");
+      },
+    });
+  });
+
+  //datos facturacion
+  $("#datos_facturacion").on("submit", function (event) {
+    event.preventDefault();
+
+    var formData = {
+      ruc: $("#ruc_factura").val(),
+      razon_social: $("#razon_socialFactura").val(),
+      direccion: $("#direccion_factura").val(),
+      correo: $("#correo_factura").val(),
+      telefono: $("#telefono_factura").val(),
+    };
+
+    $.ajax({
+      url: SERVERURL + "wallet/guardarDatosFacturacion",
+      type: "POST",
+      data: formData,
+      dataType: "json",
+      success: function (response) {
+        if (response.status == 500) {
+          toastr.error(
+            "DATOS DE FACTURACION NO SE AGREGARON CORRECTAMENTE",
+            "NOTIFICACIÓN",
+            {
+              positionClass: "toast-bottom-center",
+            }
+          );
+        } else if (response.status == 200) {
+          toastr.success(
+            "DATOS DE FACTURACION SE AGREGARON CORRECTAMENTE",
+            "NOTIFICACIÓN",
+            {
+              positionClass: "toast-bottom-center",
+            }
+          );
 
           $("#imagen_categoriaModal").modal("hide");
           initDataTable();
