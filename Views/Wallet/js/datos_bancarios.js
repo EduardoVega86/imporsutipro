e855dca40c26e6fbc6009d44ac8e3674792ef495
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-20 12:04:38 -0500

    agregar_index

--- Views/Wallet/js/datos_bancarios.js
+++ Views/Wallet/js/datos_bancarios.js
@@ -1,3 +1,4 @@
+//TABLA DE DATOS BANCARIOS
 let dataTableDatosBancarios;
 let dataTableDatosBancariosIsInitialized = false;
 
@@ -32,14 +33,18 @@ const initDataTableDatosBancarios = async () => {
 
   await listDatosBancarios();
 
-  dataTableDatosBancarios = $("#datatable_datos_bancarios").DataTable(dataTableDatosBancariosOptions);
+  dataTableDatosBancarios = $("#datatable_datos_bancarios").DataTable(
+    dataTableDatosBancariosOptions
+  );
 
   dataTableDatosBancariosIsInitialized = true;
 };
 
 const listDatosBancarios = async () => {
   try {
-    const response = await fetch("" + SERVERURL + "wallet/obtenerDatosFacturacion");
+    const response = await fetch(
+      "" + SERVERURL + "wallet/obtenerDatosFacturacion"
+    );
     const datosBancarios = await response.json();
 
     const datos_bancarios = datosBancarios.datos_bancarios;
@@ -47,7 +52,6 @@ const listDatosBancarios = async () => {
     let content = ``;
 
     datos_bancarios.forEach((dato, index) => {
-
       content += `
                 <tr>
                     <td>${dato.id_cuenta}</td>
@@ -70,6 +74,79 @@ window.addEventListener("load", async () => {
   await initDataTableDatosBancarios();
 });
 
+//TABLA DE DATOS DE FACTURACION
+let dataTableDatosFacturacion;
+let dataTableDatosFacturacionIsInitialized = false;
+
+const dataTableDatosFacturacionOptions = {
+  columnDefs: [
+    { className: "centered", targets: [0, 1, 2, 3, 4, 5] },
+    { orderable: false, targets: 0 }, //ocultar para columna 0 el ordenar columna
+  ],
+  pageLength: 10,
+  destroy: true,
+  language: {
+    lengthMenu: "Mostrar _MENU_ registros por página",
+    zeroRecords: "Ningún usuario encontrado",
+    info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+    infoEmpty: "Ningún usuario encontrado",
+    infoFiltered: "(filtrados desde _MAX_ registros totales)",
+    search: "Buscar:",
+    loadingRecords: "Cargando...",
+    paginate: {
+      first: "Primero",
+      last: "Último",
+      next: "Siguiente",
+      previous: "Anterior",
+    },
+  },
+};
+
+const initDataTableDatosFacturacion = async () => {
+  if (dataTableDatosFacturacionIsInitialized) {
+    dataTableDatosFacturacion.destroy();
+  }
+
+  await listDatosFacturacion();
+
+  dataTableDatosFacturacion = $("#datatable_datos_facturacion").DataTable(
+    dataTableDatosFacturacionOptions
+  );
+
+  dataTableDatosFacturacionIsInitialized = true;
+};
+
+const listDatosFacturacion = async () => {
+  try {
+    const response = await fetch("" + SERVERURL + "wallet/obtenerDatos");
+    const datosFacturacion = await response.json();
+
+    const datos_facturacion = datosFacturacion.datos_facturacion;
+    
+    let content = ``;
+
+    datos_facturacion.forEach((dato, index) => {
+      content += `
+                <tr>
+                    <td>${dato.id_facturacion}</td>
+                    <td>${dato.razon_social}</td>
+                    <td>${dato.ruc}</td>
+                    <td>${dato.direccion}</td>
+                    <td>${dato.correo}</td>
+                    <td>${dato.telefono}</td>
+                </tr>`;
+    });
+    document.getElementById("tableBody_datos_facturacion").innerHTML = content;
+  } catch (ex) {
+    alert(ex);
+  }
+};
+
+window.addEventListener("load", async () => {
+  await initDataTableDatosFacturacion();
+});
+
+//FUNCIONES PARA ENVIO DE INFORMACION
 $(document).ready(function () {
   //enviar datos bancarios
   $("#datos_bancario").on("submit", function (event) {
