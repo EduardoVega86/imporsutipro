7341683b45938de52c491c62ee7a2c280503ceb3
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-21 11:58:15 -0500

    agregar_index

--- Views/Shopify/constructor.php
+++ Views/Shopify/constructor.php
@@ -1,14 +1,18 @@
 <?php require_once './Views/templates/header.php'; ?>
 <?php require_once './Views/Shopify/css/constructor_style.php'; ?>
 
+<!-- Incluir los archivos CSS y JS de select2 -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
+
 <div class="full-screen-container">
     <div class="custom-container-fluid mt-4">
-        <h1>integraciones</h1>
+        <h1>Integraciones</h1>
         <div class="accordion" id="acordion_aplicacionShopify">
             <div class="accordion-item">
                 <h2 class="accordion-header" id="headingOne">
                     <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        Elija la aplicacion
+                        Elija la aplicación
                     </button>
                 </h2>
                 <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#acordion_aplicacionShopify">
@@ -34,7 +38,7 @@
                             <label for="infor">Nota: Agregar este enlace en su webhook</label>
                         </div>
 
-                        <div class="loading-animation" id="loading-below" style="display: none;">
+                        <div class="loading-animation" id="loading-below" style="display: none; margin-top: 10px;">
                             <div class="spinner-border" role="status">
                                 <span class="sr-only">Cargando...</span>
                             </div>
@@ -51,75 +55,75 @@
                 </h2>
                 <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#acordion_aplicacionShopify">
                     <div class="accordion-body" style="text-align: left;">
-                        <div class="form-group w-100 hidden-field" id="bodega-field">
-                            <label for="nombre">Nombre:</label>
-                            <select class="form-select" id="nombre">
+                        <div class="form-group w-100 hidden-field">
+                            <label for="select-nombre">Nombre:</label>
+                            <select class="form-select" id="select-nombre">
                                 <option value="" selected>-- Seleccione Nombre --</option>
                             </select>
                         </div>
-                        <div class="form-group w-100 hidden-field" id="bodega-field">
-                            <label for="apellido">Apellido:</label>
-                            <select class="form-select" id="apellido">
+                        <div class="form-group w-100 hidden-field">
+                            <label for="select-apellido">Apellido:</label>
+                            <select class="form-select" id="select-apellido">
                                 <option value="" selected>-- Seleccione Apellido --</option>
                             </select>
                         </div>
-                        <div class="form-group w-100 hidden-field" id="bodega-field">
-                            <label for="principal">Calle Principal:</label>
-                            <select class="form-select" id="principal">
+                        <div class="form-group w-100 hidden-field">
+                            <label for="select-principal">Calle Principal:</label>
+                            <select class="form-select" id="select-principal">
                                 <option value="" selected>-- Seleccione Calle Principal --</option>
                             </select>
                         </div>
-                        <div class="form-group w-100 hidden-field" id="bodega-field">
-                            <label for="secundario">Calle Secundaria:</label>
-                            <select class="form-select" id="secundario">
+                        <div class="form-group w-100 hidden-field">
+                            <label for="select-secundario">Calle Secundaria:</label>
+                            <select class="form-select" id="select-secundario">
                                 <option value="" selected>-- Seleccione Calle Secundaria --</option>
                             </select>
                         </div>
-                        <div class="form-group w-100 hidden-field" id="bodega-field">
-                            <label for="provincia">Provincia:</label>
-                            <select class="form-select" id="provincia">
+                        <div class="form-group w-100 hidden-field">
+                            <label for="select-provincia">Provincia:</label>
+                            <select class="form-select" id="select-provincia">
                                 <option value="" selected>-- Seleccione Provincia --</option>
                             </select>
                         </div>
-                        <div class="form-group w-100 hidden-field" id="bodega-field">
-                            <label for="ciudad">Ciudad:</label>
-                            <select class="form-select" id="ciudad">
+                        <div class="form-group w-100 hidden-field">
+                            <label for="select-ciudad">Ciudad:</label>
+                            <select class="form-select" id="select-ciudad">
                                 <option value="" selected>-- Seleccione Ciudad --</option>
                             </select>
                         </div>
-                        <div class="form-group w-100 hidden-field" id="bodega-field">
-                            <label for="codigo_postal">Codigo postal:</label>
-                            <select class="form-select" id="codigo_postal">
+                        <div class="form-group w-100 hidden-field">
+                            <label for="select-codigo_postal">Codigo postal:</label>
+                            <select class="form-select" id="select-codigo_postal">
                                 <option value="" selected>-- Seleccione Codigo postal --</option>
                             </select>
                         </div>
-                        <div class="form-group w-100 hidden-field" id="bodega-field">
-                            <label for="pais">País:</label>
-                            <select class="form-select" id="pais">
+                        <div class="form-group w-100 hidden-field">
+                            <label for="select-pais">País:</label>
+                            <select class="form-select" id="select-pais">
                                 <option value="" selected>-- Seleccione País --</option>
                             </select>
                         </div>
-                        <div class="form-group w-100 hidden-field" id="bodega-field">
-                            <label for="telefono">Teléfono:</label>
-                            <select class="form-select" id="telefono">
+                        <div class="form-group w-100 hidden-field">
+                            <label for="select-telefono">Teléfono:</label>
+                            <select class="form-select" id="select-telefono">
                                 <option value="" selected>-- Seleccione Teléfono --</option>
                             </select>
                         </div>
-                        <div class="form-group w-100 hidden-field" id="bodega-field">
-                            <label for="email">Email:</label>
-                            <select class="form-select" id="email">
+                        <div class="form-group w-100 hidden-field">
+                            <label for="select-email">Email:</label>
+                            <select class="form-select" id="select-email">
                                 <option value="" selected>-- Seleccione Email --</option>
                             </select>
                         </div>
-                        <div class="form-group w-100 hidden-field" id="bodega-field">
-                            <label for="total">Total:</label>
-                            <select class="form-select" id="total">
+                        <div class="form-group w-100 hidden-field">
+                            <label for="select-total">Total:</label>
+                            <select class="form-select" id="select-total">
                                 <option value="" selected>-- Seleccione Total --</option>
                             </select>
                         </div>
-                        <div class="form-group w-100 hidden-field" id="bodega-field">
-                            <label for="descuento">Descuento:</label>
-                            <select class="form-select" id="descuento">
+                        <div class="form-group w-100 hidden-field">
+                            <label for="select-descuento">Descuento:</label>
+                            <select class="form-select" id="select-descuento">
                                 <option value="" selected>-- Seleccione Descuento --</option>
                             </select>
                         </div>
