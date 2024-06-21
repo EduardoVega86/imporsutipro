4be2e6269be20dffe5d6da812384743dbf2744d1
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-21 10:01:04 -0500

    agregar_index

--- Views/Shopify/constructor.php
+++ Views/Shopify/constructor.php
@@ -7,11 +7,11 @@
         <div class="accordion" id="acordion_aplicacionShopify">
             <div class="accordion-item">
                 <h2 class="accordion-header" id="headingOne">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                         Elija la aplicacion
                     </button>
                 </h2>
-                <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#acordion_aplicacionShopify">
+                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#acordion_aplicacionShopify">
                     <div class="accordion-body" style="text-align: left;">
                         <div class="img-container text-center aplicacion" id="trigger-container">
                             <img src="<?php echo SERVERURL; ?>/public/img/logo_shopify.png" alt="Shopify">
