1788158798431e47bb44cd013a99684d7ce65742
Jeimy Jara <jeimyjara@hotmail.com>
2024-06-21 16:24:49 -0500

    Shopify

--- Models/CalculadoraModel.php
+++ Models/CalculadoraModel.php
@@ -117,26 +117,26 @@ class CalculadoraModel extends Query
         $url = "https://servientrega-ecuador.appsiscore.com/app/ws/cotizador_ser_recaudo.php?wsdl";
 
         $xml = <<<XML
-        <soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ws="https://servientrega-ecuador.appsiscore.com/app/ws/">
-        <soapenv:Header/>
-        <soapenv:Body>
-            <ws:Consultar soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-                <producto xsi:type="xsd:string">MERCANCIA PREMIER</producto>
-                <origen xsi:type="xsd:string">$ciudadO</origen>
-                <destino xsi:type="xsd:string">$destino</destino>
-                <valor_mercaderia xsi:type="xsd:string">$monto_factura</valor_mercaderia>
-                <piezas xsi:type="xsd:string">1</piezas>
-                <peso xsi:type="xsd:string">2</peso>
-                <alto xsi:type="xsd:string">10</alto>
-                <ancho xsi:type="xsd:string">50</ancho>
-                <largo xsi:type="xsd:string">50</largo>
-                <tokn xsi:type="xsd:string">1593aaeeb60a560c156387989856db6be7edc8dc220f9feae3aea237da6a951d</tokn>
-                <usu xsi:type="xsd:string">IMPCOMEX</usu>
-                <pwd xsi:type="xsd:string">Rtcom-ex9912</pwd>
-            </ws:Consultar>
-        </soapenv:Body>
-        </soapenv:Envelope>
-        XML;
+    <soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ws="https://servientrega-ecuador.appsiscore.com/app/ws/">
+    <soapenv:Header/>
+    <soapenv:Body>
+        <ws:Consultar soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+            <producto xsi:type="xsd:string">MERCANCIA PREMIER</producto>
+            <origen xsi:type="xsd:string">$ciudadO</origen>
+            <destino xsi:type="xsd:string">$destino</destino>
+            <valor_mercaderia xsi:type="xsd:string">$monto_factura</valor_mercaderia>
+            <piezas xsi:type="xsd:string">1</piezas>
+            <peso xsi:type="xsd:string">2</peso>
+            <alto xsi:type="xsd:string">10</alto>
+            <ancho xsi:type="xsd:string">50</ancho>
+            <largo xsi:type="xsd:string">50</largo>
+            <tokn xsi:type="xsd:string">1593aaeeb60a560c156387989856db6be7edc8dc220f9feae3aea237da6a951d</tokn>
+            <usu xsi:type="xsd:string">IMPCOMEX</usu>
+            <pwd xsi:type="xsd:string">Rtcom-ex9912</pwd>
+        </ws:Consultar>
+    </soapenv:Body>
+    </soapenv:Envelope>
+    XML;
 
         $ch = curl_init($url);
 
@@ -146,20 +146,22 @@ class CalculadoraModel extends Query
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
         $response = curl_exec($ch);
-        echo $response;
         curl_close($ch);
 
         // Parsear la respuesta XML
         $responseXml = new SimpleXMLElement($response);
         $namespaces = $responseXml->getNamespaces(true);
-        $body = $responseXml->children($namespaces['soapenv'])->Body;
-        $result = $body->children($namespaces['ws'])->ConsultarResponse->ConsultarResult;
+        $body = $responseXml->children($namespaces['SOAP-ENV'])->Body;
+        $result = $body->children($namespaces['ns1'])->ConsultarResponse->Result;
 
-        $flete = (float)$result->flete;
-        $seguro = (float)$result->seguro;
-        $comision = (float)$result->valor_comision;
-        $otros = (float)$result->otros;
-        $impuestos = (float)$result->impuesto;
+        // Convertir el resultado de la cadena XML a un objeto SimpleXMLElement
+        $resultXml = new SimpleXMLElement($result);
+
+        $flete = (float)$resultXml->flete;
+        $seguro = (float)$resultXml->seguro;
+        $comision = (float)$resultXml->valor_comision;
+        $otros = (float)$resultXml->otros;
+        $impuestos = (float)$resultXml->impuesto;
 
         $data = [
             "flete" => $flete,
