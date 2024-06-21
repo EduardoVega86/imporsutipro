2d4cc62541ab89323e50453fa1eafa72582a58f0
Jeimy Jara <jeimyjara@hotmail.com>
2024-06-21 09:02:32 -0500

    productos

--- Controllers/Shopify.php
+++ Controllers/Shopify.php
@@ -39,4 +39,45 @@ class Shopify extends Controller
         $data = $this->model->generarEnlace($_SESSION["id_plataforma"]);
         echo json_encode($data);
     }
+
+    public function guardarConfiguracion()
+    {
+        $nombre = $_POST["nombre"];
+        $apellido = $_POST["apellido"];
+        $principal = $_POST["principal"];
+        $secundario = $_POST["secundario"];
+        $provincia = $_POST["provincia"];
+        $ciudad = $_POST["ciudad"];
+        $codigo_postal = $_POST["codigo_postal"];
+        $pais = $_POST["pais"];
+        $telefono = $_POST["telefono"];
+        $email = $_POST["email"];
+        $total = $_POST["total"];
+        $descuento = $_POST["descuento"];
+
+        $data = array(
+            "nombre" => $nombre,
+            "apellido" => $apellido,
+            "principal" => $principal,
+            "secundario" => $secundario,
+            "provincia" => $provincia,
+            "ciudad" => $ciudad,
+            "codigo_postal" => $codigo_postal,
+            "pais" => $pais,
+            "telefono" => $telefono,
+            "email" => $email,
+            "total" => $total,
+            "descuento" => $descuento,
+            "id_plataforma" => $_SESSION["id_plataforma"]
+        );
+
+        $response = $this->model->guardarConfiguracion($data);
+        echo json_encode($response);
+    }
+
+    public function verificarConfiguracion()
+    {
+        $response = $this->model->verificarConfiguracion($_SESSION["id_plataforma"]);
+        echo json_encode($response);
+    }
 }
--- Models/ShopifyModel.php
+++ Models/ShopifyModel.php
@@ -33,4 +33,37 @@ class ShopifyModel extends Query
         );
         return $responses;
     }
+
+    public function guardarConfiguracion($id_plataforma, $data)
+    {
+        $sql = "INSERT INTO configuracion_shopify (id_plataforma, nombre, apellido, principal, secundario, provincia, ciudad, codigo_postal, pais, telefono, email, total, descuento) VALUES (?, ?, ?, ? ,?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $response = $this->insert($sql, $data);
+        if ($response == 1) {
+            $response["status"] = "200";
+            $response["message"] = "Configuracion guardada correctamente";
+        } else {
+            $response["status"] = "500";
+            $response["message"] = "Error al guardar la configuracion";
+        }
+        return $response;
+    }
+
+    public function verificarConfiguracion($id_plataforma)
+    {
+        $sql = "SELECT * FROM configuracion_shopify WHERE id_plataforma = ?";
+        $params = array($id_plataforma);
+        $response = $this->simple_select($sql, $params);
+        if ($response > 0) {
+            $response = array(
+                "status" => "200",
+                "message" => "Configuracion encontrada",
+            );
+        } else {
+            $response = array(
+                "status" => "500",
+                "message" => "No se ha encontrado la configuracion",
+            );
+        }
+        return $response;
+    }
 }
