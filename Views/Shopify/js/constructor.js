b3ae887c89c8b300cd99570c1354c7caa22b1387
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-21 11:35:13 -0500

    agregar_index

--- Views/Shopify/js/constructor.js
+++ Views/Shopify/js/constructor.js
@@ -23,75 +23,80 @@ document
     // final del cargar shopify
 
     //inicio cargar de boton verificar
-    document
-      .getElementById("verify-button")
-      .addEventListener("click", function () {
+    document.getElementById('verify-button').addEventListener('click', function() {
         // Mostrar la animación de carga debajo del input
-        document.getElementById("loading-below").style.display = "block";
+        document.getElementById('loading-below').style.display = 'block';
 
         // Iniciar el bucle de verificación
-        let intervalId = setInterval(function () {
-          $.ajax({
-            url: SERVERURL + "shopify/ultimoJson",
-            method: "GET",
-            dataType: "json",
-            success: function (data) {
-              if (data && data.id && data.confirmed) {
-                // Ocultar la animación de carga debajo del input
-                document.getElementById("loading-below").style.display = "none";
+        let intervalId = setInterval(function() {
+            $.ajax({
+                url: SERVERURL + 'shopify/ultimoJson',
+                method: 'GET',
+                dataType: 'json',
+                success: function(data) {
+                    if (data && data.id && data.confirmed) {
+                        // Ocultar la animación de carga debajo del input
+                        document.getElementById('loading-below').style.display = 'none';
 
-                // Llenar los selects con la información del JSON
-                fillSelects(data);
+                        // Llenar los selects con la información del JSON
+                        fillSelects(data);
 
-                // Abrir el siguiente acordeón
-                var collapseTwo = new bootstrap.Collapse(
-                  document.getElementById("collapseTwo"),
-                  {
-                    toggle: true,
-                  }
-                );
+                        // Abrir el siguiente acordeón
+                        var collapseTwo = new bootstrap.Collapse(document.getElementById('collapseTwo'), {
+                            toggle: true
+                        });
 
-                // Terminar el intervalo
-                clearInterval(intervalId);
-              } else {
-                // La condición no se cumple, mantener la animación de carga o mostrar un mensaje de error
-                document.getElementById("loading-below").innerHTML =
-                  '<div class="spinner-border" role="status"><span class="sr-only">Cargando...</span></div><div>No se pudo obtener información. Intentar nuevamente.</div>';
-              }
-            },
-            error: function (error) {
-              console.error("Error al llamar a la API:", error);
-              document.getElementById("loading-below").innerHTML =
-                '<div class="spinner-border" role="status"><span class="sr-only">Cargando...</span></div><div>Error al obtener información. Intentar nuevamente.</div>';
-            },
-          });
+                        // Terminar el intervalo
+                        clearInterval(intervalId);
+                    } else {
+                        // La condición no se cumple, mantener la animación de carga o mostrar un mensaje de error
+                        document.getElementById('loading-below').innerHTML =
+                          '<div class="spinner-border" role="status"><span class="sr-only">Cargando...</span></div><div>No se pudo obtener información. Intentar nuevamente.</div>';
+                    }
+                },
+                error: function(error) {
+                    console.error('Error al llamar a la API:', error);
+                    document.getElementById('loading-below').innerHTML =
+                      '<div class="spinner-border" role="status"><span class="sr-only">Cargando...</span></div><div>Error al obtener información. Intentar nuevamente.</div>';
+                }
+            });
         }, 5000);
-      });
+    });
 
     function fillSelects(data) {
-      fillSelect("#nombre", data.customer.first_name);
-      fillSelect("#apellido", data.customer.last_name);
-      fillSelect("#principal", data.billing_address.address1);
-      fillSelect("#secundario", data.billing_address.address2);
-      fillSelect("#provincia", data.billing_address.province);
-      fillSelect("#ciudad", data.billing_address.city);
-      fillSelect("#codigo_postal", data.billing_address.zip);
-      fillSelect("#pais", data.billing_address.country);
-      fillSelect("#telefono", data.billing_address.phone);
-      fillSelect("#email", data.customer.email);
-      fillSelect("#total", data.current_total_price);
-      fillSelect("#descuento", data.current_total_discounts);
+        clearSelects();
+        const keys = Object.keys(data);
+        keys.forEach(key => {
+            fillSelect('#nombre', data.customer.first_name);
+            fillSelect('#apellido', data.customer.last_name);
+            fillSelect('#principal', data.billing_address.address1);
+            fillSelect('#secundario', data.billing_address.address2);
+            fillSelect('#provincia', data.billing_address.province);
+            fillSelect('#ciudad', data.billing_address.city);
+            fillSelect('#codigo_postal', data.billing_address.zip);
+            fillSelect('#pais', data.billing_address.country);
+            fillSelect('#telefono', data.billing_address.phone);
+            fillSelect('#email', data.customer.email);
+            fillSelect('#total', data.current_total_price);
+            fillSelect('#descuento', data.current_total_discounts);
+        });
+        
+        // Inicializar select2 en todos los selects
+        $('.form-select').select2();
+    }
 
-      // Inicializar select2 en todos los selects
-      $(".form-select").select2();
+    function clearSelects() {
+        $('.form-select').empty().append('<option value="" selected>-- Seleccione --</option>');
     }
 
     function fillSelect(selectId, value) {
-      const select = document.querySelector(selectId);
-      const option = document.createElement("option");
-      option.value = value;
-      option.text = value;
-      select.appendChild(option);
+        if (value !== null && value !== undefined) {
+            const select = document.querySelector(selectId);
+            const option = document.createElement('option');
+            option.value = value;
+            option.text = value;
+            select.appendChild(option);
+        }
     }
     // final cargar de boton veritifcar
   });
