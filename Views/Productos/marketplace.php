96d3032c7b1b7f1011c7dd1ead842bfa6f9fe588
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-24 11:25:27 -0500

    agregar_index

--- Views/Productos/marketplace.php
+++ Views/Productos/marketplace.php
@@ -6,7 +6,7 @@
 
 <div class="custom-container-fluid mt-4">
     <div style="padding-bottom: 20px; padding-top: 20px;">
-        <div class="caja p-4 shadow-sm bg-white">
+    <div class="caja p-4 shadow-sm bg-white position-relative">
             <div class="row mb-3">
                 <div class="col-md-4 mb-3 mb-md-0">
                     <input type="text" class="form-control" placeholder="Código o Nombre">
@@ -27,12 +27,6 @@
                         <option value="3">Categoría 3</option>
                     </select>
                 </div>
-                <div class="col-md-2 mb-3 mb-md-0">
-                    <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="favoritosSwitch">
-                        <label class="form-check-label" for="favoritosSwitch">Favoritos</label>
-                    </div>
-                </div>
                 <div class="col-md-4 mb-3 mb-md-0">
                     <label for="priceRange" class="form-label">Rango de precio:</label>
                     <div id="price-range-slider"></div>
@@ -43,6 +37,12 @@
                 </div>
                 <div class="col-md-1">
                     <button class="btn btn-outline-secondary w-100"><i class="fa fa-sliders-h"></i> Aplicar filtros</button>
+                </div>
+            </div>
+            <div class="position-absolute top-0 end-0 mt-3 me-3">
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" id="favoritosSwitch">
+                    <label class="form-check-label" for="favoritosSwitch">Favoritos</label>
                 </div>
             </div>
         </div>
