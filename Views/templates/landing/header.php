e548969e3c4973e0e8f5c1e3992f012822de8963
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-24 16:23:17 -0500

    agregar_index

--- Views/templates/landing/header.php
+++ Views/templates/landing/header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo MARCA; ?></title>
+    <link rel="icon" type="image/png" href="<?php echo FAVICON; ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
