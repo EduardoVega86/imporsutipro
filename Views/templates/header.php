572fda20619c75058011a748c1c87ceb256d93a3
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-24 09:31:45 -0500

    agregar_index

--- Views/templates/header.php
+++ Views/templates/header.php
@@ -25,6 +25,7 @@
 
     <!-- footer -->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" rel="stylesheet">
     
 
