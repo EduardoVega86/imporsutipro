0f2acc650ea5188dd88d6cc9533725816393aa6c
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-24 11:17:14 -0500

    agregar_index

--- Views/Productos/marketplace.php
+++ Views/Productos/marketplace.php
@@ -8,12 +8,6 @@
     <div style="padding-bottom: 20px; padding-top: 20px;">
         <div class="caja p-4 shadow-sm bg-white">
             <div class="row mb-3">
-                <div class="col-md-2 mb-3 mb-md-0">
-                    <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="favoritosSwitch">
-                        <label class="form-check-label" for="favoritosSwitch">Favoritos</label>
-                    </div>
-                </div>
                 <div class="col-md-4 mb-3 mb-md-0">
                     <input type="text" class="form-control" placeholder="Código o Nombre">
                 </div>
@@ -32,6 +26,12 @@
                         <option value="2">Categoría 2</option>
                         <option value="3">Categoría 3</option>
                     </select>
+                </div>
+                <div class="col-md-2 mb-3 mb-md-0">
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" id="favoritosSwitch">
+                        <label class="form-check-label" for="favoritosSwitch">Favoritos</label>
+                    </div>
                 </div>
                 <div class="col-md-4 mb-3 mb-md-0">
                     <label for="priceRange" class="form-label">Rango de precio:</label>
