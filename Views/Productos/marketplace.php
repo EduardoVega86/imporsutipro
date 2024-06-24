2467730e8dfe4c5d6898983f3d69d398006d725e
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-24 10:42:40 -0500

    agregar_index

--- Views/Productos/marketplace.php
+++ Views/Productos/marketplace.php
@@ -5,49 +5,51 @@
 <?php require_once './Views/Productos/Modales/Seleccion_productoAtributo.php'; ?>
 
 <div class="custom-container-fluid mt-4">
-    <div class="caja p-4 shadow-sm bg-white">
-        <div class="row mb-3">
-            <div class="col-md-2 mb-3 mb-md-0">
-                <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" id="favoritosSwitch">
-                    <label class="form-check-label" for="favoritosSwitch">Favoritos</label>
+    <div>
+        <div class="caja p-4 shadow-sm bg-white">
+            <div class="row mb-3">
+                <div class="col-md-2 mb-3 mb-md-0">
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" id="favoritosSwitch">
+                        <label class="form-check-label" for="favoritosSwitch">Favoritos</label>
+                    </div>
                 </div>
-            </div>
-            <div class="col-md-2 mb-3 mb-md-0">
-                <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" id="privadosSwitch">
-                    <label class="form-check-label" for="privadosSwitch">Privados</label>
+                <div class="col-md-2 mb-3 mb-md-0">
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" id="privadosSwitch">
+                        <label class="form-check-label" for="privadosSwitch">Privados</label>
+                    </div>
                 </div>
-            </div>
-            <div class="col-md-2 mb-3 mb-md-0">
-                <select id="tipo_proveedor" class="form-select me-2">
-                    <option selected value="">Tipo de proveedor</option>
-                    <option value="1">Proveedor 1</option>
-                    <option value="2">Proveedor 2</option>
-                    <option value="3">Proveedor 3</option>
-                </select>
-            </div>
-            <div class="col-md-4 mb-3 mb-md-0">
-                <label for="priceRange" class="form-label">Rango de precio:</label>
-                <div id="price-range-slider"></div>
-                <div class="d-flex justify-content-between">
-                    <input type="text" id="price-min" class="form-control me-2" readonly>
-                    <input type="text" id="price-max" class="form-control" readonly>
+                <div class="col-md-2 mb-3 mb-md-0">
+                    <select id="tipo_proveedor" class="form-select me-2">
+                        <option selected value="">Tipo de proveedor</option>
+                        <option value="1">Proveedor 1</option>
+                        <option value="2">Proveedor 2</option>
+                        <option value="3">Proveedor 3</option>
+                    </select>
                 </div>
-            </div>
-            <div class="col-md-2 mb-3 mb-md-0">
-                <select id="categoria_filtroMarketplace" class="form-select me-2">
-                    <option selected value="">Categorías</option>
-                    <option value="1">Categoría 1</option>
-                    <option value="2">Categoría 2</option>
-                    <option value="3">Categoría 3</option>
-                </select>
-            </div>
-            <div class="col-md-1">
-                <button class="btn btn-outline-secondary w-100"><i class="fa fa-sliders-h"></i> Aplicar filtros</button>
-            </div>
-            <div class="col-md-1">
-                <button class="btn btn-warning w-100"><i class="fa fa-search"></i></button>
+                <div class="col-md-4 mb-3 mb-md-0">
+                    <label for="priceRange" class="form-label">Rango de precio:</label>
+                    <div id="price-range-slider"></div>
+                    <div class="d-flex justify-content-between">
+                        <input type="text" id="price-min" class="form-control me-2" readonly>
+                        <input type="text" id="price-max" class="form-control" readonly>
+                    </div>
+                </div>
+                <div class="col-md-2 mb-3 mb-md-0">
+                    <select id="categoria_filtroMarketplace" class="form-select me-2">
+                        <option selected value="">Categorías</option>
+                        <option value="1">Categoría 1</option>
+                        <option value="2">Categoría 2</option>
+                        <option value="3">Categoría 3</option>
+                    </select>
+                </div>
+                <div class="col-md-1">
+                    <button class="btn btn-outline-secondary w-100"><i class="fa fa-sliders-h"></i> Aplicar filtros</button>
+                </div>
+                <div class="col-md-1">
+                    <button class="btn btn-warning w-100"><i class="fa fa-search"></i></button>
+                </div>
             </div>
         </div>
     </div>
