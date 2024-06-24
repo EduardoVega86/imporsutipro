651c9f23bce69b91d99af4886a255fce3418438d
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-24 14:09:21 -0500

    agregar_index

--- Views/Wallet/pagar.php
+++ Views/Wallet/pagar.php
@@ -2,6 +2,7 @@
 <?php require_once './Views/Wallet/css/pagar_style.php'; ?>
 
 <?php require_once './Views/Wallet/Modales/solicitar_pago.php'; ?>
+
 <div class="custom-container-fluid">
     <div class="container mt-5" style="max-width: 1600px;">
         <h2 class="text-center mb-4">Pagar Wallet</h2>
