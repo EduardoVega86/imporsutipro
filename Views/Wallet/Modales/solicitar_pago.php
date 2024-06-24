833589f8edadc044f70e1cb3e721726eca8e5915
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-24 15:30:31 -0500

    agregar_index

--- Views/Wallet/Modales/solicitar_pago.php
+++ Views/Wallet/Modales/solicitar_pago.php
@@ -40,7 +40,7 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="solicitar_pagoModalLabel"><i class="fas fa-edit"></i>Solicitar Pago</h5>
+                <h5 class="modal-title" id="solicitar_pagoModalLabel"><i class="fas fa-edit"></i> Solicitar Pago</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
@@ -50,16 +50,18 @@
                         <input type="text" class="form-control" id="monto" placeholder="Ingresar monto">
                     </div>
 
-                    <label for="cuenta" class="form-label">Elegir cuenta:</label>
-                    <select class="form-select" id="cuenta">
-                        <option value="0">-- Seleccione una cuenta --</option>
-                    </select>
+                    <div class="mb-3">
+                        <label for="cuenta" class="form-label">Elegir cuenta:</label>
+                        <select class="form-select" id="cuenta">
+                            <option value="0">-- Seleccione una cuenta --</option>
+                        </select>
+                    </div>
+                </form>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                <button type="submit" class="btn btn-primary">Guardar</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                <button type="submit" class="btn btn-primary" form="solicitar_pago">Guardar</button>
             </div>
-            </form>
         </div>
     </div>
 </div>
