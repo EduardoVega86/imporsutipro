921df7d99aca13c662b28fa01940c3de84578b3f
Jeimy Jara <jeimyjara@hotmail.com>
2024-06-25 15:52:50 -0500

    Contraseña

--- Models/DashboardModel.php
+++ Models/DashboardModel.php
@@ -8,16 +8,16 @@ class DashboardModel extends Query
 
     public function filtroInicial($fecha_i, $fecha_f, $plataforma)
     {
-        $sql = "SELECT ROUND(SUM(total_venta),2) as ventas, ROUND(SUM(monto_recibir),2) as ganancias, ROUND(SUM(precio_envio),2) as envios FROM `cabecera_cuenta_pagar` WHERE fecha BETWEEN '$fecha_i' AND '$fecha_f' and estado_guia in (7,9) and tienda = $plataforma";
+        $sql = "SELECT ROUND(SUM(total_venta),2) as ventas, ROUND(SUM(monto_recibir),2) as ganancias, ROUND(SUM(precio_envio),2) as envios FROM `cabecera_cuenta_pagar` WHERE fecha BETWEEN '$fecha_i' AND '$fecha_f' and estado_guia in (7,9) and tienda = '$plataforma'";
         $response = $this->select($sql);
         echo $sql;
-        $sql = "SELECT COUNT(*) as total_guias FROM `cabecera_cuenta_pagar` WHERE fecha BETWEEN '$fecha_i' AND '$fecha_f' and tienda = $plataforma";
+        $sql = "SELECT COUNT(*) as total_guias FROM `cabecera_cuenta_pagar` WHERE fecha BETWEEN '$fecha_i' AND '$fecha_f' and tienda = '$plataforma'";
         $response2 = $this->select($sql);
 
-        $sql = "SELECT ROUND(SUM(monto_recibir),2) as devoluciones FROM `cabecera_cuenta_pagar` WHERE fecha BETWEEN '$fecha_i' AND '$fecha_f' and estado_guia = 9 and tienda = $plataforma";
+        $sql = "SELECT ROUND(SUM(monto_recibir),2) as devoluciones FROM `cabecera_cuenta_pagar` WHERE fecha BETWEEN '$fecha_i' AND '$fecha_f' and estado_guia = 9 and tienda = '$plataforma'";
         $response3 = $this->select($sql);
 
-        $sql = "SELECT COUNT(*) as pedidos from facturas_cot where fecha_factura BETWEEN '$fecha_i' AND '$fecha_f' and tienda = $plataforma";
+        $sql = "SELECT COUNT(*) as pedidos from facturas_cot where fecha_factura BETWEEN '$fecha_i' AND '$fecha_f' and tienda = '$plataforma'";
         $response4 = $this->select($sql);
 
         $datos = [
