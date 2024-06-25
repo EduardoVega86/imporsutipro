6fda1471af6f55d5ca64cba240bc5d2d5543099c
Jeimy Jara <jeimyjara@hotmail.com>
2024-06-25 15:55:54 -0500

    Contraseña

--- Models/DashboardModel.php
+++ Models/DashboardModel.php
@@ -8,25 +8,49 @@ class DashboardModel extends Query
 
     public function filtroInicial($fecha_i, $fecha_f, $plataforma)
     {
-        $sql = "SELECT ROUND(SUM(total_venta),2) as ventas, ROUND(SUM(monto_recibir),2) as ganancias, ROUND(SUM(precio_envio),2) as envios FROM `cabecera_cuenta_pagar` WHERE fecha BETWEEN '$fecha_i' AND '$fecha_f' and estado_guia in (7,9) and tienda = '$plataforma'";
+        // Consulta para ventas, ganancias, envíos y total de guías
+        $sql = "SELECT 
+                    ROUND(SUM(total_venta),2) as ventas, 
+                    ROUND(SUM(monto_recibir),2) as ganancias, 
+                    ROUND(SUM(precio_envio),2) as envios,
+                    COUNT(*) as total_guias
+                FROM cabecera_cuenta_pagar 
+                WHERE fecha BETWEEN '$fecha_i' AND '$fecha_f' 
+                AND tienda = '$plataforma' 
+                AND estado_guia IN (7, 9)";
         $response = $this->select($sql);
-        echo $sql;
-        $sql = "SELECT COUNT(*) as total_guias FROM `cabecera_cuenta_pagar` WHERE fecha BETWEEN '$fecha_i' AND '$fecha_f' and tienda = '$plataforma'";
-        $response2 = $this->select($sql);
 
-        $sql = "SELECT ROUND(SUM(monto_recibir),2) as devoluciones FROM `cabecera_cuenta_pagar` WHERE fecha BETWEEN '$fecha_i' AND '$fecha_f' and estado_guia = 9 and tienda = '$plataforma'";
+        // Consulta para devoluciones
+        $sql = "SELECT 
+                    ROUND(SUM(monto_recibir),2) as devoluciones 
+                FROM cabecera_cuenta_pagar 
+                WHERE fecha BETWEEN '$fecha_i' AND '$fecha_f' 
+                AND estado_guia = 9 
+                AND tienda = '$plataforma'";
         $response3 = $this->select($sql);
 
-        $sql = "SELECT COUNT(*) as pedidos from facturas_cot where fecha_factura BETWEEN '$fecha_i' AND '$fecha_f' and tienda = '$plataforma'";
+        // Consulta para pedidos
+        $sql = "SELECT 
+                    COUNT(*) as pedidos 
+                FROM facturas_cot 
+                WHERE fecha_factura BETWEEN '$fecha_i' AND '$fecha_f' 
+                AND tienda = '$plataforma'";
         $response4 = $this->select($sql);
 
+        $ventas = $response[0]['ventas'] ?? 0;
+        $ganancias = $response[0]['ganancias'] ?? 0;
+        $envios = $response[0]['envios'] ?? 0;
+        $total_guias = $response[0]['total_guias'] ?? 0;
+        $devoluciones = $response3[0]['devoluciones'] ?? 0;
+        $pedidos = $response4[0]['pedidos'] ?? 0;
+
         $datos = [
-            'ventas' => $response[0]['ventas'],
-            'envios' => $response[0]['envios'],
-            'ganancias' => $response[0]['ganancias'],
-            'total_guias' => $response2[0]['total_guias'],
-            'devoluciones' => $response3[0]['devoluciones'],
-            'pedidos' => $response4[0]['pedidos']
+            'ventas' => $ventas,
+            'envios' => $envios,
+            'ganancias' => $ganancias,
+            'total_guias' => $total_guias,
+            'devoluciones' => $devoluciones,
+            'pedidos' => $pedidos
         ];
 
         return $datos;
