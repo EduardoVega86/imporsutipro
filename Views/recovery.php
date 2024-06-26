257ff543a03d7538e3bb972582b75e2f6c3a3877
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-26 16:08:55 -0500

    agregar_index

--- Views/recovery.php
+++ Views/recovery.php
@@ -14,13 +14,37 @@
                 <label for="email">Restablecer contraseña</label>
                 <input type="text" class="form-control" id="email" placeholder="Email">
             </div>
-            <button type="button" class="btn btn-primary w-100"> <box-icon name='envelope' color='#ffff' type='solid'></box-icon> Enviar correo</button>
+            <button type="button" class="btn btn-primary w-100" id="sendEmailButton"> <box-icon name='envelope' color='#ffff' type='solid'></box-icon> Enviar correo</button>
             <a href="<?php echo SERVERURL ?>login" class="forgot-password">
                 <i class="fa-solid fa-arrow-left"></i> Volver
             </a>
         </form>
     </div>
 </div>
+<script>
+    $(document).ready(function() {
+        $('#sendEmailButton').click(function() {
+            var email = $('#email').val();
 
+            let formData = new FormData();
+            formData.append("correo", email);
+            $.ajax({
+                url: SERVERURL + 'acceso/recuperar_contrasena',
+                type: 'POST',
+                data: formData,
+                processData: false, // No procesar los datos
+                contentType: false, // No establecer ningún tipo de contenido
+                success: function(response) {
+                    // Maneja la respuesta de la API aquí
+                    alert('Correo enviado exitosamente');
+                },
+                error: function(error) {
+                    // Maneja el error aquí
+                    alert('Hubo un error al enviar el correo');
+                }
+            });
+        });
+    });
+</script>
 
 <?php require_once './Views/templates/landing/footer.php'; ?>
