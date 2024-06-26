371e0d6f3500097a8247eb47416c70932d0988a2
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-26 17:07:31 -0500

    agregar_index

--- Views/Acceso/recovery.php
+++ Views/Acceso/recovery.php
@@ -100,6 +100,7 @@
                 processData: false, // No procesar los datos
                 contentType: false, // No establecer ningún tipo de contenido
                 success: function(response) {
+                    response = JSON.parse(response);
                     if (response.status == 500) {
                         Swal.fire({
                             icon: 'error',
