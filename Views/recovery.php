4e939f04be8d12024ad28dd3cb7fc317b7ceb8c7
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-26 16:03:08 -0500

    agregar_index

--- Views/recovery.php
+++ Views/recovery.php
@@ -14,7 +14,7 @@
                 <label for="email">Restablecer contraseña</label>
                 <input type="text" class="form-control" id="email" placeholder="Email">
             </div>
-            <button type="button" class="btn btn-primary w-100"> <i class='bx bx-envelope' ></i> Enviar correo</button>
+            <button type="button" class="btn btn-primary w-100"> <box-icon name='envelope' color='#ffff' type='solid'></box-icon> Enviar correo</button>
             <a href="<?php echo SERVERURL ?>login" class="forgot-password">
                 <i class="fa-solid fa-arrow-left"></i> Volver
             </a>
