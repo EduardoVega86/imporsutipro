ada8d819b2ce1e1cd6d7351b6c8faff6fcafec50
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-26 16:27:37 -0500

    agregar_index

---
+++ Views/Acceso/recovery.php
@@ -0,0 +1,54 @@
+<?php require_once './Views/templates/landing/header.php'; ?>
+<?php require_once './Views/templates/landing/css/recoveryAcceso_style.php'; ?>
+
+<div class="d-flex flex-column" style="width: 700px;">
+    <div class="imagen_logo">
+        <img src="<?php echo LOGIN_IMAGE; ?>" alt="IMORSUIT" width="300px" height="150px">
+    </div>
+    <div class="container">
+        <div class="header">
+            <h1><?php echo MARCA; ?></h1>
+        </div>
+        <form id="cambiar_contrasena">
+            <div class="form-group">
+                <label for="contrasena">Contraseña</label>
+                <input type="password" class="form-control" id="contrasena" name="contrasena" placeholder="Contraseña">
+            </div>
+            <div class="form-group">
+                <label for="repetir-contrasena">Repetir Contraseña</label>
+                <input type="password" class="form-control" id="repetir-contrasena" name="repetir-contrasena" placeholder="Repetir Contraseña">
+            </div>
+            <button type="button" class="btn btn-primary w-100" id="sendEmailButton"> <box-icon name='envelope' color='#ffff' type='solid'></box-icon> Enviar correo</button>
+            <a href="<?php echo SERVERURL ?>login" class="forgot-password">
+                <i class="fa-solid fa-arrow-left"></i> Volver
+            </a>
+        </form>
+    </div>
+</div>
+<script>
+    $(document).ready(function() {
+        $('#sendEmailButton').click(function() {
+            var email = $('#email').val();
+
+            let formData = new FormData();
+            formData.append("correo", email);
+            $.ajax({
+                url: SERVERURL + 'acceso/recuperar_contrasena',
+                type: 'POST',
+                data: formData,
+                processData: false, // No procesar los datos
+                contentType: false, // No establecer ningún tipo de contenido
+                success: function(response) {
+                    // Maneja la respuesta de la API aquí
+                    alert('Correo enviado exitosamente');
+                },
+                error: function(error) {
+                    // Maneja el error aquí
+                    alert('Hubo un error al enviar el correo');
+                }
+            });
+        });
+    });
+</script>
+
+<?php require_once './Views/templates/landing/footer.php'; ?>
