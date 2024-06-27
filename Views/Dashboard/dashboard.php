13b8dba58f59a3a5fd0194265e995acf5cb3ea4f
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-27 16:31:08 -0500

    agregar_index

--- Views/Dashboard/dashboard.php
+++ Views/Dashboard/dashboard.php
@@ -101,4 +101,27 @@
 </div>
 
 <script src="<?php echo SERVERURL ?>/Views/Dashboard/js/dashboard.js"></script>
+<script>
+    var ctx = document.getElementById('salesChart').getContext('2d');
+    var salesChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['1 May', '2 May', '3 May', '4 May', '5 May', '6 May', '7 May', '8 May', '9 May', '10 May', '11 May', '12 May', '13 May', '14 May', '15 May', '16 May', '17 May', '18 May', '19 May', '20 May', '21 May', '22 May', '23 May', '24 May', '25 May', '26 May', '27 May', '28 May', '29 May', '30 May', '31 May'],
+            datasets: [{
+                label: 'Ventas este mes',
+                data: [0, 1000, 500, 1200, 1800, 2300, 300, 600, 800, 500, 700, 2000, 100, 0, 300, 500, 400, 300, 200, 1000, 200, 300, 500, 600, 300, 500, 700, 800, 600, 1000, 0],
+                backgroundColor: 'rgba(0, 123, 255, 0.5)',
+                borderColor: 'rgba(0, 123, 255, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+</script>
 <?php require_once './Views/templates/footer.php'; ?>
