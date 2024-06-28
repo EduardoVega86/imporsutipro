8ff9878a9aae68db96d0c09122a52f822094dff8
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-06-28 11:33:15 -0500

    agregar_index

--- Views/Productos/css/inventario_style.php
+++ Views/Productos/css/inventario_style.php
@@ -58,6 +58,23 @@
         padding: 4px;
         border-radius: 0.3rem;
     }
+
+    .btn-excel,
+    .btn-csv {
+        background-color: #198754;
+        /* Color verde para Excel */
+        color: white;
+        padding: 5px 10px;
+        /* Ajusta el padding para reducir el tamaño */
+        margin: 0;
+        /* Elimina el margen */
+        border: none;
+        /* Elimina el borde */
+    }
+
+    .btn-group {
+        align-content: center;
+    }
 </style>
 
 <style>
