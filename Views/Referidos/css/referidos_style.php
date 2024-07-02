c72ca4280f46b862e24124f064aed110b87e6879
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-02 08:43:15 -0500

    agregar_index

---
+++ Views/Referidos/css/referidos_style.php
@@ -0,0 +1,63 @@
+<style>
+    .table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    .table th,
+    .table td {
+        text-align: center;
+        vertical-align: middle;
+        border: 1px solid #ddd;
+        /* Añadir borde a celdas */
+    }
+
+    .table-striped tbody tr:nth-of-type(odd) {
+        background-color: rgba(0, 0, 0, .05);
+    }
+
+    .table-hover tbody tr:hover {
+        background-color: rgba(0, 0, 0, .075);
+    }
+
+    .table thead th {
+        background-color: <?php echo COLOR_FONDO; ?>;
+        color: <?php echo COLOR_LETRAS; ?>;
+    }
+
+    .centered {
+        text-align: center !important;
+        vertical-align: middle !important;
+    }
+</style>
+
+<style>
+    /* reponsive de secciones */
+    .left_right {
+        display: flex;
+        flex-direction: row;
+    }
+
+    .left {
+        max-width: 37%;
+    }
+
+    .right {
+        width: 100%;
+        max-width: 63%;
+    }
+
+    @media (max-width: 768px) {
+        .left_right {
+            flex-direction: column;
+        }
+
+        .left {
+            max-width: 100%;
+        }
+
+        .right {
+            max-width: 100%;
+        }
+    }
+</style>
---
+++ Views/Referidos/index.php
@@ -0,0 +1,89 @@
+<?php require_once './Views/templates/header.php'; ?>
+<?php require_once './Views/Referidos/css/referidos_style.php'; ?>
+
+<div class="custom-container-fluid">
+    <div class="container mt-5" style="max-width: 1600px;">
+
+        <div class="left_right gap-2">
+            <div class="table-responsive left">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <img src="" id="image_tienda" width="100px" class="rounded-circle mb-3" alt="Profile Picture">
+                        <h5 class="card-title"><a href="#" id="tienda_url"><span id="tienda_span"></span></a></h5>
+
+                        <div class="row text-start">
+                            <div class="col-12 mb-3">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <div class="d-flex align-items-center">
+                                                <i class="bi bi-cart-fill fs-1 text-primary"></i>
+                                                <div class="ms-3">
+                                                    <p class="mb-0">Cantidad de referidos</p>
+                                                    <h3 class="text-primary">$<span id="totalVentas_wallet"></span></h3>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 mb-3">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <div class="d-flex align-items-center">
+                                                <i class="bi bi-currency-dollar fs-1 text-success"></i>
+                                                <div class="ms-3">
+                                                    <p class="mb-0">Ganancias con Referidos</p>
+                                                    <h3 class="text-success">$<span id="utilidadGenerada_wallet"></span></h3>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 mb-3">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <div class="d-flex align-items-center">
+                                                <i class="bi bi-cash-coin fs-1 text-danger"></i>
+                                                <div class="ms-3">
+                                                    <p class="mb-0">DESCUENTO DEVOLUCIÓN</p>
+                                                    <h3 class="text-danger">$<span id="descuentoDevolucion_wallet"></span></h3>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 mb-3">
+                                <button class="btn btn-primary mb-3" id="regresar"><i class="fa-solid fa-arrow-left"></i> Regresar</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="right gap-2">
+                <h3 style="text-align: center; padding-top:5px;">Guias Referidos</h3>
+                <div class="table-responsive">
+                    <table id="datatable_pagos" class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th class="centered">Numero referido</th>
+                                <th class="centered">Guia</th>
+                                <th class="centered">Monto</th>
+                                <th class="centered">Fecha</th><
+                            </tr>
+                        </thead>
+                        <tbody id="tableBody_pagos"></tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="<?php echo SERVERURL ?>/Views/Referidos/js/referidos.js"></script>
+<?php require_once './Views/templates/footer.php'; ?>
