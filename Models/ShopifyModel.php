be278404c6271de4a11d9f54d207d69a7f52bc6c
Jeimy Jara <jeimyjara@hotmail.com>
2024-07-04 09:12:47 -0500

    XD

--- Models/ShopifyModel.php
+++ Models/ShopifyModel.php
@@ -293,7 +293,6 @@ class ShopifyModel extends Query
     {
         $sql = "REPLACE INTO configuracion_shopify (`nombre`, `apellido`, `principal`, `secundaria`, `provincia`, `ciudad`, `codigo_postal`, `pais`, `telefono`, `email`, `total`, `discount`, `id_plataforma`) VALUES (?, ?, ?, ? ,?, ?, ?, ?, ?, ?, ?, ?, ?)";
         $response = $this->insert($sql, [$nombre, $apellido, $principal, $secundario, $provincia, $ciudad, $codigo_postal, $pais, $telefono, $email, $total, $descuento, $id_plataforma]);
-        print_r($response);
         if ($response == 2) {
             $responses["status"] = 200;
             $responses["message"] = "Configuracion guardada correctamente";
