da5aef542c271b407e18133862899589b7f82df7
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-06 12:30:11 -0500

    agregar_index

--- Views/Wallet/datos_bancarios.php
+++ Views/Wallet/datos_bancarios.php
@@ -94,7 +94,7 @@
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="headingTwo">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                Elegir forma de pago
+                                Otras formas de pago
                             </button>
                         </h2>
                         <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#acordion_datosBancarios">
