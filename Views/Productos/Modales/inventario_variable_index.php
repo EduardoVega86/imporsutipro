aab34050261d50aca657c05ec5f81602a47a8eae
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-08 08:53:53 -0500

    agregar_index

--- Views/Productos/Modales/inventario_variable_index.php
+++ Views/Productos/Modales/inventario_variable_index.php
@@ -110,7 +110,7 @@
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
             </div>
         </div>
     </div>
