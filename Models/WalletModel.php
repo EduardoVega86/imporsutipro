b59b6b61ce75dd95970f5a2254e561c0c8b1114e
Jeimy Jara <jeimyjara@hotmail.com>
2024-07-09 11:17:24 -0500

    XD

--- Models/WalletModel.php
+++ Models/WalletModel.php
@@ -630,7 +630,7 @@ class WalletModel extends Query
         $id_matriz = $this->obtenerMatriz();
         $id_matriz = $id_matriz[0]['idmatriz'];
 
-        $sql = "SELECT * FROM solicitudes_pago inner join datos_banco_usuarios on solicitudes_pago.id_cuenta = datos_banco_usuarios.id_cuenta where solicitudes_pago.matriz = $id_matriz";
+        $sql = "SELECT * FROM solicitudes_pago inner join datos_banco_usuarios on solicitudes_pago.id_cuenta = datos_banco_usuarios.id_cuenta where solicitudes_pago.id_matriz = $id_matriz";
         $response =  $this->select($sql);
         return $response;
     }
