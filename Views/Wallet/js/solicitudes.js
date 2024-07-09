280d510a664ca1ae10bb3bc5e4cb93ea720b2653
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-09 11:03:30 -0500

    agregar_index

---
+++ Views/Wallet/js/solicitudes.js
@@ -0,0 +1,69 @@
+let dataTableSolicitudes;
+let dataTableSolicitudesIsInitialized = false;
+
+const dataTableSolicitudesOptions = {
+  columnDefs: [
+    { className: "centered", targets: [1, 2, 3, 4, 5] },
+    { orderable: false, targets: 0 }, //ocultar para columna 0 el ordenar columna
+  ],
+  pageLength: 10,
+  destroy: true,
+  language: {
+    lengthMenu: "Mostrar _MENU_ registros por página",
+    zeroRecords: "Ningún usuario encontrado",
+    info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+    infoEmpty: "Ningún usuario encontrado",
+    infoFiltered: "(filtrados desde _MAX_ registros totales)",
+    search: "Buscar:",
+    loadingRecords: "Cargando...",
+    paginate: {
+      first: "Primero",
+      last: "Último",
+      next: "Siguiente",
+      previous: "Anterior",
+    },
+  },
+};
+
+const initDataTableSolicitudes = async () => {
+  if (dataTableSolicitudesIsInitialized) {
+    dataTableSolicitudes.destroy();
+  }
+
+  await listSolicitudes();
+
+  dataTableSolicitudes = $("#datatable_solicitudes").DataTable(dataTableSolicitudesOptions);
+
+  dataTableSolicitudesIsInitialized = true;
+};
+
+const listSolicitudes = async () => {
+  try {
+    const response = await fetch("" + SERVERURL + "wallet/obtenerSolicitudes");
+    const solicitudes = await response.json();
+
+    let content = ``;
+
+    solicitudes.forEach((solicitud, index) => {
+
+      content += `
+                <tr>
+                    <td>${solicitud.ventas}</td>
+                    <td>${solicitud.utilidad}</td>
+                    <td>${solicitud.count_visto_0}</td>
+                    <td>
+                    <button id="downloadExcel" class="btn btn-success" onclick="descargarExcel_general('${solicitud.tienda}')">Descargar Excel general</button>
+                    <button id="downloadExcel" class="btn btn-success" onclick="descargarExcel('${solicitud.tienda}')">Descargar Excel</button>
+                    </td>
+
+                </tr>`;
+    });
+    document.getElementById("tableBody_solicitudes").innerHTML = content;
+  } catch (ex) {
+    alert(ex);
+  }
+};
+
+window.addEventListener("load", async () => {
+  await initDataTableSolicitudes();
+});
---
+++ Views/Wallet/solicitudes.php
@@ -0,0 +1,38 @@
+<?php require_once './Views/templates/header.php'; ?>
+<?php require_once './Views/Pedidos/css/guias_style.php'; ?>
+
+
+<?php require_once './Views/Pedidos/Modales/informacion_plataforma.php'; ?>
+
+<div class="custom-container-fluid">
+    <div class="container mt-5" style="max-width: 1600px;">
+        <h2 class="text-center mb-4">Guias</h2>
+
+        <div class="table-responsive">
+            <!-- <table class="table table-bordered table-striped table-hover"> -->
+            <table id="datatable_guias" class="table table-striped">
+                <!-- <caption>
+                    DataTable.js Demo
+                </caption> -->
+                <thead>
+                    <tr>
+                        <th class="centered"><input type="checkbox" id="selectAll"></th>
+                        <th class="centered"># Orden</th>
+                        <th class="centered">Detalle</th>
+                        <th class="centered">Cliente</th>
+                        <th class="centered">Destino</th>
+                        <th class="centered">Tienda</th>
+                        <th class="centered">Transportadora</th>
+                        <th class="centered">Estado</th>
+                        <th class="centered">Impreso</th>
+                        <th class="centered">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody id="tableBody_guias"></tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<script src="<?php echo SERVERURL ?>/Views/Wallet/js/solicitudes.js"></script>
+<?php require_once './Views/templates/footer.php'; ?>
