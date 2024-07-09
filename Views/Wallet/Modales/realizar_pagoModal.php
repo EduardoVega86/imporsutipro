58465367be133734d13ac8f68ed0f9f71ad28d27
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-09 12:04:48 -0500

    agregar_index

--- Views/Wallet/Modales/realizar_pagoModal.php
+++ Views/Wallet/Modales/realizar_pagoModal.php
@@ -52,7 +52,7 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="realizar_pagoModalLabel"><i class="fas fa-edit"></i> Solicitar Pago</h5>
+                <h5 class="modal-title" id="realizar_pagoModalLabel"><i class="fas fa-edit"></i> Realizar Pago</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
