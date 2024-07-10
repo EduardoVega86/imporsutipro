9993392ea0ee2481180acec8719e7b81d28d3c08
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-10 14:05:25 -0500

    agregar_index

--- Views/Wallet/js/pagar.js
+++ Views/Wallet/js/pagar.js
@@ -140,15 +140,15 @@ const listFacturas = async () => {
       if (factura.guia.includes("IMP")) {
         url_tracking = `https://fenix.laarcourier.com/Tracking/Guiacompleta.aspx?guia=${factura.guia}`;
         url_descargar = `https://api.laarcourier.com:9727/guias/pdfs/DescargarV2?guia=${factura.guia}`;
-        estado = validar_estadoLaar(guia.estado_guia_sistema);
+        estado = validar_estadoLaar(factura.guia);
       } else if (factura.guia.includes("I")) {
         url_tracking = `https://ec.gintracom.site/web/site/tracking`;
         url_descargar = `https://guias.imporsuitpro.com/Gintracom/label/${factura.guia}`;
-        estado = validar_estadoGintracom(guia.estado_guia_sistema);
+        estado = validar_estadoGintracom(factura.guia);
       } else if (factura.guia.includes("SPD")) {
         url_tracking = ``;
         url_descargar = `https://guias.imporsuitpro.com/Speed/descargar/${factura.guia}`;
-        estado = validar_estadoSpeed(guia.estado_guia_sistema);
+        estado = validar_estadoSpeed(factura.guia);
       } else {
         url_tracking = `https://servientrega-ecuador.appsiscore.com/app/app-cliente/cons_publica.php?guia=${factura.guia}&Request=Buscar+`;
         url_descargar = `https://guias.imporsuitpro.com/Servientrega/guia/${factura.guia}`;
