c7c0823e613a95c110a0179b0ac4e830bf726fae
Jeimy Jara <jeimyjara@hotmail.com>
2024-07-10 12:32:53 -0500

    XD

--- Models/WalletModel.php
+++ Models/WalletModel.php
@@ -75,9 +75,12 @@ class WalletModel extends Query
         $monto_recibir = $total_venta - $costo - $full - $precio_envio;
         $sql = "UPDATE cabecera_cuenta_pagar set total_venta = ?, precio_envio = ?, full = ?, costo = ?, monto_recibir = ?, valor_pendiente = ? WHERE id_cabecera = ?";
         $response =  $this->update($sql, array($total_venta, $precio_envio, $full, $costo, $monto_recibir, $monto_recibir, $id_cabecera));
-        print_r($response);
+
         if ($response == 1) {
             $responses["status"] = 200;
+        } else if ($response == 0) {
+            $responses["status"] = 201;
+            $responses["message"] = "No se realizaron cambios";
         } else {
             $responses["status"] = 400;
             $responses["message"] = $response["message"];
