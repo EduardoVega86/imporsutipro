9154fdd19fcf4bac49bd46841c100cb1a7c83a09
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-11 11:08:20 -0500

    agregar_index

--- Views/Usuarios/tienda_online.php
+++ Views/Usuarios/tienda_online.php
@@ -163,7 +163,7 @@
                     </div>
                     <div class="table-responsive">
                         <!-- <table class="table table-bordered table-striped table-hover"> -->
-                        <table id="datatable_productos" class="table table-striped">
+                        <table id="datatable_banner" class="table table-striped">
                             <thead>
                                 <tr>
                                     <th class="text-nowrap">Fondo Banner</th>
@@ -175,7 +175,7 @@
                                     <th class="text-nowrap">Acciones</th>
                                 </tr>
                             </thead>
-                            <tbody id="tableBody_productos"></tbody>
+                            <tbody id="tableBody_banner"></tbody>
                         </table>
                     </div>
                 </div>
