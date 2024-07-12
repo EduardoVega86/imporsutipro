695f0f967e4875ce20a65f84d8d88bf28bcfa4a8
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-11 19:14:24 -0500

    agregar_index

--- Views/Productos/js/categorias.js
+++ Views/Productos/js/categorias.js
@@ -78,21 +78,23 @@ const listCategorias = async () => {
           '<span style="background-color: #28C839; color: white; padding: 5px; border-radius: 0.3rem;">Activo</span>';
       }
 
+      let id_plataforma = obtenerIdPlataforma();
+
       if (!categoria.imagen) {
-        if (categoria.global == 1) {
+        if (categoria.global == 1 && categoria.id_plataforma == id_plataforma) {
           cargar_imagen = ``;
         } else {
           cargar_imagen = `<i class="bx bxs-camera-plus" onclick="agregar_imagenCategoria(${categoria.id_linea})"></i>`;
         }
       } else {
-        if (categoria.global == 1) {
+        if (categoria.global == 1 && categoria.id_plataforma == id_plataforma) {
           cargar_imagen = ``;
         } else {
           cargar_imagen = `<img src="${SERVERURL}${categoria.imagen}" class="icon-button" onclick="agregar_imagenCategoria(${categoria.id_linea})" alt="Agregar imagen" width="50px">`;
         }
       }
 
-      if (categoria.global == 1) {
+      if (categoria.global == 1 && categoria.id_plataforma == id_plataforma) {
         botones_accion = ``;
       } else {
         botones_accion = `<button class="btn btn-sm btn-primary" onclick="editar_categoria(${categoria.id_linea})"><i class="fa-solid fa-pencil"></i>Editar</button>
@@ -117,6 +119,20 @@ const listCategorias = async () => {
     alert(ex);
   }
 };
+
+function obtenerIdPlataforma(){
+  $.ajax({
+    url: SERVERURL + "pedidos/info",
+    type: "GET",
+    dataType: "json",
+    success: function (response) {
+      return response[0].id_plataforma;
+    },
+    error: function (error) {
+      console.error("Error al obtener la lista de bodegas:", error);
+    },
+  });
+}
 
 function eliminar_categoria(id) {
   $.ajax({
