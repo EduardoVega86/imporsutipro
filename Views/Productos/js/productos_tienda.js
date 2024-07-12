034aa5e2ad87098b433f029eb2f07bc58e0e6fcb
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-11 19:16:47 -0500

    agregar_index

--- Views/Productos/js/productos_tienda.js
+++ Views/Productos/js/productos_tienda.js
@@ -42,7 +42,7 @@ const dataTableProductosOptions = {
       title: "Panel de Control: Productos",
       titleAttr: "Exportar a CSV",
       exportOptions: {
-        columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        columns: [0, 1, 2, 3, 4, 5],
       },
       filename: "Productos" + "_" + getFecha(),
       footer: true,
