55563224ce22bf9ae9dc70ff77526b6e52cfb5d8
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-17 17:21:30 -0500

    agregar_index

--- Views/Wallet/js/solicitudes.js
+++ Views/Wallet/js/solicitudes.js
@@ -131,7 +131,7 @@ let dataTableAuditoriaIsInitialized = false;
 
 const dataTableAuditoriaOptions = {
   columnDefs: [
-    { className: "centered", targets: [1, 2, 3, 4, 5] },
+    { className: "centered", targets: [0, 1, 2, 3, 4] },
     { orderable: false, targets: 0 }, //ocultar para columna 0 el ordenar columna
   ],
   pageLength: 10,
