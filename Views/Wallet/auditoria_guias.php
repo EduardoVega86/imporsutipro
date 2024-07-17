0910efb5eac4d8a758e7b904c45fa45194bb6d1a
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-17 16:25:00 -0500

    agregar_index

--- Views/Wallet/auditoria_guias.php
+++ Views/Wallet/auditoria_guias.php
@@ -1,39 +1,46 @@
 <?php require_once './Views/templates/header.php'; ?>
-<?php require_once './Views/Pedidos/css/guias_style.php'; ?>
+<?php require_once './Views/Wallet/css/auditoria_guias_style.php'; ?>
 
-
-<?php require_once './Views/Pedidos/Modales/informacion_plataforma.php'; ?>
 
 <div class="custom-container-fluid">
     <div class="container mt-5" style="max-width: 1600px;">
-        <h2 class="text-center mb-4">Guias</h2>
+        <h2 class="text-center mb-4">Auditoria</h2>
 
         <div class="table-responsive">
             <!-- <table class="table table-bordered table-striped table-hover"> -->
-            <table id="datatable_solicitudes" class="table table-striped">
-                <!-- <caption>
-                    DataTable.js Demo
-                </caption> -->
+            <div class="filter-container">
+                <button class="filter-btn active" data-filter="pendientes">Pendientes</button>
+                <button class="filter-btn" data-filter="abonadas">Abonadas</button>
+            </div>
+            <table id="datatable_auditoria" class="table table-striped">
+
                 <thead>
                     <tr>
-                        <th class="centered"></th>
-                        <th class="centered">Nombre</th>
-                        <th class="centered">Correo</th>
-                        <th class="centered">Cedula</th>
-                        <th class="centered">Fecha solicitud</th>
-                        <th class="centered">Telefono</th>
-                        <th class="centered">Tipo de cuenta</th>
-                        <th class="centered">Banco</th>
-                        <th class="centered">Numero de cuenta</th>
-                        <th class="centered">Cantidad</th>
-                        <th class="centered">Acciones</th>
+                        <th></th>
+                        <th class="centered">Factura</th>
+                        <th class="centered">Detalle factura</th>
+                        <th class="centered">Estado Guia</th>
+                        <th class="centered">Tienda</th>
+                        <th class="centered">Venta total</th>
+                        <th class="centered">Costo</th>
+                        <th class="centered">Precio envio</th>
+                        <th class="centered">Fulfillment</th>
+                        <th class="centered">Monto a recibir</th>
+                        <th class="centered">Monto pendiente</th>
+                        <th class="centered">Peso</th>
+                        <th class="centered">Guia</th>
+                        <th class="centered">Editar</th>
+                        <th class="centered">Devolucion</th>
+                        <th class="centered">Tipo envio</th>
+                        <th class="centered">Ganancia</th>
+                        <th class="centered">Eliminar</th>
                     </tr>
                 </thead>
-                <tbody id="tableBody_solicitudes"></tbody>
+                <tbody id="tableBody_auditoria"></tbody>
             </table>
         </div>
     </div>
 </div>
 
-<script src="<?php echo SERVERURL ?>/Views/Wallet/js/solicitudes.js"></script>
+<script src="<?php echo SERVERURL ?>/Views/Wallet/js/audtoria_guias.js"></script>
 <?php require_once './Views/templates/footer.php'; ?>
