6a6af0ffb0678a83d3410df626f57b4dc0d9e0c7
User <User@DESKTOP-9FVJ3SL>
2024-07-17 16:02:37 -0500

    producto

--- Views/Wallet/auditoria_guias.php
+++ Views/Wallet/auditoria_guias.php
@@ -1,8 +1,39 @@
-<?php
+<?php require_once './Views/templates/header.php'; ?>
+<?php require_once './Views/Pedidos/css/guias_style.php'; ?>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+<?php require_once './Views/Pedidos/Modales/informacion_plataforma.php'; ?>
+
+<div class="custom-container-fluid">
+    <div class="container mt-5" style="max-width: 1600px;">
+        <h2 class="text-center mb-4">Guias</h2>
+
+        <div class="table-responsive">
+            <!-- <table class="table table-bordered table-striped table-hover"> -->
+            <table id="datatable_solicitudes" class="table table-striped">
+                <!-- <caption>
+                    DataTable.js Demo
+                </caption> -->
+                <thead>
+                    <tr>
+                        <th class="centered"></th>
+                        <th class="centered">Nombre</th>
+                        <th class="centered">Correo</th>
+                        <th class="centered">Cedula</th>
+                        <th class="centered">Fecha solicitud</th>
+                        <th class="centered">Telefono</th>
+                        <th class="centered">Tipo de cuenta</th>
+                        <th class="centered">Banco</th>
+                        <th class="centered">Numero de cuenta</th>
+                        <th class="centered">Cantidad</th>
+                        <th class="centered">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody id="tableBody_solicitudes"></tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<script src="<?php echo SERVERURL ?>/Views/Wallet/js/solicitudes.js"></script>
+<?php require_once './Views/templates/footer.php'; ?>
