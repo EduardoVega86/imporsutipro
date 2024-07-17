d8dfb18b7e9f6148270378bc9e15b70fe5e5e11b
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-17 14:06:20 -0500

    agregar_index

--- Views/Wallet/js/detalle.js
+++ Views/Wallet/js/detalle.js
@@ -50,7 +50,7 @@ const listDetalleWallet = async () => {
 
       content += `
                 <tr>
-                    <td><a class="dropdown-item link-like" href="${SERVERURL}wallet/pagar?tienda=${detalleWallet.id_plataforma}">${detalleWallet.tienda}</a></td>
+                    <td><a class="dropdown-item link-like" href="${SERVERURL}wallet/pagar?id_plataforma=${detalleWallet.id_plataforma}">${detalleWallet.tienda}</a></td>
                     <td>${detalleWallet.ventas}</td>
                     <td>${detalleWallet.utilidad}</td>
                     <td>${detalleWallet.count_visto_0}</td>
