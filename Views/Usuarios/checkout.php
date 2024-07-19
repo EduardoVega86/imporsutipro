1a4f7901951b16014945dc8efb734375af07f840
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-19 11:10:54 -0500

    agregar_index

--- Views/Usuarios/checkout.php
+++ Views/Usuarios/checkout.php
@@ -1,1 +1,7 @@
-hola
+<?php require_once './Views/templates/header.php'; ?>
+<?php require_once './Views/Usuarios/css/checkout_style.php'; ?>
+ 
+<div class="custom-container-fluid mt-4">
+
+</div>
+<?php require_once './Views/templates/footer.php'; ?>
---
+++ Views/Usuarios/css/checkout.php
@@ -0,0 +1,235 @@
+<style>
+    .left-column {
+        width: 50%;
+        padding: 20px;
+        padding-top: 60px;
+        position: -webkit-sticky;
+        /* Para compatibilidad con Safari */
+        position: sticky;
+        top: 0;
+        /* Ajusta esto a la altura de cualquier cabecera o menú que tengas */
+        height: 100%;
+        /* O la altura que quieras que tenga */
+    }
+
+    .right-column {
+        width: 50%;
+        padding: 20px;
+        padding-top: 60px;
+    }
+
+    /* Seccion Hidden */
+    .list-group-item {
+        display: flex;
+        flex-direction: column;
+        /* Asegura que el contenido fluya de arriba hacia abajo */
+    }
+
+    .edit-section {
+        width: 100%;
+        /* Ocupa todo el ancho disponible */
+        /* Otros estilos que desees aplicar */
+    }
+
+    .hidden {
+        display: none;
+        /* Oculta la sección */
+    }
+
+    /* Este estilo se aplica cuando se muestra la sección */
+    .edit-section:not(.hidden) {
+        display: block;
+        /* O 'flex' si necesitas más control sobre el contenido interior */
+    }
+
+    .caja_transparente {
+        border-radius: 0.5rem;
+        border: 1px solid #ccc;
+        padding: 10px;
+    }
+
+    .caja_variable {
+        padding-top: 10px;
+        padding-right: 10px !important;
+        padding-left: 10px !important;
+        border-radius: 0.5rem;
+        background-color: #dedbdb;
+    }
+
+    .caja_oferta {
+        padding: 10px;
+        border-radius: 0.5rem;
+        background-color: rgba(0, 164, 251, 0.5);
+        /* 50% de opacidad */
+    }
+
+    .discount-code-container {
+        max-width: 300px;
+        /* O el ancho que prefieras */
+        padding-top: 10px;
+    }
+
+    .applied-discount {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-top: 10px;
+        padding: 5px 10px;
+        background: #f2f2f2;
+        /* Fondo gris claro para destacar */
+        border-radius: 5px;
+    }
+
+    .discount-tag {
+        font-weight: bold;
+    }
+
+    .close {
+        font-size: 20px;
+        color: #000;
+        opacity: 0.6;
+    }
+
+    .close:hover {
+        opacity: 1;
+    }
+
+    .sub_titulos {
+        font-size: 17px;
+        font-weight: 700;
+    }
+
+    hr {
+        border: none;
+        /* Quita el borde predeterminado */
+        height: 2px;
+        /* Ajusta el grosor de la línea */
+        background-color: #000;
+        /* Ajusta el color de la línea */
+        margin: 15px 0;
+        /* Ajusta el espaciado vertical de la línea */
+    }
+
+    .input-group-text {
+        background: transparent;
+        padding-right: 0;
+        /* Remover el espacio a la derecha del ícono si es necesario */
+    }
+
+    .form-group .input-group .form-control {
+        border: 1px solid #ced4da;
+        /* Ajusta al color de borde deseado */
+        border-left: none;
+        /* Remueve el borde izquierdo donde se unen el ícono y el input */
+    }
+
+    /* Ajusta el tamaño y el color del icono según sea necesario */
+    .bx {
+        font-size: 1.5rem;
+        /* Tamaño del icono */
+        color: #757575;
+        /* Color del icono */
+    }
+
+    .icon-btn.active i {
+        color: white;
+        /* O puedes usar #FFFFFF */
+    }
+
+    .form-group {
+        margin: 0 !important;
+    }
+
+    .btn_comprar {
+        border-radius: 0.5rem;
+        padding: 10px;
+    }
+
+    /* CSS para tachar codigo de descuentro*/
+    #codigosDescuento_temporal .d-flex {
+        text-decoration: line-through;
+        color: red;
+        /* Cambia el color del texto a rojo */
+    }
+
+    #codigosDescuento_temporal button {
+        pointer-events: none;
+        /* Desactiva los eventos del ratón, haciendo los botones no clickeables */
+        opacity: 0.5;
+        /* Cambia la opacidad para mostrar que están desactivados */
+    }
+
+    /* animaciones del boton comprar */
+    /* Animación Bounce */
+    .bounce {
+        animation: bounce 1.2s ease-in-out infinite;
+    }
+
+    @keyframes bounce {
+
+        0%,
+        100% {
+            transform: translateY(0);
+        }
+
+        50% {
+            transform: translateY(-10px);
+        }
+    }
+
+    /* Animación Shake */
+    .shake {
+        animation: shake 6s linear infinite;
+    }
+
+    @keyframes shake {
+
+        0%,
+        100% {
+            transform: translateX(0);
+        }
+
+        10%,
+        20%,
+        30%,
+        40%,
+        50%,
+        60%,
+        70%,
+        80%,
+        90% {
+            transform: translateX(5px);
+        }
+
+        15%,
+        25%,
+        35%,
+        45%,
+        55%,
+        65%,
+        75%,
+        85%,
+        95% {
+            transform: translateX(-5px);
+        }
+    }
+
+    /* Animación Pulse */
+    .pulse {
+        animation: pulse 1s ease-in-out infinite;
+    }
+
+    @keyframes pulse {
+        0% {
+            transform: scale(1);
+        }
+
+        50% {
+            transform: scale(1.1);
+        }
+
+        100% {
+            transform: scale(1);
+        }
+    }
+</style>
