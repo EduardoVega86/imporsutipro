81cb82649785dfb89a6c6207707fdfd6acd3abf6
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-23 13:51:36 -0500

    agregar_index

--- Views/Usuarios/tienda_online.php
+++ Views/Usuarios/tienda_online.php
@@ -101,7 +101,7 @@
                                         <div class="mb-3">
                                             <input type="file" class="form-control" id="imageInputFav" accept=".jpg,.jpeg,.png" name="imagen">
                                         </div>
-                                        <img id="imagePreviewFav" class="image-preview mb-3" src="" alt="Preview" width="200px">
+                                        <img id="imagePreviewFav" class="image-preview mb-3" src="" alt="Preview" width="50%">
                                     </form>
                                 </div>
                             </div>
