59ef8c975772c32f824824388f751a8988489aab
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-23 11:47:13 -0500

    agregar_index

--- Models/UsuariosModel.php
+++ Models/UsuariosModel.php
@@ -222,7 +222,7 @@ class UsuariosModel extends Query
     public function guardar_imagen_favicon($imagen, $plataforma)
     {
         $response = $this->initialResponse();
-        $target_dir = "public/img/favicon/";
+        $target_dir = "public/img/favicon_tienda/";
         $target_file = $target_dir . basename($imagen["name"]);
         $uploadOk = 1;
         $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
