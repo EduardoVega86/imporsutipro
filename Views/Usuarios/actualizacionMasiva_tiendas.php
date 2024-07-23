853412899273d440109e2c0d4e53e4ab75dd0ae1
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-23 17:07:33 -0500

    agregar_index

---
+++ Views/Usuarios/actualizacionMasiva_tiendas.php
@@ -0,0 +1,22 @@
+<?php require_once './Views/templates/header.php'; ?>
+<?php require_once './Views/Despacho/css/actualizacionMasiva_tiendas_style.php'; ?>
+
+<div class="full-screen-container">
+    <div class="custom-container-fluid mt-4" style="margin-right: 20px;">
+        <h1>Despacho de guías</h1>
+        <div class="form-group">
+            <label for="numeroGuia">Número de Guía</label>
+            <input type="text" id="numeroGuia" placeholder="Coloca el cursor aquí antes de">
+        </div>
+        <button id="despachoBtn" class="btn btn-success">Despacho</button>
+    </div>
+    <div class="guides-list-container mt-4" style="margin-right: auto; margin-left: 30px;">
+        <h2>Guías Ingresadas</h2>
+        <ul id="guidesList" class="list-group"></ul>
+        <div style="padding-top:10px;">
+            <button id="generarImpresionBtn" class="btn btn-success">Generar Impresion</button>
+        </div>
+    </div>
+</div>
+
+<?php require_once './Views/templates/footer.php'; ?>
---
+++ Views/Usuarios/css/actualizacionMasiva_tiendas_style.php
@@ -0,0 +1,72 @@
+<style>
+    .full-screen-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background-color: #f0f0f0;
+        padding-left: 30px;
+    }
+
+    .custom-container-fluid {
+        background-color: <?php echo COLOR_FONDO; ?>;
+        padding: 20px;
+        border-radius: 8px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        text-align: center;
+    }
+
+    .custom-container-fluid h1 {
+        color: <?php echo COLOR_LETRAS;?>;
+        margin-bottom: 20px;
+    }
+
+    .form-group {
+        margin-bottom: 15px;
+    }
+
+    .form-group label {
+        display: block;
+        color: <?php echo COLOR_LETRAS;?>;
+        margin-bottom: 5px;
+    }
+
+    .form-group input {
+        width: 100%;
+        padding: 10px;
+        border: 1px solid #ced4da;
+        border-radius: 4px;
+    }
+
+    .form-group input::placeholder {
+        color: #6c757d;
+    }
+
+    .btn {
+        background-color: #198754;
+        color: black;
+        border: none;
+        padding: 10px 20px;
+        border-radius: 4px;
+        cursor: pointer;
+    }
+
+    @media (max-width: 768px) {
+        .full-screen-container {
+            flex-direction: column;
+            gap: 2px;
+            height: 60vh;
+        }
+    }
+
+    .delete-btn {
+        margin-left: 10px;
+        color: red;
+        cursor: pointer;
+        transition: transform 0.3s ease;
+    }
+
+    .delete-btn:hover {
+        transform: scale(1.2);
+    }
+</style>
