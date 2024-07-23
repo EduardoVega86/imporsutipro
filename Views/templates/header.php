26f843dc7d9871725fafdb0589bdd7e7f556dc26
Jeimy Jara <jeimyjara@hotmail.com>
2024-07-23 16:17:28 -0500

    XD

--- Views/templates/header.php
+++ Views/templates/header.php
@@ -40,6 +40,7 @@
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script src="https://kit.fontawesome.com/0022adc953.js" crossorigin="anonymous"></script>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.18/summernote-bs4.min.css" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.18/summernote-bs4.min.js"></script>
 
 <?php require_once './Views/templates/css/header_style.php'; ?>
 </head>
