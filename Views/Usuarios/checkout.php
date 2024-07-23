40ddc6819a41dd8f3c2a4f41c4ac3468533a4221
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-23 10:04:42 -0500

    agregar_index

--- Views/Usuarios/checkout.php
+++ Views/Usuarios/checkout.php
@@ -659,7 +659,6 @@
                 console.error("Error en la solicitud AJAX:", error);
                 console.log("Estado:", status);
                 console.log("XHR:", xhr);
-                alert("Hubo un problema al obtener la información de la categoría");
             }
         });
     });
