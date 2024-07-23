5a4d09f83ad36255659561e23396986475e765e2
Jeimy Jara <jeimyjara@hotmail.com>
2024-07-23 15:46:39 -0500

    XD

--- Models/ReferidosModel.php
+++ Models/ReferidosModel.php
@@ -15,6 +15,8 @@ class ReferidosModel extends Query
     }
 
 
+
+
     public function crearReferido($id)
     {
         $sql = "UPDATE plataformas set token_referido = ? WHERE id_plataforma = ?";
