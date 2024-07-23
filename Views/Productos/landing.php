b685dcc4999a2276f348d6f133956705ce3dc8f8
Jeimy Jara <jeimyjara@hotmail.com>
2024-07-23 16:09:17 -0500

    XD

--- Views/Productos/landing.php
+++ Views/Productos/landing.php
@@ -1,4 +1,5 @@
 <?php require_once './Views/templates/header.php'; ?>
+<?php require_once './Views/Productos/css/landing.php'; ?>
 <?php
 
 if ($data == 0) {
@@ -19,11 +20,33 @@ if ($data == 0) {
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1 class="text-center">Landing</h1>
+                <h3 class="text-center">Landing</h3>
+                <div class="editor-container">
+                    <textarea id="text-editor"></textarea>
+                    <button class="accept-btn" id="accept-btn">Aceptar</button>
+                    <div class="html-output" id="html-output"></div>
+                </div>
             </div>
         </div>
     <?php
 }
     ?>
+    <script>
+        tinymce.init({
+            selector: '#text-editor',
+            menubar: false,
+            plugins: [
+                'advlist autolink lists link image charmap print preview anchor',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime media table paste code help wordcount'
+            ],
+            toolbar: 'undo redo | formatselect | image | media | link | code | table | bold italic backcolor | \
+                              alignleft aligncenter alignright alignjustify | \
+                              bullist numlist outdent indent | removeformat | help'
+        });
 
-    <?php require_once './Views/templates/footer.php'; ?>
+        document.getElementById('accept-btn').addEventListener('click', () => {
+            const editorContent = tinymce.get('text-editor').getContent();
+            document.getElementById('html-output').innerHTML = editorContent;
+        });
+        <?php require_once './Views/templates/footer.php'; ?>
