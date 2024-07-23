6ae95f8f6a721b35d77692887330fb205dbef7bb
Jeimy Jara <jeimyjara@hotmail.com>
2024-07-23 10:09:17 -0500

    XD

--- Models/ShopifyModel.php
+++ Models/ShopifyModel.php
@@ -147,6 +147,9 @@ class ShopifyModel extends Query
 
         $contiene = trim($contiene); // Eliminar el espacio extra al final
 
+        //si tiene emojis o caracteres especiales elimina los emojis
+        $contiene = preg_replace('/[^\x20-\x7E]/', '', $contiene);
+
         // Aquí se pueden continuar los procesos necesarios para la orden
         ///iniciar curl
         $ch = curl_init();
