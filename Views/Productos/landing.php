16ecd7fa5ec106fc5809b9ff801fdb9f83a62efb
Jeimy Jara <jeimyjara@hotmail.com>
2024-07-25 17:26:37 -0500

    XD

--- Views/Productos/landing.php
+++ Views/Productos/landing.php
@@ -1,5 +1,9 @@
 <?php require_once './Views/templates/header.php'; ?>
-
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.18/summernote.min.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.18/summernote.min.js"></script>
 <?php require_once './Views/Productos/css/landing.php'; ?>
 <?php
 
