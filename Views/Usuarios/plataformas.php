2aa4842171e8e5701aaac9cfda6f4e180a585631
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-25 17:02:26 -0500

    agregar_index

--- Views/Usuarios/plataformas.php
+++ Views/Usuarios/plataformas.php
@@ -1,7 +1,6 @@
 <?php require_once './Views/templates/header.php'; ?>
-<?php require_once './Views/Usuarios/css/listamatriz_style.php'; ?>
+<?php require_once './Views/Usuarios/css/plataformas_style.php'; ?>
 
-<?php require_once './Views/Usuarios/Modales/cambiarClave_usuario.php'; ?>
 <div class="custom-container-fluid">
     <div class="container mt-5" style="max-width: 1600px;">
         <h2 class="text-center mb-4">Lista de Usuarios Matriz</h2>
@@ -9,7 +8,7 @@
         <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#agregar_productoModal"><i class="fas fa-plus"></i> Agregar</button>
         <div class="table-responsive">
             <!-- <table class="table table-bordered table-striped table-hover"> -->
-            <table id="datatable_lista_usuarioMatriz" class="table table-striped">
+            <table id="datatable_plataformas" class="table table-striped">
                 <thead>
                     <tr>
                         <th class="centered">ID</th>
@@ -21,10 +20,10 @@
                         <th class="centered">Ocultar</th>
                     </tr>
                 </thead>
-                <tbody id="tableBody_lista_usuarioMatriz"></tbody>
+                <tbody id="tableBody_plataformas"></tbody>
             </table>
         </div>
     </div>
 </div>
-<script src="<?php echo SERVERURL ?>/Views/Usuarios/js/listamatriz.js"></script>
+<script src="<?php echo SERVERURL ?>/Views/Usuarios/js/plataformas.js"></script>
 <?php require_once './Views/templates/footer.php'; ?>
