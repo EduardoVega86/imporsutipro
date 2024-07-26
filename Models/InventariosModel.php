3a6980419a70b879bbdfacb80f78596586850c8b
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-26 09:25:25 -0500

    agregar_index

--- Models/InventariosModel.php
+++ Models/InventariosModel.php
@@ -222,7 +222,7 @@ class InventariosModel extends Query
         if ($uploadOk == 0) {
             $response['status'] = 500;
             $response['title'] = 'Error';
-            $response['message'] = 'Error al subir la imagen';
+            $response['message'] = 'Error al subir la imagen1';
         } else {
             if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
                 $response['status'] = 200;
@@ -240,12 +240,12 @@ class InventariosModel extends Query
                 } else {
                     $response['status'] = 500;
                     $response['title'] = 'Error';
-                    $response['message'] = 'Error al subir la imagen';
+                    $response['message'] = 'Error al subir la imagen2';
                 }
             } else {
                 $response['status'] = 500;
                 $response['title'] = 'Error';
-                $response['message'] = 'Error al subir la imagen';
+                $response['message'] = 'Error al subir la imagen3';
             }
         }
         return $response;
