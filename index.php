48fc08148ad5c2384b171e50f4ec7a48c5e4752e
Jeimy Jara <jeimyjara@hotmail.com>
2024-07-26 10:14:38 -0500

    XD

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: * ");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 require_once 'Config/Config.php';
