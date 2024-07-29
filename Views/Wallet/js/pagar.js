b5c0f49b4f17623ed98b13f489a477b71c976990
Jeimy Jara <jeimyjara@hotmail.com>
2024-07-29 09:41:49 -0500

    XD

--- Views/Wallet/js/pagar.js
+++ Views/Wallet/js/pagar.js
@@ -390,43 +390,45 @@ function eliminar_wallet(id_cabecera) {
 function ver_detalle_cot(numero_factura) {
   let formData = new FormData();
   formData.append("numero_factura", numero_factura);
+  if (numero_factura.contains("-F")) {
+    console.log("XD");
+  } else {
+    $.ajax({
+      url: SERVERURL + "Pedidos/obtenerDetalleWallet",
+      type: "POST",
+      data: formData,
+      processData: false, // No procesar los datos
+      contentType: false, // No establecer ningún tipo de contenido
+      success: function (response) {
+        response = JSON.parse(response);
 
-  $.ajax({
-    url: SERVERURL + "Pedidos/obtenerDetalleWallet",
-    type: "POST",
-    data: formData,
-    processData: false, // No procesar los datos
-    contentType: false, // No establecer ningún tipo de contenido
-    success: function (response) {
-      response = JSON.parse(response);
+        // Mostrar los detalles principales de la primera factura
+        $("#ordePara_detalleFac").text(response[0].nombre);
+        $("#direccion_detalleFac").text(
+          `${response[0].c_principal},${response[0].c_secundaria}`
+        );
+        $("#telefono_detalleFac").text(response[0].telefono);
+        $("#numOrden_detalleFac").text(response[0].numero_factura);
+        $("#fecha_detalleFac").text(response[0].fecha_factura);
+        $("#companiaEnvio_detalleFac").text(response[0].transporte);
+        if (response[0].cod == 1) {
+          $("#tipoEnvio_detalleFac").text("Con Recaudo");
+        } else {
+          $("#tipoEnvio_detalleFac").text("Sin Recaudo");
+        }
 
-      // Mostrar los detalles principales de la primera factura
-      $("#ordePara_detalleFac").text(response[0].nombre);
-      $("#direccion_detalleFac").text(
-        `${response[0].c_principal},${response[0].c_secundaria}`
-      );
-      $("#telefono_detalleFac").text(response[0].telefono);
-      $("#numOrden_detalleFac").text(response[0].numero_factura);
-      $("#fecha_detalleFac").text(response[0].fecha_factura);
-      $("#companiaEnvio_detalleFac").text(response[0].transporte);
-      if (response[0].cod == 1) {
-        $("#tipoEnvio_detalleFac").text("Con Recaudo");
-      } else {
-        $("#tipoEnvio_detalleFac").text("Sin Recaudo");
-      }
+        // Verificar si la respuesta tiene elementos y llenar la tabla
+        if (response.length > 0) {
+          let tableBody = $("#tabla_body");
+          tableBody.empty(); // Limpiar cualquier contenido previo
 
-      // Verificar si la respuesta tiene elementos y llenar la tabla
-      if (response.length > 0) {
-        let tableBody = $("#tabla_body");
-        tableBody.empty(); // Limpiar cualquier contenido previo
+          let total = 0; // Variable para calcular el total
 
-        let total = 0; // Variable para calcular el total
+          response.forEach(function (detalle) {
+            let subtotal = detalle.cantidad * detalle.precio_venta;
+            total += subtotal;
 
-        response.forEach(function (detalle) {
-          let subtotal = detalle.cantidad * detalle.precio_venta;
-          total += subtotal;
-
-          let rowHtml = `
+            let rowHtml = `
             <tr>
               <td>${detalle.nombre_producto}</td>
               <td>${detalle.cantidad}</td>
@@ -434,25 +436,26 @@ function ver_detalle_cot(numero_factura) {
               <td>${subtotal.toFixed(2)}</td>
             </tr>
           `;
-          tableBody.append(rowHtml);
-        });
+            tableBody.append(rowHtml);
+          });
 
-        // Agregar la fila del total
-        let totalRowHtml = `
+          // Agregar la fila del total
+          let totalRowHtml = `
           <tr class="custom-total-row">
             <td colspan="3" class="text-right">Total</td>
             <td>${total.toFixed(2)}</td>
           </tr>
         `;
-        tableBody.append(totalRowHtml);
-      }
+          tableBody.append(totalRowHtml);
+        }
 
-      $("#detalles_facturaModal").modal("show");
-    },
-    error: function (error) {
-      console.error("Error al obtener la lista de bodegas:", error);
-    },
-  });
+        $("#detalles_facturaModal").modal("show");
+      },
+      error: function (error) {
+        console.error("Error al obtener la lista de bodegas:", error);
+      },
+    });
+  }
 }
 
 function procesarPlataforma(url) {
