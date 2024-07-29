73f48a9e95f794fcd26dbbdfb5e8a7c911529033
Jeimy Jara <jeimyjara@hotmail.com>
2024-07-29 15:57:58 -0500

    XD

--- Controllers/Integraciones.php
+++ Controllers/Integraciones.php
@@ -7,6 +7,9 @@ class Integraciones extends Controller
     public function __construct()
     {
         parent::__construct();
+        if (!$this->isAuth()) {
+            header("Location:  " . SERVERURL . "login");
+        }
     }
 
     public function index()
