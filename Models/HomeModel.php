4e17df80178df35e764345d0e950fff6eee21638
Jeimy Jara <jeimyjara@hotmail.com>
2024-07-30 09:08:32 -0500

    XD

--- Models/HomeModel.php
+++ Models/HomeModel.php
@@ -9,6 +9,9 @@ class HomeModel extends Query
 
     public function validarRefiere($id)
     {
+        if ($id == "") {
+            return false;
+        }
         $sql = "SELECT * FROM plataformas WHERE token_referido = ?";
         $params = [$id];
         $result = $this->simple_select($sql, $params);
