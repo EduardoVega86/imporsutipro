286888719b995f4a109e58d023bb4b478f88f0b6
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-30 10:25:21 -0500

    agregar_index

--- Views/Integraciones/js/integraciones.js
+++ Views/Integraciones/js/integraciones.js
@@ -53,74 +53,85 @@ function abrirmodal_tiktok() {
 }
 
 function estado_facebook() {
-  let formData = new FormData();
-  formData.append("tipo", 1); // Añadir el SKU al FormData
+  return new Promise((resolve, reject) => {
+    let formData = new FormData();
+    formData.append("tipo", 1); // Añadir el SKU al FormData
 
-  $.ajax({
-    url: SERVERURL + "tienda/obtenerPixel",
-    type: "POST", // Cambiar a POST para enviar FormData
-    data: formData,
-    processData: false, // No procesar los datos
-    contentType: false, // No establecer ningún tipo de contenido
-    dataType: "json",
-    success: function (response) {
-      if (response == 0) {
-        $("#conectado_facebook").val(0);
-      } else {
-        $("#conectado_facebook").val(1);
-      }
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-      alert(errorThrown);
-    },
+    $.ajax({
+      url: SERVERURL + "tienda/obtenerPixel",
+      type: "POST", // Cambiar a POST para enviar FormData
+      data: formData,
+      processData: false, // No procesar los datos
+      contentType: false, // No establecer ningún tipo de contenido
+      dataType: "json",
+      success: function (response) {
+        if (response == 0) {
+          $("#conectado_facebook").val(0);
+        } else {
+          $("#conectado_facebook").val(1);
+        }
+        resolve();
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        alert(errorThrown);
+        reject(errorThrown);
+      },
+    });
   });
 }
 
 function estado_tiktok() {
-  let formData = new FormData();
-  formData.append("tipo", 2); // Añadir el SKU al FormData
+  return new Promise((resolve, reject) => {
+    let formData = new FormData();
+    formData.append("tipo", 2); // Añadir el SKU al FormData
 
-  $.ajax({
-    url: SERVERURL + "tienda/obtenerPixel",
-    type: "POST", // Cambiar a POST para enviar FormData
-    data: formData,
-    processData: false, // No procesar los datos
-    contentType: false, // No establecer ningún tipo de contenido
-    success: function (response) {
-      if (response == 0) {
-        $("#conectado_tiktok").val(0);
-      } else {
-        $("#conectado_tiktok").val(1);
-      }
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-      alert(errorThrown);
-    },
+    $.ajax({
+      url: SERVERURL + "tienda/obtenerPixel",
+      type: "POST", // Cambiar a POST para enviar FormData
+      data: formData,
+      processData: false, // No procesar los datos
+      contentType: false, // No establecer ningún tipo de contenido
+      success: function (response) {
+        if (response == 0) {
+          $("#conectado_tiktok").val(0);
+        } else {
+          $("#conectado_tiktok").val(1);
+        }
+        resolve();
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        alert(errorThrown);
+        reject(errorThrown);
+      },
+    });
   });
 }
 
 $(document).ready(function () {
-  estado_facebook();
-  estado_tiktok();
-  
-  var estado_conectado_facebook = $('#conectado_facebook').val();
-  var estado_conectado_tiktok = $('#conectado_tiktok').val();
+  Promise.all([estado_facebook(), estado_tiktok()])
+    .then(() => {
+      var estado_conectado_facebook = $("#conectado_facebook").val();
+      var estado_conectado_tiktok = $("#conectado_tiktok").val();
 
-  //facebook
-  console.log("estado: "+estado_conectado_facebook);
-  if (estado_conectado_facebook == 0) {
-    $("#desconectarFacebook").show();
-    $("#conectarFacebook").hide();
-  } else {
-    $("#conectarFacebook").show();
-    $("#desconectarFacebook").hide();
-  }
-  //tiktok
-  if (estado_conectado_tiktok == 0) {
-    $("#desconectarTiktok").show();
-    $("#conectarTiktok").hide();
-  } else {
-    $("#conectarTiktok").show();
-    $("#desconectarTiktok").hide();
-  }
+      //facebook
+      console.log("estado: " + estado_conectado_facebook);
+      if (estado_conectado_facebook == 0) {
+        $("#desconectarFacebook").show();
+        $("#conectarFacebook").hide();
+      } else {
+        $("#conectarFacebook").show();
+        $("#desconectarFacebook").hide();
+      }
+      //tiktok
+      if (estado_conectado_tiktok == 0) {
+        $("#desconectarTiktok").show();
+        $("#conectarTiktok").hide();
+      } else {
+        $("#conectarTiktok").show();
+        $("#desconectarTiktok").hide();
+      }
+    })
+    .catch((error) => {
+      console.error("Error al obtener el estado de las integraciones:", error);
+    });
 });
