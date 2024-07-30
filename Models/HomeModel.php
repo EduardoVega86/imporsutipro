e4cb018141a08383db43b049acce672af50ccbbd
Jeimy Jara <jeimyjara@hotmail.com>
2024-07-30 09:08:00 -0500

    XD

--- Models/HomeModel.php
+++ Models/HomeModel.php
@@ -12,6 +12,7 @@ class HomeModel extends Query
         $sql = "SELECT * FROM plataformas WHERE token_referido = ?";
         $params = [$id];
         $result = $this->simple_select($sql, $params);
+        echo $result;
         if ($result > 0) {
             return true;
         }
