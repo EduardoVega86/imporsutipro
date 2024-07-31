86a14a1d758aaa547ab862da029415e05e77a70f
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-31 10:21:02 -0500

    agregar_index

--- Views/Wallet/js/solicitudes.js
+++ Views/Wallet/js/solicitudes.js
@@ -165,7 +165,7 @@ const dataTableOtrasFormasPagoOptions = {
       title: "Panel de Control: Productos",
       titleAttr: "Exportar a CSV",
       exportOptions: {
-        columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        columns: [0, 1, 2, 3, 4, 5, 6],
       },
       filename: "Productos" + "_" + getFecha(),
       footer: true,
