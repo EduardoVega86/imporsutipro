f1da7e64c9222387075abb436a15e02ae6aa1091
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-08-02 15:54:29 -0500

    agregar_index

--- Views/Shopify/js/constructor_vista.js
+++ Views/Shopify/js/constructor_vista.js
@@ -1,69 +1,67 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const apiURL = SERVERURL + "/shopify/obtenerConfiguracion";
-
-  async function fetchData() {
-    try {
-      const response = await fetch(apiURL);
-      const data = await response.json();
-      displayData(data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  }
-
-  function displayData(data) {
-    const container = document.querySelector(".datos_shopify");
-    container.innerHTML = "";
-
-    data.forEach((config) => {
-      const configDiv = document.createElement("div");
-      configDiv.classList.add("config-item");
-
-      const configHtml = generateHtmlFromJson(config);
-      configDiv.innerHTML = configHtml;
-      container.appendChild(configDiv);
-    });
-  }
-
-  function generateHtmlFromJson(obj, level = 0) {
-    let html = "<ul>";
-
-    for (let key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        const value = obj[key];
-        const formattedKey = `<strong>${key}</strong>`;
-
-        if (typeof value === "object" && value !== null) {
-          html += `<li>${formattedKey}:<ul class="nested">${generateHtmlFromJson(
-            value,
-            level + 1
-          )}</ul></li>`;
-        } else if (typeof value === "string" && value.includes("/")) {
-          html += `<li>${formattedKey}: ${formatComplexString(
-            value,
-            level
-          )}</li>`;
-        } else {
-          html += `<li>${formattedKey}: <span>${value}</span></li>`;
-        }
+    const apiURL = SERVERURL + "/shopify/obtenerConfiguracion";
+  
+    async function fetchData() {
+      try {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        displayData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
       }
     }
-
-    html += "</ul>";
-    return html;
-  }
-
-  function formatComplexString(value, level) {
-    const parts = value.split("/");
-    let html = '<ul class="nested">';
-
-    parts.forEach((part) => {
-      html += `<li style="padding-left: 5px;"><strong>${part}</strong></li>`;
-    });
-
-    html += "</ul>";
-    return html;
-  }
-
-  fetchData();
-});
+  
+    function displayData(data) {
+      const container = document.querySelector(".datos_shopify");
+      container.innerHTML = "";
+  
+      data.forEach((config) => {
+        const configDiv = document.createElement("div");
+        configDiv.classList.add("config-item");
+  
+        const configHtml = generateHtmlFromJson(config);
+        configDiv.innerHTML = configHtml;
+        container.appendChild(configDiv);
+      });
+    }
+  
+    function generateHtmlFromJson(obj, level = 0) {
+      let html = "<ul>";
+  
+      for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          const value = obj[key];
+          const formattedKey = `<strong>${key}</strong>`;
+  
+          if (typeof value === "object" && value !== null) {
+            html += `<li>${formattedKey}:<ul class="nested">${generateHtmlFromJson(
+              value,
+              level + 1
+            )}</ul></li>`;
+          } else if (typeof value === "string" && value.includes("/")) {
+            html += `<li>${formattedKey}: ${formatComplexString(value, level)}</li>`;
+          } else {
+            html += `<li>${formattedKey}: <span>${value}</span></li>`;
+          }
+        }
+      }
+  
+      html += "</ul>";
+      return html;
+    }
+  
+    function formatComplexString(value, level) {
+      const parts = value.split("/");
+      let html = '<ul class="nested">';
+  
+      parts.forEach((part) => {
+        html += `<li style="padding-left: 5px;"><strong>${part}</strong></li>`;
+      });
+  
+      html += "</ul>";
+      return html;
+    }
+  
+    fetchData();
+  });
+  
