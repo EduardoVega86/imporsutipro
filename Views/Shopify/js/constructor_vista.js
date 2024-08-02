94a254c942e7f64e8c284c96a95cc008a713a7dd
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-08-02 15:56:19 -0500

    agregar_index

--- Views/Shopify/js/constructor_vista.js
+++ Views/Shopify/js/constructor_vista.js
@@ -55,7 +55,7 @@ document.addEventListener("DOMContentLoaded", function () {
       let html = '<ul class="nested">';
   
       parts.forEach((part) => {
-        html += `<li style="padding-left: 5px;"><strong>${part}</strong></li>`;
+        html += `<li style="padding-left: 5px;">${part}</li>`;
       });
   
       html += "</ul>";
