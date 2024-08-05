41f511526c0f78c9df7a8c0416e1c127ccabff50
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-08-05 15:21:59 -0500

    agregar_index

--- Views/Productos/gestion_privados.php
+++ Views/Productos/gestion_privados.php
@@ -23,24 +23,30 @@
                 </table>
             </div>
             <div class="right gap-2 hidden" id="inventarioSection">
-                <div class="card" style="height: 550px; padding:10px">
+                <div class="card" style="height: 550px; padding: 10px;">
                     <div class="form-group" id="tiendas-field">
                         <input type="hidden" id="id_producto" name="id_producto">
                         <label for="tiendas">Tiendas:</label>
                         <select class="form-select" id="select_tiendas">
-                            <option value="0" selected> Selecciona tiendas</option>
+                            <option value="0" selected>Selecciona tiendas</option>
                         </select>
                     </div>
-                    <div id="informacion_tienda" style="display: none; max-width: 300px; margin: 0 auto; padding: 10px; border: 1px solid #ddd; border-radius: 5px;">
-                        <img src="tu-imagen.png" alt="Producto" id="image_tienda" style="width: 100%; padding-top: 10px; padding-bottom: 10px;">
-                        <h6 style="padding-top: 5px; text-align: center;"><strong><span id="nombre_tienda"></span></strong></h6>
-                        <hr>
-                        <div style="display: flex; flex-direction: column; gap: 10px;">
+
+                    <!-- Contenedor de Información de la Tienda -->
+                    <div id="informacion_tienda" class="card mt-3 p-3" style="display: none; max-width: 100%; border: 1px solid #ddd; border-radius: 5px;">
+                        <img src="tu-imagen.png" alt="Producto" id="image_tienda" class="img-fluid rounded" style="max-height: 200px; margin-bottom: 15px;">
+
+                        <h6 class="text-center mb-3"><strong><span id="nombre_tienda"></span></strong></h6>
+
+                        <hr class="mb-3">
+
+                        <div class="d-flex flex-column gap-2">
                             <label for="url"><strong>URL: </strong><span id="url"></span></label>
                             <label for="telefono"><strong>Teléfono: </strong><span id="telefono"></span></label>
                             <label for="correo"><strong>Correo: </strong><span id="correo"></span></label>
                         </div>
-                        <button class="btn btn-add" onclick="agregar_stock()" style="width: 100%; margin-top: 10px;">Agregar Stock</button>
+
+                        <button class="btn btn-primary btn-block mt-3" onclick="agregar_stock()">Agregar Stock</button>
                     </div>
                 </div>
                 <div class="table-responsive">
