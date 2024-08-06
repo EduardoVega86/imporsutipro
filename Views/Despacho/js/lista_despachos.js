60826fdcad4dd4543e5471068bd34c40d382ef9f
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-08-06 11:56:41 -0500

    agregar_index

--- Views/Despacho/js/lista_despachos.js
+++ Views/Despacho/js/lista_despachos.js
@@ -9,6 +9,33 @@ const dataTableListaDespachosOptions = {
   order: [[5, "desc"]], // Ordenar por la primera columna (fecha) en orden descendente
   pageLength: 10,
   destroy: true,
+  dom: '<"d-flex w-full justify-content-between"lBf><t><"d-flex justify-content-between"ip>',
+  buttons: [
+    {
+      extend: "excelHtml5",
+      text: 'Excel <i class="fa-solid fa-file-excel"></i>',
+      title: "Panel de Control: Usuarios",
+      titleAttr: "Exportar a Excel",
+      exportOptions: {
+        columns: [0, 1, 2, 3, 4, 5],
+      },
+      filename: "Productos" + "_" + getFecha(),
+      footer: true,
+      className: "btn-excel",
+    },
+    {
+      extend: "csvHtml5",
+      text: 'CSV <i class="fa-solid fa-file-csv"></i>',
+      title: "Panel de Control: Productos",
+      titleAttr: "Exportar a CSV",
+      exportOptions: {
+        columns: [0, 1, 2, 3, 4, 5],
+      },
+      filename: "Productos" + "_" + getFecha(),
+      footer: true,
+      className: "btn-csv",
+    },
+  ],
   language: {
     lengthMenu: "Mostrar _MENU_ registros por página",
     zeroRecords: "Ningún usuario encontrado",
@@ -25,6 +52,15 @@ const dataTableListaDespachosOptions = {
     },
   },
 };
+
+function getFecha() {
+  let fecha = new Date();
+  let mes = fecha.getMonth() + 1;
+  let dia = fecha.getDate();
+  let anio = fecha.getFullYear();
+  let fechaHoy = anio + "-" + mes + "-" + dia;
+  return fechaHoy;
+}
 
 const initDataTableListaDespachos = async () => {
   if (dataTableListaDespachosIsInitialized) {
--- Views/Despacho/js/lista_devoluciones.js
+++ Views/Despacho/js/lista_devoluciones.js
@@ -8,6 +8,33 @@ const dataTableListaDevolucionesOptions = {
   ],
   pageLength: 10,
   destroy: true,
+  dom: '<"d-flex w-full justify-content-between"lBf><t><"d-flex justify-content-between"ip>',
+  buttons: [
+    {
+      extend: "excelHtml5",
+      text: 'Excel <i class="fa-solid fa-file-excel"></i>',
+      title: "Panel de Control: Usuarios",
+      titleAttr: "Exportar a Excel",
+      exportOptions: {
+        columns: [0, 1, 2, 3, 4, 5],
+      },
+      filename: "Productos" + "_" + getFecha(),
+      footer: true,
+      className: "btn-excel",
+    },
+    {
+      extend: "csvHtml5",
+      text: 'CSV <i class="fa-solid fa-file-csv"></i>',
+      title: "Panel de Control: Productos",
+      titleAttr: "Exportar a CSV",
+      exportOptions: {
+        columns: [0, 1, 2, 3, 4, 5],
+      },
+      filename: "Productos" + "_" + getFecha(),
+      footer: true,
+      className: "btn-csv",
+    },
+  ],
   language: {
     lengthMenu: "Mostrar _MENU_ registros por página",
     zeroRecords: "Ningún usuario encontrado",
@@ -24,6 +51,14 @@ const dataTableListaDevolucionesOptions = {
     },
   },
 };
+function getFecha() {
+  let fecha = new Date();
+  let mes = fecha.getMonth() + 1;
+  let dia = fecha.getDate();
+  let anio = fecha.getFullYear();
+  let fechaHoy = anio + "-" + mes + "-" + dia;
+  return fechaHoy;
+}
 
 const initDataTableListaDevoluciones = async () => {
   if (dataTableListaDevolucionesIsInitialized) {
