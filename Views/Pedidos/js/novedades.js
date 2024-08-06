a3d80d5c6d497eea32e39c455143493f14798d34
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-08-06 15:20:13 -0500

    agregar_index

--- Views/Pedidos/js/novedades.js
+++ Views/Pedidos/js/novedades.js
@@ -332,7 +332,7 @@ let dataTableNovedadesGestionadasIsInitialized = false;
 
 const dataTableNovedadesGestionadasOptions = {
   columnDefs: [
-    { className: "centered", targets: [1, 2, 3, 4, 5] },
+    { className: "centered", targets: [0, 1, 2, 3] },
     { orderable: false, targets: 0 }, //ocultar para columna 0 el ordenar columna
   ],
   pageLength: 10,
