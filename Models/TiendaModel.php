04e1c4e6df3e28a3ad81b02091d25327c54c4ccb
Jeimy Jara <jeimyjara@hotmail.com>
2024-08-12 08:30:21 -0500

    XD

--- Models/TiendaModel.php
+++ Models/TiendaModel.php
@@ -36,7 +36,7 @@ class TiendaModel extends Query
         // Configuración de cPanel
         $cpanelUrl = 'https://administracion.imporsuitpro.com:2083/';
         $cpanelUsername = 'imporsuitpro';
-        $cpanelPassword = 'd.)~Y=}+*!2vVrm5';
+        $cpanelPassword = 'd.)~Y=}+*!2vVrm5s';
         $rootdomain = 'imporsuitpro.com';
 
         // El subdominio y su carpeta ya existen
