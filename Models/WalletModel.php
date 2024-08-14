a1ea35f5c4859002f0a32a3f5c42bcbcfe3d3a42
EduardoVega86 <edu.vega86@gmail.com>
2024-08-14 14:20:53 -0500

    auditoria

--- Models/WalletModel.php
+++ Models/WalletModel.php
@@ -1244,9 +1244,7 @@ ORDER BY
     
      public function guardarArchivo($fileTmpPath, $fileName, $id_transportadora) {
         // Definir la ruta donde se guardará el archivo
-         
-          $uploadDir = __DIR__ . '/transportadoras/';
-       //$uploadDir = 'transportadoras/';
+        $uploadDir = 'transportadoras/';
 
         // Obtener la extensión del archivo
         $fileExtension = pathinfo($fileName, PATHINFO_EXTENSION);
