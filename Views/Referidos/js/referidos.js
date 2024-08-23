23eea0057e4a745690b25c9a9627a92628af7f36
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-08-23 14:19:09 -0500

    agregar_index

--- Views/Referidos/js/referidos.js
+++ Views/Referidos/js/referidos.js
@@ -17,6 +17,12 @@ $(document).ready(function () {
       );
       $("#ganancias_referidos").text(parseFloat(response.saldo).toFixed(2));
 
+      if (response.saldo){
+        $("#boton_solicitar_pago").show();
+      }else{
+
+      } $("#boton_solicitar_pago").hide();
+
       $("#image_tienda").attr(
         "src",
         SERVERURL + "public/img/profile_wallet.png"
