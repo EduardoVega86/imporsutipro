c7f0eb29200f7590680b02cb18214c0b5ab99a15
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-08-27 15:12:05 -0500

    agregar_index

---
+++ Views/Dashboard/css/home_style.php
@@ -0,0 +1,26 @@
+<style>
+        .slider-container {
+            width: 100%;
+            max-width: 350px;
+            height: auto;
+        }
+
+        .card {
+            border: none;
+        }
+
+        .module-card img {
+            width: 100%;
+            height: auto;
+        }
+
+        .schedule-btn {
+            background-color: #f68736;
+            color: white;
+            border-radius: 5px;
+        }
+
+        .schedule-btn:hover {
+            background-color: #d67530;
+        }
+    </style>
