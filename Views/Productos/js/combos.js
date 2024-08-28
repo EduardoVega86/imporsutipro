7971bbbe699c5dd184bc34c859486357d7777e4d
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-08-28 10:48:58 -0500

    agregar_index

--- Views/Productos/js/combos.js
+++ Views/Productos/js/combos.js
@@ -100,7 +100,7 @@ let dataTableAsignacionProductoIsInitialized = false;
 
 const dataTableAsignacionProductoOptions = {
   columnDefs: [
-    { className: "centered", targets: [1, 2, 3, 4, 5] },
+    { className: "centered", targets: [0, 1, 2, 3, 4] },
     { orderable: false, targets: 0 }, //ocultar para columna 0 el ordenar columna
   ],
   pageLength: 10,
