828f5c35e4a075664e92c5e1241d741fb3c53093
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-08-28 08:53:24 -0500

    agregar_index

--- Views/Dashboard/home.php
+++ Views/Dashboard/home.php
@@ -5,7 +5,7 @@
 
     <div class="container mt-4">
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-4 mt-3">
                 <div class="slider-container">
                     <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
                         <div class="carousel-inner">
@@ -29,7 +29,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-8">
+            <div class="col-md-8 mt-3">
                 <div class="mb-3">
                     <div class="card p-3">
                         <div class="card-body d-flex align-items-center">
@@ -46,7 +46,7 @@
                         </div>
                     </div>
                 </div>
-                    <div class="mb-3 col-md-5">
+                    <div class="mb-3">
                         <div class="card module-card">
                             <img src="<?php echo SERVERURL; ?>/public/noticias/imagen_youtube.jpeg" class="card-img-top" alt="Módulo de Garantías">
                             <div class="card-body">
@@ -54,7 +54,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="d-flex flex-column gap-2">
+                    <div class="d-flex flex-column gap-2 mb-3">
                         <div class="card">
                             <div class="card-body d-flex justify-content-between align-items-center">
                                 <img src="<?php echo SERVERURL; ?>/public/noticias/FULLFILLMENT.png" class="card-img-top" alt="FULLFILLMENT" style="width: 10%;">
