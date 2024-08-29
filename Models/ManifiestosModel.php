d4408df40f23b20e6ce9eb04b42aa144e67fb251
EduardoVega86 <edu.vega86@gmail.com>
2024-08-29 15:47:10 -0500

    auditori

--- Models/ManifiestosModel.php
+++ Models/ManifiestosModel.php
@@ -892,6 +892,115 @@ GROUP BY dfc.id_producto,
         }
         return $response;
     }
+    
+    
+    public function despacho_guia_devolucion($num_guia, $plataforma, $id_cabecera)
+    {
+
+        $response = $this->initialResponse();
+
+
+        $sql = "INSERT INTO detalle_devolucion (numero_guia, id_cabecera_devolucion) VALUES (?, ?)";
+        $data = [$num_guia, $id_cabecera];
+        // Ejecuta la inserción
+        $insertar_detalle_rd = $this->insert($sql, $data);
+        
+        //print_r($insertar_detalle_rd);
+        $sql_factura = "SELECT * FROM facturas_cot WHERE numero_guia = '$num_guia'";
+        //  echo $sql_factura;
+        $factura = $this->select($sql_factura);
+        $id_factura = $factura[0]['id_factura'];
+        $estado_factura = $factura[0]['estado_factura'];
+
+        $sql_plataforma_bodega = "SELECT b.id_plataforma FROM `detalle_fact_cot` dfc, inventario_bodegas  ib, bodega b where ib.bodega=b.id and id_factura=$id_factura and dfc.id_inventario=ib.id_inventario GROUP by bodega";
+          //echo $sql_factura;$id_factura
+        $plataforma_bodega = $this->select($sql_plataforma_bodega);
+        $id_plataforma_bodega = $plataforma_bodega[0]['id_plataforma'];
+
+        if ($id_plataforma_bodega == $plataforma) {
+            if ($estado_factura == 1) {
+                //  echo $id_factura;
+
+                $tmp_cotizaciones = $this->select("SELECT * FROM detalle_fact_cot WHERE id_factura = $id_factura");
+                $detalle_sql_despacho = "INSERT INTO `historial_depacho` (`id_pedido`, `guia`, `id_producto`, `sku`, `cantidad`, `id_usuario`, `id_plataforma`) VALUES (?, ?, ?, ?, ?, ?, ?)";
+                // $sql = "INSERT INTO `historial_productos` (`id_users`, `id_inventario`, `id_plataforma`, `sku`, `nota_historial`, `referencia_historial`, `cantidad_historial`, `tipo_historial`, `id_bodega`, `id_producto`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                $detalle_sql_historial = "INSERT INTO `historial_productos` (`id_users`, `id_inventario`, `id_plataforma`, `sku`, `nota_historial`, `referencia_historial`, `cantidad_historial`, `tipo_historial`, `id_bodega`, `id_producto`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                //print_r($tmp_cotizaciones);
+                //$nota='Se descuenta'
+                $id_usuario = $_SESSION['id'];
+                foreach ($tmp_cotizaciones as $tmp) {
+                    //  echo 'enta';
+                    $despacho_data = array(
+                        $id_factura,
+                        $num_guia,
+                        $tmp['id_producto'],
+                        $tmp['sku'],
+                        $tmp['cantidad'],
+                        $id_usuario,
+                        $plataforma
+                    );
+                    $guardar_detalle = $this->insert($detalle_sql_despacho, $despacho_data);
+                    $nota = 'Se elimina ' . $tmp['cantidad'] . ' productos(s) del inventario -DESPACHO GUIA-';
+                    $id_inventario = $tmp['id_inventario'];
+                    $sql_bodega = "SELECT bodega FROM inventario_bodegas WHERE id_inventario = $id_inventario";
+                    //echo $sql_bodega;
+                    $bodega = $this->select($sql_bodega);
+                    //print_r($bodega);
+                    $id_bodega = $bodega[0]['bodega'];
+
+                    $historial_data = array(
+                        $id_usuario,
+                        $tmp['id_inventario'],
+                        $plataforma,
+                        $tmp['sku'],
+                        $nota,
+                        $num_guia,
+                        $tmp['cantidad'],
+                        2,
+                        $id_bodega,
+                        $tmp['id_producto']
+                    );
+                    $guardar_detalle = $this->insert($detalle_sql_historial, $historial_data);
+
+                    $sql_id = "SELECT saldo_stock FROM inventario_bodegas WHERE id_inventario = $id_inventario";
+                    $stock = $this->select($sql_id);
+                    $stock_inventario = $stock[0]['saldo_stock'];
+                    $saldo_stock = $stock_inventario - $tmp['cantidad'];
+                    $sql_update = "update inventario_bodegas set saldo_stock=? where id_inventario=?";
+                    $data = [$saldo_stock, $id_inventario];
+                    $actualizar_stock = $this->update($sql_update, $data);
+
+
+                    //print_r($guardar_detalle);
+                }
+
+                $sql = "UPDATE `facturas_cot` SET `estado_factura` = ? WHERE `id_factura` = ?";
+                $data = [2, $id_factura];
+                $editar_categoria = $this->update($sql, $data);
+
+                //print_r($tmp_cotizaciones);
+
+                if ($editar_categoria == 1) {
+                    $response['status'] = 200;
+                    $response['title'] = 'Peticion exitosa';
+                    $response['message'] = 'Despacho Exitoso';
+                } else {
+                    $response['status'] = 500;
+                    $response['title'] = 'Error';
+                    $response['message'] = 'Error al generar el despacho';
+                }
+            } else {
+                $response['status'] = 500;
+                $response['title'] = 'Error';
+                $response['message'] = 'Esta guia ya ha sido despachada';
+            }
+        } else {
+            $response['status'] = 500;
+            $response['title'] = 'Error';
+            $response['message'] = 'La guía no pertenece a esta bodega';
+        }
+        return $response;
+    }
 
     public function guardarCabecera($bodega, $transportadora, $plataforma)
     {
@@ -916,5 +1025,228 @@ GROUP BY dfc.id_producto,
 
         // Devuelve el ID generado
         return $lastInsertId;
+    }
+    
+     public function guardarCabeceraDevolucion($plataforma)
+    {
+
+        $id_usuario = $_SESSION['id'];
+        $fecha_actual = date('Y-m-d H:i:s');
+
+        $sql = "INSERT INTO cabecera_devolucion (id_usuario, id_plataforma, fecha_hora) VALUES (?, ?, ?)";
+        $data = [$id_usuario, $plataforma, $fecha_actual];
+        // Ejecuta la inserción
+        $insertar_producto = $this->insert($sql, $data);
+
+        //print_r($insertar_producto);
+
+        $sql_id = "SELECT id_devolucion FROM cabecera_devolucion WHERE id_usuario = $id_usuario "
+            . " and fecha_hora = '$fecha_actual' ";
+        $stock = $this->select($sql_id);
+        //echo $sql_id;
+        $lastInsertId = $stock[0]['id_devolucion'];
+
+        // Obtiene el ID del último registro insertado
+
+        // Devuelve el ID generado
+        return $lastInsertId;
+    }
+    public function generarManifiestoGuiasDevolucion($arreglo, $id_cabecera, $transportadora)
+    {
+        if (count($arreglo) == 0) return;
+
+
+        $string = "('" . implode("','", $arreglo) . "')";
+
+        // Consulta de facturas con el número de productos
+        $sql = "SELECT fc.*, 
+                   (SELECT SUM(cantidad) 
+                    FROM detalle_fact_cot dfc 
+                    WHERE dfc.id_factura = fc.id_factura) AS numero_productos 
+            FROM facturas_cot fc 
+            WHERE fc.numero_guia IN $string";
+
+        $resumen = $this->select($sql);
+
+        // Verificar que se haya obtenido el resumen
+        if (empty($resumen)) {
+            return ['status' => '500', 'message' => 'No se encontraron datos para generar el PDF.'];
+        }
+
+
+        $sql_bodega = "SELECT b.id as id, b.nombre as bodega, b.contacto, b.responsable, b.direccion FROM `facturas_cot` fc, detalle_fact_cot dfc, inventario_bodegas ib, bodega b WHERE numero_guia in $string and fc.id_factura=dfc.id_factura  and ib.id_inventario=dfc.id_inventario and ib.bodega=b.id limit 1;";
+        //echo $sql_bodega;
+        //  echo $sql_factura;$id_factura
+        $bodega = $this->select($sql_bodega);
+        $bodega_nombre = $bodega[0]['bodega'];
+        $telefono = $bodega[0]['contacto'];
+        $responsable = $bodega[0]['responsable'];
+        $direccion = $bodega[0]['direccion'];
+        $id_bodega = $bodega[0]['id'];
+
+
+
+        // $html ='<h3 style="text-align: center;>tecto</h3>';
+        $html = $this->generarTablaManifiestoDevolucion($resumen, $bodega_nombre, $direccion, $telefono, $responsable, $transportadora);
+        //echo $html;
+        // Generar el PDF con Dompdf
+        $dompdf = new Dompdf();
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+
+        // Ruta del archivo PDF
+        $combinedPdfPath = $this->generateUniqueFilename('RelacionDespacho-', __DIR__ . '/manifiestos');
+        $tempName = explode('-', $combinedPdfPath);
+        $tempName[0] = str_replace(__DIR__ . '/manifiestos/', '', $tempName[0]);
+        $lastNumber = glob(__DIR__ . '/manifiestos/' . $tempName[0] . '-*');
+        if (count($lastNumber) > 0) {
+            $lastNumber = explode('-', $lastNumber[count($lastNumber) - 1]);
+            $lastNumber = $lastNumber[1];
+            $lastNumber = explode('.', $lastNumber);
+            $lastNumber = $lastNumber[0];
+            $lastNumber = intval($lastNumber) + 1;
+            $combinedPdfPath = __DIR__ . '/manifiestos/' . $tempName[0] . '-' . $lastNumber . '.pdf';
+        } else {
+            $combinedPdfPath = __DIR__ . '/manifiestos/' . $tempName[0] . '-1000.pdf';
+        }
+
+        // Guardar el PDF en el servidor
+        file_put_contents($combinedPdfPath, $dompdf->output());
+
+        // Devolver la respuesta
+        $new_url = str_replace("/home/imporsuitpro/public_html/new", "", $combinedPdfPath);
+        $new_url = URL_MATRIZ . $new_url;
+
+        $reponse = [
+            "url" => $combinedPdfPath,
+            "download" => $new_url,
+            "status" => "200"
+        ];
+
+        $update = "UPDATE cabecera_relacion_despacho SET url_documento = '$new_url' WHERE id_relacion_despacho = $id_cabecera";
+        $this->select($update);
+
+        return $reponse;
+    }
+    public function generarTablaManifiestoDevolucion($data, $direccion, $telefono, $responsable)
+    {
+        $fecha = date('Y-m-d H:i:s'); // Obtén la fecha y hora actual
+        $generator = new BarcodeGeneratorHTML();
+
+        $id_usuario = $_SESSION['id'];
+        $sql_usuario = "SELECT nombre_users FROM users WHERE id_users = $id_usuario";
+        $usuario = $this->select($sql_usuario);
+        $nombre_usuario = $usuario[0]['nombre_users'];
+
+
+        $html = '
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        th, td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: left;
+            font-size: 8px;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+        .barcode img {
+            width: 100px; /* Ajusta el tamaño según sea necesario */
+            height: auto;
+        }
+        .footer {
+            position: fixed;
+            bottom: 20px;
+            left: 0;
+            width: 100%;
+            text-align: center;
+        }
+        hr {
+            width: 200px;
+            border: 1px solid #000;
+            margin: 10px auto;
+        }
+        @media screen and (max-width: 600px) {
+            table, thead, tbody, th, td, tr {
+                display: block;
+                width: 100%;
+            }
+            th, td {
+                box-sizing: border-box;
+                width: 100%;
+                text-align: right;
+            }
+            tr {
+                margin-bottom: 15px;
+            }
+            td {
+                text-align: right;
+                padding-left: 50%;
+                position: relative;
+            }
+            td:before {
+                content: attr(data-label);
+                position: absolute;
+                left: 10px;
+                width: calc(50% - 10px);
+                padding-right: 10px;
+                white-space: nowrap;
+                text-align: left;
+            }
+        }
+    </style>
+   
+    <p style="text-align: center; font-size: 12px;">' . strtoupper($direccion) . '</p>
+    <p style="text-align: center; font-size: 12px;">' . strtoupper($responsable) . ' / ' . strtoupper($telefono) . '</p>
+    <table>
+        <tr>
+         <th>Responsable</th>
+            <th>' . $nombre_usuario . '</th>
+            <th>Fecha</th>
+            <th>' . $fecha . '</th>
+        </tr>
+    </table>
+    <table>
+        <tr>
+            <th style="width:5%">#</th>
+            <th style="width:25%">Guia</th>
+            <th style="width:20%">Cliente</th>
+            <th style="width:20%">Contiene</th>
+            <th style="width:10%">Productos</th>
+            <th style="width:20%">Monto a cobrar</th>
+        </tr>';
+
+        $numero = 1;
+        foreach ($data as $row) {
+            $codigoBarras = $generator->getBarcode($row['numero_guia'], $generator::TYPE_CODE_128);
+            $html .= '<tr>';
+            $html .= '<td data-label="ID Producto">' . $numero . '</td>';
+            $html .= '<td data-label="Documento"><div class="barcode">' . $codigoBarras . '</div><br>' . htmlspecialchars($row['numero_guia']) . '</td>';
+            $html .= '<td data-label="Cliente">' . htmlspecialchars($row['nombre']) . '</td>';
+            $html .= '<td data-label="Contiene">' . htmlspecialchars($row['contiene']) . '</td>';
+            $html .= '<td data-label="No Productos">' . htmlspecialchars($row['numero_productos']) . '</td>';
+            if ($row['cod'] == 1) {
+                $monto_cobrar = htmlspecialchars($row['monto_factura']);
+            } else {
+                $monto_cobrar = 0;
+            }
+            $html .= '<td data-label="Monto a Cobrar">$ ' . number_format($monto_cobrar, 2) . '</td>';
+            $html .= '</tr>';
+            $numero++;
+        }
+
+        $html .= '
+    </table>
+    <div class="footer">
+        <hr>
+        <p>' . $nombre_usuario . '</p>
+    </div>';
+
+        return $html;
     }
 }
