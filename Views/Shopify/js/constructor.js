aa44e36c36de64ce523e63756809de2cbfa1e8ca
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-09-04 12:06:28 -0500

    agregar_index

--- Views/Shopify/js/constructor.js
+++ Views/Shopify/js/constructor.js
@@ -224,10 +224,10 @@ document.addEventListener("DOMContentLoaded", function () {
             title: "Guardado Correctamente",
             text: response.message,
             showConfirmButton: false,
-            timer: 2000,
+            timer: 2000
           }).then(() => {
-            window.location.href = "" + SERVERURL + "shopify/constructor_vista";
-          });
+            window.location.href = '' + SERVERURL + 'shopify/constructor_vista';
+        });
         }
       },
       error: function (error) {
@@ -266,86 +266,4 @@ document.addEventListener("DOMContentLoaded", function () {
       console.log(`Cambio detectado en select con id ${event.target.id}`);
     }
   });
-});
-
-let dataTableProductosShopify;
-let dataTableProductosShopifyIsInitialized = false;
-
-const dataTableProductosShopifyOptions = {
-  columnDefs: [
-    { className: "centered", targets: [1, 2, 3, 4, 5] },
-    { orderable: false, targets: 0 }, //ocultar para columna 0 el ordenar columna
-  ],
-  pageLength: 10,
-  destroy: true,
-  language: {
-    lengthMenu: "Mostrar _MENU_ registros por página",
-    zeroRecords: "Ningún producto encontrado",
-    info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
-    infoEmpty: "Ningún producto encontrado",
-    infoFiltered: "(filtrados desde _MAX_ registros totales)",
-    search: "Buscar:",
-    loadingRecords: "Cargando...",
-    paginate: {
-      first: "Primero",
-      last: "Último",
-      next: "Siguiente",
-      previous: "Anterior",
-    },
-  },
-};
-
-const initDataTableProductosShopify = async () => {
-  if (dataTableProductosShopifyIsInitialized) {
-    dataTableProductosShopify.destroy();
-  }
-
-  await listProductosShopify();
-
-  dataTableProductosShopify = $("#datatable_productos_shopify").DataTable(
-    dataTableProductosShopifyOptions
-  );
-
-  dataTableProductosShopifyIsInitialized = true;
-};
-
-const listProductosShopify = async () => {
-  try {
-    const response = await fetch("" + SERVERURL + "Productos/obtener_productos_shopify");
-    const productosShopify = await response.json();
-
-    let content = ``;
-
-    productosShopify.forEach((producto, index) => {
-      content += `
-                <tr>
-                    <td><a class="dropdown-item link-like" href="${SERVERURL}shopify/verProducto?producto=${producto.id}">${producto.nombre}</a></td>
-                    <td>${producto.precio}</td>
-                    <td>${producto.stock}</td>
-                    <td>${producto.categoria}</td>
-                    <td>
-                    <button id="downloadExcel" class="btn btn-success" onclick="descargarExcel_general('${producto.id}')">Descargar Excel general</button>
-                    <button id="downloadExcel" class="btn btn-success" onclick="descargarExcel('${producto.id}')">Descargar Excel</button>
-                    </td>
-                    <td>
-                    <div class="dropdown">
-                    <button class="btn btn-sm btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="fa-solid fa-gear"></i>
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <li><a class="dropdown-item" style="cursor: pointer;" href="${SERVERURL}shopify/editarProducto?producto=${producto.id}"><i class='bx bx-edit'></i>Editar</a></li>
-                        <li><a class="dropdown-item" style="cursor: pointer;" href="${SERVERURL}shopify/eliminarProducto?producto=${producto.id}"><i class='bx bx-trash'></i>Eliminar</a></li>
-                    </ul>
-                    </div>
-                    </td>
-                </tr>`;
-    });
-    document.getElementById("tableBody_productos_shopify").innerHTML = content;
-  } catch (ex) {
-    alert(ex);
-  }
-};
-
-window.addEventListener("load", async () => {
-  await initDataTableProductosShopify();
 });
