8b784dbc4e36319b92524c25b8a8c0c2a5f3d0ca
Jeimy Jara <jeimyjara@hotmail.com>
2024-09-16 12:58:58 -0500

    XD

--- Views/Wallet/js/historial_solicitudes.js
+++ Views/Wallet/js/historial_solicitudes.js
@@ -12,14 +12,12 @@ const getFecha = () => {
 let dataTableHistorialSolicitudes = false;
 
 const dataTableOptions = {
-  columnDefs: [{ className: "centered", targets: "_all" }],
+  columnDefs: [{ className: "text-center", targets: "_all" }],
   pageLength: 10,
   destroy: true,
   responsive: true,
-  dom:
-    "<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'f>>" +
-    "<'row'<'col-sm-12'tr>>" +
-    "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+  dom: '<"d-flex w-full justify-content-between"lBf><t><"d-flex justify-content-between"ip>',
+
   buttons: [
     {
       extend: "copy",
