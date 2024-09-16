0282f024f973e16f0ab56e24af35d7bf2b922d6b
Jeimy Jara <jeimyjara@hotmail.com>
2024-09-16 12:57:02 -0500

    XD

--- Views/Wallet/historial_solicitudes.php
+++ Views/Wallet/historial_solicitudes.php
@@ -1,27 +1,29 @@
 <?php require_once './Views/templates/header.php'; ?>
+<main class="container">
 
-<h2>
-    Historial de solicitudes
-</h2>
-<hr>
+    <h2 class="text-center">
+        Historial de solicitudes
+    </h2>
+    <hr>
 
-<div class="table-responsive">
-    <table id="datatable_historial" class="table table-striped table-hover">
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Fecha</th>
-                <th>Tipo de Solicitud</th>
-                <th>Cuenta</th>
-                <th>Usuario</th>
-                <th>Monto</th>
-            </tr>
-        </thead>
-        <tbody id="tableBody_historial">
+    <div class="table-responsive">
+        <table id="datatable_historial" class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Fecha</th>
+                    <th>Tipo de Solicitud</th>
+                    <th>Cuenta</th>
+                    <th>Usuario</th>
+                    <th>Monto</th>
+                </tr>
+            </thead>
+            <tbody id="tableBody_historial">
 
-        </tbody>
-    </table>
-</div>
+            </tbody>
+        </table>
+    </div>
+</main>
 <script src="<?php echo SERVERURL ?>/Views/Wallet/js/historial_solicitudes.js"></script>
 
 <?php require_once './Views/templates/footer.php'; ?>
