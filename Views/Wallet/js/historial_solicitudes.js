69a06154fea06b4b63c5b355122af229dc8b780d
Jeimy Jara <jeimyjara@hotmail.com>
2024-09-16 14:21:39 -0500

    XD

--- Views/Wallet/js/historial_solicitudes.js
+++ Views/Wallet/js/historial_solicitudes.js
@@ -42,7 +42,19 @@ const dataTableOptions = {
   ],
 
   language: {
-    url: "https://cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json",
+    lengthMenu: "Mostrar _MENU_ registros por página",
+    zeroRecords: "Ningún usuario encontrado",
+    info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+    infoEmpty: "Ningún usuario encontrado",
+    infoFiltered: "(filtrados desde _MAX_ registros totales)",
+    search: "Buscar:",
+    loadingRecords: "Cargando...",
+    paginate: {
+      first: "Primero",
+      last: "Último",
+      next: "Siguiente",
+      previous: "Anterior",
+    },
   },
 };
 
