625f6555947d99726255631f57fc86458f0d0bc2
Jeimy Jara <jeimyjara@hotmail.com>
2024-09-16 12:55:25 -0500

    XD

--- Models/WalletModel.php
+++ Models/WalletModel.php
@@ -1455,27 +1455,26 @@ class WalletModel extends Query
                     </button>
                     
                     <div class='modal fade' id='modal" . $key . "' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>
-                    <div class='modal-dialog' role='document'>
-                    <div class='modal-content'>
-                    <div class='modal-header'>
-                    <h5 class='modal-title' id='exampleModalLabel'>Detalles</h5>
-                    <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
-                    <span aria-hidden='true'>&times;</span>
-                    </button>
-                    </div>
-                    <div class='modal-body' style='text-align: left;'>
-                    <p><strong>Nombre:</strong> " . $response2[0]['nombre'] . "</p>
-                    <p><strong>Banco:</strong> " . $response2[0]['banco'] . "</p>
-                    <p><strong>Cedula:</strong> " . $response2[0]['cedula'] . "</p>
-                    <p><strong>Numero de cuenta:</strong> " . $response2[0]['numero_cuenta'] . "</p>
-                    <p><strong>Tipo de cuenta:</strong> " . $response2[0]['tipo_cuenta'] . "</p>
-                    <p><strong>Telefono:</strong> " . $response2[0]['telefono'] . "</p>
-                    </div>
-                    <div class='modal-footer'>
-                    <button type='button' class='btn btn-secondary' data-dismiss='modal'>Cerrar</button>
-                    </div>
-                    </div>
-                    </div>
+                        <div class='modal-dialog' role='document'>
+                            <div class='modal-content'>
+                                <div class='modal-header'>
+                                    <h5 class='modal-title' id='exampleModalLabel'>Detalles</h5>
+                                    <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+                                    </button>
+                                </div>
+                                <div class='modal-body' style='text-align: left;'>
+                                    <p><strong>Nombre:</strong> " . $response2[0]['nombre'] . "</p>
+                                    <p><strong>Banco:</strong> " . $response2[0]['banco'] . "</p>
+                                    <p><strong>Cedula:</strong> " . $response2[0]['cedula'] . "</p>
+                                    <p><strong>Numero de cuenta:</strong> " . $response2[0]['numero_cuenta'] . "</p>
+                                    <p><strong>Tipo de cuenta:</strong> " . $response2[0]['tipo_cuenta'] . "</p>
+                                    <p><strong>Telefono:</strong> " . $response2[0]['telefono'] . "</p>
+                                </div>
+                                <div class='modal-footer'>
+                                <button type='button' class='btn btn-secondary' data-dismiss='modal'>Cerrar</button>
+                                </div>
+                            </div>
+                        </div>
                     
                     </div>
                     
