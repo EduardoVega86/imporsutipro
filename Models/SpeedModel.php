9d04bca21429a9857dfb1a267b33225078bf89e6
Jeimy Jara <jeimyjara@hotmail.com>
2024-09-19 09:05:02 -0500

    XD

--- Models/SpeedModel.php
+++ Models/SpeedModel.php
@@ -47,6 +47,7 @@ class SpeedModel extends Query
         if (count($res) > 0) {
             $response['status'] = 200;
             $response['message'] = "El recibo se ha guardado correctamente";
+            $response['title'] = "¡Éxito!";
         } else {
             $response['message'] = "Lo siento, hubo un error al subir el archivo.";
         }
