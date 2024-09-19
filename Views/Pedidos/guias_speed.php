ac9b3c6c1d8529f7c98c6a7f897eb974b63fe366
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-09-19 08:47:51 -0500

    agregar_index

--- Views/Pedidos/guias_speed.php
+++ Views/Pedidos/guias_speed.php
@@ -6,7 +6,7 @@
 <?php require_once './Views/Pedidos/Modales/detalles_factura.php'; ?>
 <?php require_once './Views/Pedidos/Modales/gestionar_novedad.php'; ?>
 <?php require_once './Views/Pedidos/Modales/gestionar_novedadSpeed.php'; ?>
-<?php require_once './Views/Pedidos/Modales/subir_imagen_speed.php'; ?>
+<?php require_once './Views/Pedidos/Modales/subir_recibo_speed.php'; ?>
 
 <div class="custom-container-fluid">
     <div class="container mt-5" style="max-width: 1600px;">
