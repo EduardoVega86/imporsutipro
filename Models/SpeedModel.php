775b437ee5f5ea7b04eeb5f218e7f27505d184a6
Jeimy Jara <jeimyjara@hotmail.com>
2024-09-25 10:26:29 -0500

    Testing

--- Models/SpeedModel.php
+++ Models/SpeedModel.php
@@ -109,7 +109,7 @@ class SpeedModel extends Query
         } else {
             $response = $this->initialResponse();
             $response['status'] = 500;
-            $response['message'] = $response["message"];
+            $response['message'] = $res["message"];
         }
         return $response;
     }
