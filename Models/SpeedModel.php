fbf12bbfae4ec23283829c5a82722e4b8a1b382c
Jeimy Jara <jeimyjara@hotmail.com>
2024-09-25 11:26:22 -0500

    Testing

--- Models/SpeedModel.php
+++ Models/SpeedModel.php
@@ -172,7 +172,7 @@ class SpeedModel extends Query
             $res = $this->simple_insert($sql);
             $response = $this->handleSimpleResponse($res);
         } elseif ($estado == 3 || $estado == 7 || $estado == 9 || $estado == 14) {
-            $sql = "UPDATE facturas_cot SET estado_guia_sistema = $estado, googlemaps = '$googlemaps' WHERE id_factura = '$id_factura";
+            $sql = "UPDATE facturas_cot SET estado_guia_sistema = $estado, googlemaps = '$googlemaps' WHERE id_factura = '$id_factura'";
             $res = $this->simple_insert($sql);
 
             if ($res == 1) {
