75800944079f466d8fe3873c5b41b38e42f66355
EduardoVega86 <edu.vega86@gmail.com>
2024-10-01 18:20:31 -0500

    asa

--- Views/Pedidos/js/guias.js
+++ Views/Pedidos/js/guias.js
@@ -3,7 +3,7 @@ let dataTableIsInitialized = false;
 
 const dataTableOptions = {
   columnDefs: [
-    { className: "centered", targets: [1, 2, 3, 4, 5, 6, 7, 8, 9,10,11] },
+    { className: "centered", targets: [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10] },
     { orderable: false, targets: 0 } // Asegúrate de que esta sea la columna correcta
   ],
   order: [[2, "desc"]], // Ordenar por la primera columna (fecha) en orden descendente
