9c3c682bdec5c38782274c6cc2d7fa60136bc6db
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-10-09 11:44:11 -0500

    agregar_index

--- Views/Usuarios/js/tienda_online.js
+++ Views/Usuarios/js/tienda_online.js
@@ -159,6 +159,8 @@ $(document).ready(function () {
       plantilla = 1;
     } else if ((selectedTemplate == "template2")) {
       plantilla = 2;
+    } else if ((selectedTemplate == "template3")) {
+      plantilla = 3;
     }
     // Prepara los datos para la API
     let formData = new FormData();
