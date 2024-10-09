5019de0f121a3f2e3b8fe05dd2971d5f9c1473cf
Jeimy Jara <jeimyjara@hotmail.com>
2024-10-09 11:07:54 -0500

    Redirecciones

--- Views/Acceso/redirects.php
+++ Views/Acceso/redirects.php
@@ -1,25 +1,6 @@
 <?php require_once './Views/templates/header.php'; ?>
 <?php require_once './Views/Productos/css/bodegas_style.php'; ?>
 
-<!-- genera 4 botones con funciones -->
-<div class="custom-container-fluid">
-    <div class="container mt-5" style="max-width: 1600px;">
-        <h2 class="text-center mb-4">Bodegas</h2>
-        <button onclick="redirect('herramientas')" class="btn btn-success">
-            <i class="fas fa-plus"></i> Agregar
-        </button>
-        <button onclick="redirect('plataformas')" class="btn btn-success">
-            <i class="fas fa-plus"></i> Agregar
-        </button>
-        <button onclick="redirect('cotizador')" class="btn btn-success">
-            <i class="fas fa-plus"></i> Agregar
-        </button>
-        <button onclick="redirect('infoaduana')" class="btn btn-success">
-            <i class="fas fa-plus"></i> Agregar
-        </button>
-    </div>
-</div>
-
 <script>
     function redirect(direccion) {
         let token = "<?= $_SESSION['token'] ?>";
@@ -38,5 +19,24 @@
         window.location.href = ruta;
     }
 </script>
+
+<!-- genera 4 botones con funciones -->
+<div class="custom-container-fluid">
+    <div class="container mt-5" style="max-width: 1600px;">
+        <h2 class="text-center mb-4">Bodegas</h2>
+        <button onclick="redirect('herramientas')" class="btn btn-success">
+            <i class="fas fa-plus"></i> Agregar
+        </button>
+        <button onclick="redirect('plataformas')" class="btn btn-success">
+            <i class="fas fa-plus"></i> Agregar
+        </button>
+        <button onclick="redirect('cotizador')" class="btn btn-success">
+            <i class="fas fa-plus"></i> Agregar
+        </button>
+        <button onclick="redirect('infoaduana')" class="btn btn-success">
+            <i class="fas fa-plus"></i> Agregar
+        </button>
+    </div>
+</div>
 
 <?php require_once './Views/templates/footer.php'; ?>
