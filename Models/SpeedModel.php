f528c12743f8e1ff301c1419243b107675146983
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-10-10 10:51:42 -0500

    agregar_index

--- Models/SpeedModel.php
+++ Models/SpeedModel.php
@@ -519,10 +519,10 @@ class SpeedModel extends Query
             foreach ($json_bloques as $bloque_info) {
                 // Verificamos si el id_block es "0"
                 if ($bloque_info['id_block'] == "0") {
-
+                    echo "XD";
                     // Verificamos que 'status[]' exista y que sea un array
                     if (isset($bloque_info['status[]']) && is_array($bloque_info['status[]'])) {
-
+                        echo "DX";
                         // Iteramos sobre cada estado dentro del array 'status[]'
                         foreach ($bloque_info['status[]'] as $status) {
                             // Comprobamos si el valor de status es 1
