bc25fd6096034370644e22b3028e489924b6db02
Jeimy Jara <jeimyjara@hotmail.com>
2024-10-15 10:52:28 -0500

    Redirecciones

--- Views/Dashboard/redirect.php
+++ Views/Dashboard/redirect.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Imporsuit</title>
-    <link rel="stylesheet" href="css/redirect.css" />
+    <link rel="stylesheet" href="<?php echo SERVERURL ?>Views/Dashboard/css/redirect.css" />
     <script src="https://cdn.tailwindcss.com"></script>
     <link
         rel="stylesheet"
