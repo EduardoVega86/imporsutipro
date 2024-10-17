104908690c295ebd4f61bcc3317672bc608f5c7c
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-10-17 16:41:10 -0500

    agregar_index

--- Views/Pedidos/js/editar_pedido.js
+++ Views/Pedidos/js/editar_pedido.js
@@ -368,7 +368,7 @@ $(document).ready(function () {
 
       var button2 = document.getElementById("generarGuiaBtn");
 
-      if (saldo < -10 && ID_PLATAFORMA != 1238) {
+      if (saldo < -10 && ID_PLATAFORMA != 1238 && ID_PLATAFORMA !=1226) {
         button2.disabled = true;
         Swal.fire({
           icon: "error",
