d5b3c1a12015f7b8546b9711749264fead6bd5cf
Jeimy Jara <jeimyjara@hotmail.com>
2024-10-18 13:47:38 -0500

    Redirecciones

--- Views/Funnelish/productos.php
+++ Views/Funnelish/productos.php
@@ -1,7 +1,33 @@
 <?php require_once './Views/templates/header.php'; ?>
 
 <div class="custom-container-fluid">
+    <div class="container mt-5" style="max-width: 1600px;">
+        <h2 class="text-center mb-4">Guias Anuladas</h2>
 
+        <div class="table-responsive">
+            <!-- <table class="table table-bordered table-striped table-hover"> -->
+            <table id="datatable_guias" class="table table-striped">
+                <!-- <caption>
+                    DataTable.js Demo
+                </caption> -->
+                <thead>
+                    <tr>
+                        <th class="centered"><input type="checkbox" id="selectAll"></th>
+                        <th class="centered"># Orden</th>
+                        <th class="centered">Detalle</th>
+                        <th class="centered">Cliente</th>
+                        <th class="centered">Destino</th>
+                        <th class="centered">Tienda</th>
+                        <th class="centered">Transportadora</th>
+                        <th class="centered">Estado</th>
+                        <th class="centered">Impreso</th>
+                        <th class="centered">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody id="tableBody_guias"></tbody>
+            </table>
+        </div>
+    </div>
 </div>
 
 <?php require_once './Views/templates/footer.php'; ?>
