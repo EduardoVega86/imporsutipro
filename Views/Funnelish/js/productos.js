cfbd42af753d0b95ca9c95426a80da7e9600b54e
Jeimy Jara <jeimyjara@hotmail.com>
2024-10-18 15:36:04 -0500

    Redirecciones

---
+++ Views/Funnelish/js/productos.js
@@ -0,0 +1,150 @@
+let dataTable;
+let dataTableIsInitialized = false;
+
+const dataTableOptions = {
+  columnDefs: [
+    { className: "text-center", targets: "_all" },
+    { orderable: false, targets: 0 },
+  ],
+  order: [[1, "asc"]],
+  pageLength: 10,
+  lengthMenu: [10, 25, 50, 100],
+  destroy: true,
+  responsive: true,
+  dom: '<"d-flex w-full justify-content-between"lBf><t><"d-flex justify-content-between"ip>',
+  buttons: [
+    {
+      extend: "excelHtml5",
+      text: 'Excel <i class="fa-solid fa-file-excel"></i>',
+      title: "Panel de Control: Usuarios",
+      titleAttr: "Exportar a Excel",
+      exportOptions: {
+        columns: "_all",
+      },
+      filename: "Productos" + "_" + getFecha(),
+      footer: true,
+      className: "btn-excel",
+    },
+    {
+      extend: "csvHtml5",
+      text: 'CSV <i class="fa-solid fa-file-csv"></i>',
+      title: "Panel de Control: Productos",
+      titleAttr: "Exportar a CSV",
+      exportOptions: {
+        columns: "_all",
+      },
+      filename: "Productos" + "_" + getFecha(),
+      footer: true,
+      className: "btn-csv",
+    },
+  ],
+  language: {
+    lengthMenu: "Mostrar _MENU_ registros por página",
+    zeroRecords: "Ningún usuario encontrado",
+    info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+    infoEmpty: "Ningún usuario encontrado",
+    infoFiltered: "(filtrados desde _MAX_ registros totales)",
+    search: "Buscar:",
+    loadingRecords: "Cargando...",
+    paginate: {
+      first: "Primero",
+      last: "Último",
+      next: "Siguiente",
+      previous: "Anterior",
+    },
+  },
+};
+
+function getFecha() {
+  let fecha = new Date();
+  let mes = fecha.getMonth() + 1;
+  let dia = fecha.getDate();
+  let anio = fecha.getFullYear();
+  let fechaHoy = anio + "-" + mes + "-" + dia;
+  return fechaHoy;
+}
+const initDataTable = async () => {
+  if (dataTableIsInitialized) {
+    dataTable.destroy();
+  }
+
+  await listProductos();
+
+  dataTable = $("#tableProductos").DataTable(dataTableOptions);
+
+  dataTableIsInitialized = true;
+};
+
+const listProductos = async () => {
+  try {
+    const response = await fetch("/productos/listar");
+
+    const data = await response.json();
+
+    let content = "";
+
+    data.forEach((producto) => {
+      content += `
+            <tr>
+            <td>
+                <span>${producto.id}</span>
+            </td>
+            <td>
+                <span>${producto.nombre}</span>
+            </td>
+            <td>
+                <span>${producto.codigo_funnelish}</span>
+            </td>
+            <td>
+                <span>${producto.codigo_producto}</span>
+            </td>
+            <td>
+                <div class="d-flex justify-content-center">
+                    <button class="btn btn-primary btn-sm" onclick="editarProducto(${producto.id})">
+                        <i class="fa-solid fa-pencil"></i>
+                    </button>
+                    <button class="btn btn-danger btn-sm" onclick="eliminarProducto(${producto.id})">
+                        <i class="fa-solid fa-trash"></i>
+                    </button>
+                </div>
+            </td>
+            </tr>
+        `;
+    });
+
+    $("#tableProductos tbody").html(content);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const editarProducto = async (id) => {
+  try {
+    const response = await fetch(`/productos/obtener/${id}`);
+
+    const data = await response.json();
+
+    $("#modalProducto").modal("show");
+
+    $("#id").val(data.id);
+    $("#nombre").val(data.nombre);
+    $("#codigo_funnelish").val(data.codigo_funnelish);
+    $("#codigo_producto").val(data.codigo_producto);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const eliminarProducto = async (id) => {
+  try {
+    const response = await fetch(`/productos/eliminar/${id}`);
+
+    const data = await response.json();
+
+    if (data.ok) {
+      initDataTable();
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
--- Views/Funnelish/productos.php
+++ Views/Funnelish/productos.php
@@ -12,22 +12,59 @@
                 </caption> -->
                 <thead>
                     <tr>
-                        <th class="centered"><input type="checkbox" id="selectAll"></th>
-                        <th class="centered"># Orden</th>
-                        <th class="centered">Detalle</th>
-                        <th class="centered">Cliente</th>
-                        <th class="centered">Destino</th>
-                        <th class="centered">Tienda</th>
-                        <th class="centered">Transportadora</th>
-                        <th class="centered">Estado</th>
-                        <th class="centered">Impreso</th>
-                        <th class="centered">Acciones</th>
+                        <th class="centered">Id</th>
+                        <th class="centered">Producto</th>
+                        <th class="centered">Codigo Funnelish</th>
+                        <th class="centered">Codigo Producto</th>
+                        <th colspan="2" class="centered">Acciones</th>
                     </tr>
                 </thead>
-                <tbody id="tableBody_guias"></tbody>
+                <tbody id="tableBody_guias">
+
+                </tbody>
             </table>
         </div>
     </div>
+
+    <!-- modal para editar -->
+    <div class="modal fade" id="modalProducto" tabindex="-1" role="dialog" aria-labelledby="modalProductoLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <form id="formEditar">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modalProductoLabel">Editar Producto</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="id">Id</label>
+                            <input type="text" class="form-control" id="id" name="id" readonly>
+                        </div>
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <label for="producto">Producto</label>
+                                <input disabled type="text" class="form-control" id="producto" name="producto">
+
+                            </div>
+                            <div class="form-group">
+                                <label for="codigo_funnelish">Codigo Funnelish</label>
+                                <input type="text" class="form-control" id="codigo_funnelish" name="codigo_funnelish">
+                            </div>
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                            <button type="submit" class="btn btn-primary">Guardar Cambios</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
+
+<script src="<?php echo SERVERURL ?>/Views/Funnelish/js/productos.js"></script>
 
 <?php require_once './Views/templates/footer.php'; ?>
