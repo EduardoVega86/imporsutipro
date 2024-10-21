b1e319b5d03b9d10cf12ea1869232d2ae05a8af8
Jeimy Jara <jeimyjara@hotmail.com>
2024-10-21 16:25:07 -0500

    Redirecciones

--- Models/ProductosModel.php
+++ Models/ProductosModel.php
@@ -116,7 +116,7 @@ class ProductosModel extends Query
     {
 
         if ($plataforma == 1206 || $plataforma == 2324) {
-            $sql = "SELECT st.id_inventario, st.id_plataforma, p.nombre_producto FROM `shopify_tienda` st INNER JOIN `inventario_bodegas` ib ON ib.id_inventario = st.id_inventario INNER JOIN `productos` p ON p.id_producto = ib.id_producto where st.id_plataforma = $plataforma;";
+            $sql = "SELECT st.id_inventario, st.id_plataforma, p.nombre_producto as 'nombre_producto_tienda' FROM `shopify_tienda` st INNER JOIN `inventario_bodegas` ib ON ib.id_inventario = st.id_inventario INNER JOIN `productos` p ON p.id_producto = ib.id_producto where st.id_plataforma = $plataforma;";
         } else {
 
             $sql = "SELECT * FROM `productos_tienda` pt, productos p, inventario_bodegas ib WHERE  pt.id_producto=p.id_producto and pt.id_inventario=ib.id_inventario and pt.id_plataforma=$plataforma";
