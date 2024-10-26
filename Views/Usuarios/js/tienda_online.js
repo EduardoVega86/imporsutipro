8fe2a66ec626df3b503a218a473b9b9c9eed6a9d
EduardoVega86 <edu.vega86@gmail.com>
2024-10-25 22:33:44 -0500

    bodegas

--- Views/Usuarios/js/tienda_online.js
+++ Views/Usuarios/js/tienda_online.js
@@ -525,7 +525,7 @@ $("#vista_previa").html(
         $("#seccion_paralax_plantilla3").hide();
         
         valor_banner="<strong>Atención:</strong> las dimensines de la imagen deben ser 2550x860 y en formato .png, .jpg, .jpeg";
-       alert(valor_banner)
+      // alert(valor_banner)
             $("#muestra_banner").html(valor_banner);
         
         
@@ -540,7 +540,7 @@ $("#seccion_paralax_plantilla3").hide();
         // Actualiza los valores de los inputs hidden
         $("#plantilla_selected").val("template2");
         
-         valor_banner="<strong>Atención:</strong> se recomienda utilizar las dimensiones de la muestra y en formato .png , .webp, descargar muetra <a target='blank' href='https://new.imporsuitpro.com/public/img/banner/muestra_parallax.webp' download='mi_imagen.png' class='btn-descargar'>";
+         valor_banner="<strong>Atención:</strong> las dimensines de la imagen deben ser 2550x860 y en formato .png, .jpg, .jpeg";
         $("#muestra_banner").html(valor_banner);
 
         // Remueve la clase 'selected' de todas las plantillas
@@ -558,7 +558,7 @@ $("#seccion_promocion_plantilla2").hide();
 
 $("#seccion_paralax_plantilla3").show();
 
- valor_banner="<strong>Atención:</strong> las dimensines de la imagen deben ser 2550x860 y en formato .png, .jpg, .jpeg";
+ valor_banner="<strong>Atención:</strong> se recomienda utilizar las dimensiones de la muestra y en formato .png , .webp, descargar muetra <a target='blank' href='https://new.imporsuitpro.com/public/img/banner/muestra_banner.webp' download='mi_imagen.png' class='btn-descargar'><i class='fas fa-download'></i></a>";
         $("#muestra_banner").html(valor_banner);
 
         // Agrega la clase 'selected' a la plantilla correcta
