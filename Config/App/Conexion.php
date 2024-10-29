662d3ea134e2dbbd3b53b3b72e6b344ac7761635
Jeimy Jara <jeimyjara@hotmail.com>
2024-10-29 16:18:15 -0500

    XD

--- Config/App/Conexion.php
+++ Config/App/Conexion.php
@@ -11,7 +11,10 @@ class Conexion
         try {
             $this->connection = new PDO($pdo, USER, PASSWORD, [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4'
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4',
+                PDO::ATTR_PERSISTENT => true,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => false
             ]);
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
