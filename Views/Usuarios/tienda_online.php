b63e50af331e0df3189930417aaccb20878dea4f
EduardoVega86 <edu.vega86@gmail.com>
2024-10-30 13:56:29 -0500

    bodegas

--- Views/Usuarios/tienda_online.php
+++ Views/Usuarios/tienda_online.php
@@ -850,7 +850,7 @@
                                     <th class="text-nowrap">Imagen</th>
                                     <th class="text-nowrap">Titulo</th>
                                     <th class="text-nowrap">Nombre</th>
-                                    <th class="text-nowrap">Testimonio</th>
+                                    <th class="text-nowrap">Resumen</th>
                                     <th class="text-nowrap">LinkedIng</th>
                                     <th class="text-nowrap">Facebook</th>
                                     <th class="text-nowrap">Instagram</th>
