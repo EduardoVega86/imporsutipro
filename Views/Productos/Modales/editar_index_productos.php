75e679e52c27e52ad68a1b16fd61c99a571a39fe
EduardoVega86 <edu.vega86@gmail.com>
2024-11-05 17:11:50 -0500

    bodegas

--- Views/Productos/Modales/editar_index_productos.php
+++ Views/Productos/Modales/editar_index_productos.php
@@ -84,25 +84,17 @@
                     <div class="tab-pane fade" id="editar-precios-stock" role="tabpanel" aria-labelledby="editar-precios-stock-tab">
                         <div class="d-flex flex-column">
                             <div class="d-flex flex-row gap-3">
-                                <div class="form-group w-100">
-                                    <label for="editar_ultimo_costo">Ultimo Costo:</label>
-                                    <input type="text" class="form-control" id="editar_ultimo_costo">
-                                </div>
+                               
                                 <div class="form-group w-100">
                                     <label for="editar_precio_proveedor">Precio Proveedor:</label>
                                     <input type="text" class="form-control" id="editar_precio_proveedor">
                                 </div>
-                            </div>
-                            <div class="d-flex flex-row gap-3">
-                                <div class="form-group w-100">
+                                  <div class="form-group w-100">
                                     <label for="editar_precio_venta">Precio de Venta (Sugerido):</label>
                                     <input type="text" class="form-control" id="editar_precio_venta">
                                 </div>
-                                <div class="form-group w-100">
-                                    <label for="editar_precio_referencial">Precio Referencial</label>
-                                    <input type="text" class="form-control mt-2" id="editar_precio_referencial">
-                                </div>
                             </div>
+                          
                             <div class="d-flex flex-row gap-3">
                                 <div class="form-group w-100">
                                     <label for="editar_maneja_inventario">Maneja Inventario:</label>
