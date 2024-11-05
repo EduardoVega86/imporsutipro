633a1b8037d38b468a8eb211237ef2b8f0f14d8f
Jeimy Jara <jeimyjara@hotmail.com>
2024-11-05 16:06:48 -0500

    CAMBIO DE BASE DATOS

--- Config/App/Conexion.php
+++ Config/App/Conexion.php
@@ -13,7 +13,6 @@ class Conexion
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4',
                 PDO::ATTR_PERSISTENT => true,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => false
             ]);
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
