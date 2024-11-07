1715e1d40781ca4829191d0c743175026b645729
Jeimy Jara <jeimyjara@hotmail.com>
2024-11-07 10:02:45 -0500

    CAMBIO DE BASE DATOS

--- Controllers/Dashboard.php
+++ Controllers/Dashboard.php
@@ -40,8 +40,8 @@ class Dashboard extends Controller
 
     public function filtroInicial()
     {
-        $fecha_i = $_POST['fecha_i'] ?? date('Y-m-01');
-        $fecha_f = $_POST['fecha_f'] ?? date('y-m-t', strtotime($fecha_i));
+        $fecha_i = $_POST['fechai'] ?? date('Y-m-01');
+        $fecha_f = $_POST['fechaf'] ?? date('y-m-t', strtotime($fecha_i));
 
         $data = $this->model->filtroInicial($fecha_i, $fecha_f, $_SESSION['enlace'], $_SESSION['id_plataforma']);
         echo json_encode($data);
