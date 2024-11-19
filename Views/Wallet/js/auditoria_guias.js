d75b8fe00ca9ae1c7b73f62a307517e09c60fd09
Jeimy Jara <jeimyjara@hotmail.com>
2024-11-19 11:13:45 -0500

    Modificación auditoria

--- Views/Wallet/js/auditoria_guias.js
+++ Views/Wallet/js/auditoria_guias.js
@@ -317,7 +317,11 @@ const listAuditoria = async (estado, id_transporte) => {
             motivo +
             " VALOR DEVUELTO";
         }
-        if (item.drogshipin == 1 && item.costo_wallet <= 0) {
+        if (
+          item.drogshipin == 1 &&
+          item.costo_wallet <= 0 &&
+          item.id_plataforma != 2324
+        ) {
           motivo = motivo + "DROP COSTO 0";
         }
         mostrar = 1;
