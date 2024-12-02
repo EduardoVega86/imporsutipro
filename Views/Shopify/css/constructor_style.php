1b8bdc536cc808e6f6dd877f0e87c15d31126558
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-12-02 11:19:36 -0500

    agregar_index

--- Views/Shopify/css/constructor_style.php
+++ Views/Shopify/css/constructor_style.php
@@ -3,7 +3,7 @@
         display: flex;
         justify-content: center;
         align-items: center;
-        height: 100vh;
+        height: 92vh;
         background-color: #f0f0f0;
         padding-left: 30px;
         overflow: hidden;
