d71f70e67b73078c3b38f83c1305aa750d510207
Jeimy Jara <jeimyjara@hotmail.com>
2024-12-09 11:34:22 -0500

    Se gestiono un api de prueba

--- Models/PruebaModel.php
+++ Models/PruebaModel.php
@@ -15,6 +15,14 @@ class PruebaModel extends Query
         $sql = "SELECT * FROM plataformas";
         $data = $this->select($sql);
 
+        if (is_array($data) && count($data) > 0) {
+            $response["message"] = "Datos encontrados";
+            $response['status'] = 200;
+        } else {
+            $response['status'] = 404;
+            $response["message"] = "No se encontraron datos";
+        }
+
         $response['data'] = $data;
 
         return $response;
