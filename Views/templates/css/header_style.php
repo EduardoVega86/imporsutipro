a35ed03c1054448f203b5084b7db893396950ca0
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-12-09 16:58:42 -0500

    agregar_index

--- Views/templates/css/header_style.php
+++ Views/templates/css/header_style.php
@@ -311,6 +311,5 @@
         transform: scale(1.1);
         box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.3);
     }
-
     /* Fin whatsapp flotante */
 </style>
--- Views/templates/landing/footer.php
+++ Views/templates/landing/footer.php
@@ -1,4 +1,7 @@
-
+<!-- WhatsApp Floating Icon -->
+<a href="https://wa.me/1234567890" target="_blank" class="whatsapp-float">
+    <i class="bx bxl-whatsapp bx-lg"></i>
+</a>
 
 </body>
 
