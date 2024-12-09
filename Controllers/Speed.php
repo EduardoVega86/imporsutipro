6414551c06a501b6f3f6683f3b39592ea1bcd094
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-12-09 14:58:50 -0500

    agregar_index

--- Controllers/Speed.php
+++ Controllers/Speed.php
@@ -148,10 +148,11 @@ class Speed extends Controller
 
 
     public function automatizador()
-    {
+    {  
+        echo "entro en la funcion automatizador";
         $id_factura = $_POST['id_factura'];
         $response = $this->model->verificarAutomatizacion($id_factura);
-
+        print_r($response);
         if ($response['status'] == 200) {
             $response2 = $this->model->automatizar($response['data']);
             print_r($response2);
