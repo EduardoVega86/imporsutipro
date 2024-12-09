ffc4ffdf15e9b3238e4a407e68ed35c7e36c018a
Jeimy Jara <jeimyjara@hotmail.com>
2024-12-09 14:39:49 -0500

    BACK: testing

--- Controllers/Speed.php
+++ Controllers/Speed.php
@@ -151,9 +151,10 @@ class Speed extends Controller
     {
         $id_factura = $_POST['id_factura'];
         $response = $this->model->verificarAutomatizacion($id_factura);
-        print_r($response);
+
         if ($response['status'] == 200) {
             $response2 = $this->model->automatizar($response['data']);
+            print_r($response2);
         }
         echo json_encode($response);
     }
