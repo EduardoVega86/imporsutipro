beddefc81bd60269ddd90834ab76aae631f26236
Jeimy Jara <jeimyjara@hotmail.com>
2024-12-11 09:47:06 -0500

    Reportes: Test

--- Views/Inconsistencias/gintracom.php
+++ Views/Inconsistencias/gintracom.php
@@ -150,9 +150,7 @@
                 try {
                     const response = await fetch(url, {
                         method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
+
                         body: formData
                     });
 
