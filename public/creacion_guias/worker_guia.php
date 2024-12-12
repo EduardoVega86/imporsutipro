b85de353d45d622cdfbc156a4c6b598bf2c162b4
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-12-12 15:51:50 -0500

    sistema de colas creacion de guias

--- public/creacion_guias/worker_guia.php
+++ public/creacion_guias/worker_guia.php
@@ -21,7 +21,6 @@ if (!is_dir($logDirectory)) {
 // Ruta del archivo de log
 $logFile = $logDirectory . '/error_log_worker_guia.txt';
 
-
 // Función para registrar errores o mensajes en el archivo de log
 function logError($message)
 {
