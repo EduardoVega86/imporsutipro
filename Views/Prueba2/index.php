773ad1d0927ca540bfe749394a88ae37407c36ee
Jeimy Jara <jeimyjara@hotmail.com>
2024-12-26 10:32:16 -0500

    Subida de un archivo de prueba

---
+++ Views/Prueba2/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <main>
+        <section>
+            <article>
+                <h1>Mi primera vista</h1>
+                <p>Hola</p>
+                <span>Mundo</span>
+            </article>
+        </section>
+    </main>
+</body>
+
+</html>
