f5c8cb6ad56e72fe35bee3139ac2f04f8e1ef774
Jeimy Jara <jeimyjara@hotmail.com>
2024-12-27 15:17:56 -0500

    guias_administrador_2 a sus estado base

--- Models/PedidosModel.php
+++ Models/PedidosModel.php
@@ -312,10 +312,10 @@ class PedidosModel extends Query
             }
         }
 
-        // Si existe búsqued
+        // Si existe búsqueda
         if (!empty($search)) {
             $search = '%' . $search . '%';
-            $sql .= "OR (numero_factura LIKE $search)";
+            $sql .= " AND (numero_factura LIKE '$search')";
         }
 
         if ($drogshipin == 0 || $drogshipin == 1) {
