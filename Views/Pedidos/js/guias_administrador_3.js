65f2dba7656bfd26a8f964d64659f3c8cafc4765
Jeimy Jara <jeimyjara@hotmail.com>
2024-12-28 16:00:06 -0500

    Guias Administrador: Generado la version de sebastian

--- Views/Pedidos/js/guias_administrador_3.js
+++ Views/Pedidos/js/guias_administrador_3.js
@@ -110,7 +110,7 @@ const listGuias = async () => {
     formData.append("despachos", $("#despachos").val());
 
     const response = await fetch(
-      `${SERVERURL}pedidos/obtener_guiasAdministrador3`,
+      `${SERVERURL}pedidos/obtener_guiasAdministrador`,
       {
         method: "POST",
         body: formData,
