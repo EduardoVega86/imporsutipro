b14308c1370b5827bd2298c82151c284c318f381
Sebastian Ordoñez <desarrollo3imporfactory@gmail.com>
2025-01-07 13:51:44 -0500

    Swagger controlador  update  - Sebastian

--- Controllers/Swagger.php
+++ Controllers/Swagger.php
@@ -52,11 +52,43 @@ class Swagger extends Controller
      * )
      * )
      */
+    public function pedidos($uuid)
+    {
+        try {
+            $this->logRequest('api/pedidos', $_SERVER['REQUEST_METHOD'], file_get_contents('php://input'));
+            $response = $this->model->getPedidos($uuid);
+            if ($response['status'] === 200) {
+                http_response_code(200);
+            } else {
+                http_response_code(400);
+            }
+            echo json_encode($response);
+        } catch (Exception $e) {
+            http_response_code(500);
+            echo json_encode(['status' => 500, 'message' => 'Error interno del servidor', 'error' => $e->getMessage()]);
+        }
+    }
 
     public function docs()
     {
-        $openapi = \OpenApi\Generator::scan([__DIR__ . "/Swagger.php"]);
+        $openapi = \OpenApi\Generator::scan([__DIR__]);
         header('Content-Type: application/json');
         echo $openapi->toJson();
+    }
+
+    private function logRequest($endpoint, $method, $body)
+    {
+        $logData = [
+            'endpoint' => $endpoint,
+            'method' => $method,
+            'body' => $body,
+            'ip_address' => $_SERVER['REMOTE_ADDR'],
+            'user_agent' => $_SERVER['HTTP_USER_AGENT'],
+            'timestamp' => date('Y-m-d H:i:s'),
+        ];
+
+        // Guardar en un archivo o base de datos
+        $logFile = __DIR__ . '/../../api_requests ' . date('Y-m-d HH:ii:ss') . '.log';
+        file_put_contents($logFile, json_encode($logData) . PHP_EOL, FILE_APPEND);
     }
 }
