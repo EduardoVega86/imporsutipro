22f81b88d42d5b218f679da7467ea18044d518e1
Sebastian Ordoñez <desarrollo3imporfactory@gmail.com>
2025-01-07 13:28:30 -0500

    Swagger controlador  update  - Sebastian

--- Controllers/Swagger.php
+++ Controllers/Swagger.php
@@ -4,16 +4,18 @@ require 'vendor/autoload.php';
 use OpenApi\Annotations as OA;
 
 /**
- * @OA\Info(
- *     title="Documentación de la API Externa",
- *     version="1.0.0",
- *     description="Documentación de la API generada con Swagger | Ante cualquier duda, contactar con el administrador",
- *    @OA\Contact(
- *        email="jjara@imporfactorylatam.com"
+ * @OA\OpenApi(
+ *   openapi="3.0.0",
+ *   @OA\Info(
+ *       title="Documentación de la API Externa",
+ *       version="1.0.0",
+ *       description="Documentación generada con Swagger. Ante cualquier duda, contactar con el administrador",
+ *       @OA\Contact(
+ *          email="jjara@imporfactorylatam.com"
+ *       )
  *   )
  * )
  */
-
 class Swagger extends Controller
 {
     public function __construct()
@@ -26,9 +28,10 @@ class Swagger extends Controller
         $this->views->render($this, "index");
     }
 
-
     public function docs()
     {
+        // Escanea SOLO este archivo
+        // (lo ideal sería escanear todo el directorio con anotaciones)
         $openapi = \OpenApi\Generator::scan([__DIR__ . "/Swagger.php"]);
         header('Content-Type: application/json');
         echo $openapi->toJson();
