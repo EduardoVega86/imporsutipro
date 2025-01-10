f48a88dc98551c36fa01be94f23bcfcc11d8cba9
Sebastian Ordoñez <desarrollo3imporfactory@gmail.com>
2025-01-10 12:02:32 -0500

    Creando tabla bobeda - Sebastian

---
+++ Views/Productos/bobeda.php
@@ -0,0 +1,17 @@
+<?php require_once './Views/templates/header.php'; ?>
+
+
+<div class="custom-container-fluid">
+    <div class="container mt-5" style="max-width: 1600px;">
+        <thead>
+            <tr>Nombre</tr>
+            <tr>Categoría</tr>
+            <tr>Proveedor</tr>
+            <tr>Ejemplo Landing</tr>
+            <tr>Duplicar Funnel</tr>
+            <tr>Videos</tr>
+        </thead>
+    </div>
+</div>
+<script src="<?php echo SERVERURL ?>/Views/Productos/js/bobeda.js"></script>
+<?php require_once './Views/templates/footer.php'; ?>
