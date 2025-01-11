3d7924b7dbc9278c82285fa3eeba7a5c785da950
Michael Ordoñez <desarrollo3imporfactory@gmail.com>
2025-01-11 18:34:16 -0500

    Styles  en tabla bovedas - Sebastian

---
+++ Views/Productos/css/bovedas_style.php
@@ -0,0 +1,34 @@
+<style>
+    .table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    .table th,
+    .table td {
+        text-align: center;
+        vertical-align: middle;
+        border: 1px solid #ddd;
+        /* Añadir borde a celdas */
+    }
+
+    .table-striped tbody tr:nth-of-type(odd) {
+        background-color: rgba(0, 0, 0, .05);
+    }
+
+    .table-hover tbody tr:hover {
+        background-color: rgba(0, 0, 0, .075);
+    }
+
+    .table thead th {
+        background-color: <?php echo COLOR_FONDO; ?>;
+        color: <?php echo COLOR_LETRAS; ?>;
+    }
+
+    .centered {
+        text-align: center !important;
+        vertical-align: middle !important;
+    }
+</style>
+
+
