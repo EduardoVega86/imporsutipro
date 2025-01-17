32ae2c449cde01cb05b29aa945002a822d33a30e
Sebastian Ordoñez <desarrollo3imporfactory@gmail.com>
2025-01-17 09:49:02 -0500

    Editar boveda en controlador y modelo  Swagger - Sebastian

--- Models/SwaggerModel.php
+++ Models/SwaggerModel.php
@@ -322,7 +322,7 @@ class SwaggerModel extends Query
         }
     }
 
-    public function edtarBoveda($uuid, $id_boveda, $id_linea, $id_plataforma, $imagen, $id_produto, $ejemploLanding, $duplicarFunnel, $videos)
+    public function editarBoveda($uuid, $id_boveda, $id_linea, $id_plataforma, $imagen, $id_produto, $ejemploLanding, $duplicarFunnel, $videos)
     {
         try {
             // Verificar si existe usuario con ese UUID
