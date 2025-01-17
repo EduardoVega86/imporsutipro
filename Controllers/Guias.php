b2882a7c80312fab1462c8c098591796df7716ca
Jeimy Jara <desarrollo1@imporfactoryusa.com>
2025-01-17 15:46:27 -0500

    Eliminar producto

--- Controllers/Guias.php
+++ Controllers/Guias.php
@@ -384,6 +384,7 @@ class Guias extends Controller
 
         $nombre = $_POST['nombre'];
         $ciudad = $_POST['ciudad'];
+        $provincia = $_POST['provincia'];
         $ciudadDestino = $this->model->obtenerNombre($ciudad, "ciudad")[0]['ciudad'];
         $direccion = $_POST['calle_principal'] . " y " . $_POST['calle_secundaria'];
         $telefono = $_POST['telefono'];
@@ -423,7 +424,7 @@ class Guias extends Controller
             $response["status"] = 200;
             $this->model->aumentarMatriz();
             $flete_envio = $ciudad == 599 ? 5.5 : 6.5;
-            $response2 = $this->model->actualizarGuia($numero_factura, $response["guia"], $nombre, $ciudad, $direccion, $telefono, $celular, $referencia, $recaudo, $monto_factura, $observacion, $_SESSION["id"] ?? $_POST["id"], $_POST['calle_principal'], $_POST['calle_secundaria'], $contiene, $ciudad, $flete_envio, "SPEED", 2, $url_google_speed_pedido);
+            $response2 = $this->model->actualizarGuia($numero_factura, $response["guia"], $nombre, $ciudad, $direccion, $telefono, $celular, $referencia, $recaudo, $monto_factura, $observacion, $_SESSION["id"] ?? $_POST["id"], $_POST['calle_principal'], $_POST['calle_secundaria'], $contiene, $provincia, $flete_envio, "SPEED", 2, $url_google_speed_pedido);
             $this->model->asignarWallet($numero_factura, $response["guia"], $fecha, $nombre, $_SESSION["id_plataforma"] ?? $_POST["id_plataforma"], 1, $monto_factura, $recaudo, $flete_envio);
         } else {
             echo "error" . $response;
