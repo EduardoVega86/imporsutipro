1c2ba135cf912829cc56616f187ecdce70afc196
Sebastian Ordoñez <desarrollo3imporfactory@gmail.com>
2025-01-21 13:26:53 -0500

    Implementando editarCategoria  en Swagger controller y model

--- Models/SwaggerModel.php
+++ Models/SwaggerModel.php
@@ -915,6 +915,65 @@ class SwaggerModel extends Query
             ];
         }
     }
+    public function editarCategoria(
+        $uuid,
+        $id_categoria,
+        $nombre_linea,
+        $descripcion_linea,
+        $estado_linea,
+        $date_added,
+        $online,
+        $imagen,
+        $tipo,
+        $padre,
+        $orden
+    ) {
+        try {
+            // 1. Verificar si existe usuario con ese UUID
+            $usuario = $this->accesoModel->getUserByUUID($uuid);
+            if (empty($usuario)) {
+                return [
+                    'status'  => 404,
+                    'message' => "No existe un usuario con el UUID: $uuid"
+                ];
+            }
+
+            // 2. Obtener el ID de usuario y luego el ID de plataforma asociado
+            $id_users   = $usuario[0]['id_users'];
+            $plataforma = $this->accesoModel->getPlatformByUserId($id_users);
+
+            if (empty($plataforma) || !isset($plataforma[0]['id_plataforma'])) {
+                return [
+                    'status'  => 404,
+                    'message' => 'No se encontró la plataforma asociada al usuario'
+                ];
+            }
+
+            // 3. Obtener ID de plataforma
+            $idPlataforma = $plataforma[0]['id_plataforma'];
+
+            // 4. Llamar al método de productos para insertar la categoría
+            return $this->productosModel->editarCategoria(
+                $id_categoria,
+                $nombre_linea,
+                $descripcion_linea,
+                $estado_linea,
+                $date_added,
+                $online,
+                $imagen,
+                $tipo,
+                $padre,
+                $idPlataforma,
+                $orden
+            );
+        } catch (Exception $e) {
+            return [
+                'status'  => 500,
+                'message' => 'Error interno al agregar la categoría',
+                'error'   => $e->getMessage()
+            ];
+        }
+    }
 
 
 
