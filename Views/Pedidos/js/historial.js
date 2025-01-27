4b7a2b98b5230d26adbfb26263c3553a70a973c0
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2025-01-27 15:28:18 -0500

    boton de enviar mensaje automatizador

--- Views/Pedidos/js/historial.js
+++ Views/Pedidos/js/historial.js
@@ -128,13 +128,13 @@ const listHistorialPedidos = async () => {
       let boton_envia_automatizador;
       if (ID_PLATAFORMA == 3436) {
         boton_envia_automatizador = `<button class="btn btn-sm btn-danger" onclick="enviar_mensaje_automatizador(
-          ${historialPedido.nueva_factura},
+          ${historialPedido.numero_factura},
           ${historialPedido.ciudad_cot},
-          ${historialPedido.celular},
-          ${historialPedido.nombre},
-          ${historialPedido.c_principal},
-          ${historialPedido.c_secundaria},
-          ${historialPedido.contiene},
+          '${historialPedido.celular}',
+          '${historialPedido.nombre}',
+          '${historialPedido.c_principal}',
+          '${historialPedido.c_secundaria}',
+          '${historialPedido.contiene}',
           ${historialPedido.monto_factura}
         )"><i class="fa-brands fa-whatsapp"></i></button>`;
       }
