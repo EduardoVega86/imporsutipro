b1c265ea7a2c2b177f64988196ec0a6fd54d01c5
Sebastian Ordoñez <desarrollo3imporfactory@gmail.com>
2025-01-28 11:43:20 -0500

    Trabajando en seleccion provedores estilo dropi - sebastian

--- Views/Productos/marketplace_pro.php
+++ Views/Productos/marketplace_pro.php
@@ -1,5 +1,5 @@
 <?php require_once './Views/templates/header.php'; ?>
-<?php require_once './Views/Productos/css/marketplacepro_style.php'; ?>
+<?php require_once './Views/Productos/css/marketplace_style.php'; ?>
 
 <?php require_once './Views/Productos/Modales/descripcion_marketplace.php'; ?>
 <?php require_once './Views/Productos/Modales/Seleccion_productoAtributo.php'; ?>
@@ -7,29 +7,14 @@
 <?php require_once './Views/Pedidos/Modales/informacion_plataforma.php'; ?>
 
 <div class="custom-container-fluid mt-4" style="text-align: -webkit-center;">
-    <div style="padding-bottom: 20px; padding-top: 20px; width: 81%;">
+    <div style="padding-bottom: 20px; padding-top: 20px;">
         <div class="caja p-4 shadow-sm bg-white">
             <div class="caja_filtros">
-                <div class="slider-container">
-                    <div class="slider" id="proveedores-slider">
-                        <button class="slider-arrow left-arrow">&lt;</button>
-                        <div class="slider-content"></div>
-                        <button class="slider-arrow right-arrow">&gt;</button>
-                    </div>
-                </div>
-                <div class="slider-container">
-                    <div class="slider" id="categorias-slider">
-                        <button class="slider-arrow left-arrow">&lt;</button>
-                        <div class="slider-content"></div>
-                        <button class="slider-arrow right-arrow">&gt;</button>
-                    </div>
-                </div>
                 <div class="primer_seccionFiltro">
                     <div class="col-md-4 mb-3 mb-md-0">
-                        <input type="text" class="form-control" placeholder="Nombre:" id="buscar_nombre">
+                        <input type="text" class="form-control" placeholder="Nombre" id="buscar_nombre">
                     </div>
-
-                    <!-- <div class="col-md-2 mb-3 mb-md-0">
+                    <div class="col-md-2 mb-3 mb-md-0">
                         <select id="proveedor_filtroMarketplace" class="form-select me-2">
                             <option selected value="">Seleccione un proveedor</option>
                         </select>
@@ -38,7 +23,7 @@
                         <select id="categoria_filtroMarketplace" class="form-select me-2">
                             <option selected value="">Seleccione una categoría</option>
                         </select>
-                    </div> -->
+                    </div>
                     <div class="boton_favoritos">
                         <div class="form-check form-switch">
                             <input class="form-check-input" type="checkbox" id="favoritosSwitch">
@@ -74,6 +59,6 @@
 </div>
 
 
-<script src="<?php echo SERVERURL ?>/Views/Productos/js/marketplace_pro.js"></script>
+<script src="<?php echo SERVERURL ?>/Views/Productos/js/marketplace.js"></script>
 <script src="<?php echo SERVERURL ?>/Views/Productos/js/tablaSeleccion_Producto.js"></script>
 <?php require_once './Views/templates/footer.php'; ?>
