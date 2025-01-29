983dce04ba2353065b78d684d3c2142b9a2b2949
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2025-01-29 13:19:06 -0500

    depurando error tienda online

--- Models/UsuariosModel.php
+++ Models/UsuariosModel.php
@@ -1473,7 +1473,9 @@ ON
 
         $sql = "UPDATE perfil SET plantilla = ? WHERE id_plataforma = ?";
         $data = [$plantilla, $plataforma];
-        $editar_perfil = $this->update($sql, $data);
+        $editar_perfil = $this->dselect($sql, $data);
+
+        echo $editar_perfil;
         if ($editar_perfil == 1) {
             $response['status'] = 200;
             $response['title'] = 'Peticion exitosa';
