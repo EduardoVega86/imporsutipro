025892af75ba4e9115d8f0be7cfbf605da25d683
Sebastian Ordoñez <desarrollo3imporfactory@gmail.com>
2025-01-29 09:56:01 -0500

    Trabajando en seleccion provedores estilo dropi - sebastian

--- Views/Productos/js/marketplace_pro.js
+++ Views/Productos/js/marketplace_pro.js
@@ -546,10 +546,12 @@ document.addEventListener("DOMContentLoaded", function () {
     success: function (response) {
       console.log("Respuesta de obtener proveedores:", response);
       if (Array.isArray(response)) {
+        //Limitamosa mostrar 15
+        const limitedProveedores = response.slice(0,15);
         const sliderProveedores = document.getElementById("sliderProveedores");
         sliderProveedores.innerHTML = ""; // Limpia antes de insertar
 
-        response.forEach(function (proveedor) {
+        limitedProveedores.forEach(function (proveedor) {
           const chipProv = document.createElement("div");
           chipProv.classList.add("slider-chip");
           chipProv.textContent = proveedor.nombre_tienda.toUpperCase();
@@ -576,6 +578,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
           sliderProveedores.appendChild(chipProv);
         });
+              // Si deseas un botón "Ver todos"
+          // const verTodos = document.createElement("div");
+          // verTodos.classList.add("slider-chip");
+          // verTodos.textContent = "Ver todos";
+          // verTodos.addEventListener("click", function(){
+          //   window.location.href = SERVERURL + "ruta/proveedores_completos";
+          // });
+          // sliderProveedores.appendChild(verTodos);
       } else {
         console.log("La respuesta de la API no es un array:", response);
       }
