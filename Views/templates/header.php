d9bc9a5e76bea231eb76d89de1570578404d7a81
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2025-01-29 10:19:57 -0500

    cambios en menu para la seccion de automatizador

--- Views/templates/header.php
+++ Views/templates/header.php
@@ -195,9 +195,8 @@
                 <a href="<?php echo SERVERURL ?>Pedidos/scanner_speed"><i class='fa-solid fa-motorcycle menu-icon'></i> <span class="menu-text">Scanner Speed</span></a>
             <?php } ?>
 
-            <?php if ($_SESSION['id_plataforma'] == 2293 || $_SESSION['id_plataforma'] == 3436 || $_SESSION['id_plataforma'] == 1166  ||  $_SESSION["id_plataforma"] == 1251  || $_SESSION['id_plataforma'] == 1206 || $_SESSION['id_plataforma'] == 2307) { ?>
-                <a href="#" class="dropdown-btn" data-target="#submenu5"><i class='bx bxs-bot menu-icon'></i> <span class="menu-text">Automatizador</span></a>
-            <?php } ?>
+
+            <a href="#" class="dropdown-btn" data-target="#submenu5"><i class='bx bxs-bot menu-icon'></i> <span class="menu-text">Automatizador</span></a>
             <div class="submenu" id="submenu5">
                 <a href="<?php echo SERVERURL ?>Pedidos/plantillas_chat_center"><i class="fa-solid fa-message menu-icon"></i> <span class="menu-text">Plantillas</span></a>
                 <a href="<?php echo SERVERURL ?>/Pedidos/configuracion_chats_imporsuit"><i class="fa-solid fa-wrench menu-icon"></i> <span class="menu-text">Configuracion</span></a>
