f27cc42ef97a9f5bc9700982344001c7ffcdbfdc
Sebastian Ordoñez <desarrollo3imporfactory@gmail.com>
2025-01-29 10:04:08 -0500

    Trabajando en seleccion provedores estilo dropi - sebastian

--- Views/Productos/css/marketplacepro_style.php
+++ Views/Productos/css/marketplacepro_style.php
@@ -603,7 +603,7 @@
         display: flex;
         gap: 10px;
         overflow-x: auto;
-        max-width: 100%;
+        max-width: 768px;
         /* Evitar desbordes */
         box-sizing: border-box;
         border-radius: 8px;
