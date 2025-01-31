85d137ea5792a133fc13054a8381165c11bb837c
Sebastian Ordoñez <desarrollo3imporfactory@gmail.com>
2025-01-31 10:32:21 -0500

    Poniendo .. si contiene muchas categorias el proveedores para poder filtrar mejor - sEBASTIAN

--- Views/Productos/css/marketplacepro_style.php
+++ Views/Productos/css/marketplacepro_style.php
@@ -600,7 +600,7 @@
         display: flex;
         gap: 10px;
         overflow-x: auto;
-        max-width: 1670px;
+        max-width: auto;
         /* Evitar desbordes */
         box-sizing: border-box;
         border-radius: 8px;
