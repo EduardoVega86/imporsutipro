61351d4dd43dd8fb17cbbcf5f2bb164f76e3614b
Jeimy Jara <desarrollo1@imporfactoryusa.com>
2025-02-05 13:00:20 -0500

    Productos: fix

--- Views/Pedidos/js/agregar_productos_editarPedido.js
+++ Views/Pedidos/js/agregar_productos_editarPedido.js
@@ -13,7 +13,7 @@ const dataTableNuevosPedidosOptions = {
     {
       extend: "excelHtml5",
       text: 'Excel <i class="fa-solid fa-file-excel"></i>',
-      title: "Panel de Control: Productos",
+      title: "Panel de Control: Usuarios",
       titleAttr: "Exportar a Excel",
       exportOptions: {
         columns: [1, 2, 3, 4, 5, 6],
