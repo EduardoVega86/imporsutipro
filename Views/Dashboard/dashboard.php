b46f8eff7937d4be284f547e81eedcaf38f43f7a
Sebastian Ordoñez <desarrollo3imporfactory@gmail.com>
2025-02-06 16:35:40 -0500

    Trabajando en vista con cards en Dashboard - Sebastian

--- Views/Dashboard/dashboard.php
+++ Views/Dashboard/dashboard.php
@@ -11,7 +11,7 @@
                 <div class="row mb-4">
                     <div class="flex-fill" style="padding: 10px;">
                         <div class="input-group">
-                            <h6>Seleccione el rango de fechas:</h6>
+                            <h6 class="h6">Seleccione el rango de fechas:</h6>
                             <input type="text" class="form-control" id="daterange">
                             <span class="input-group-text"><i class="fa fa-calendar" aria-hidden="true"></i></span>
                         </div>
