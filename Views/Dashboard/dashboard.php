d3a8e7fbae4fec9e13d2d769e371a7394d194a5b
Sebastian Ordoñez <desarrollo3imporfactory@gmail.com>
2025-02-07 11:30:52 -0500

    Trabajando Diagrama Ventas ultimo mes en vista  Dashboard - Sebastian

--- Views/Dashboard/dashboard.php
+++ Views/Dashboard/dashboard.php
@@ -87,12 +87,12 @@
         <div class="tablas_estaditicas">
             <div class="content-container">
                 <div class="content-box">
-                    <h3 class="display-1 fw-bold">Ventas del Último Mes</h3>
+                    <h3 class="fw-bold">Ventas del último Mes</h3>
                     <canvas id="salesChart"></canvas>
                 </div>
                 <!-- tabla ultimos pedidos -->
                 <div class="content-box">
-                    <h3>Últimos Pedidos</h3>
+                    <h3 class="fw-bold">Últimos pedidos</h3>
                     <div class="table-responsive">
                         <table class="table">
                             <thead>
@@ -111,7 +111,7 @@
                 </div>
                 <!-- fin de tabla ultimos pedidos -->
                 <div class="content-box" id="pie-chart-container" style="text-align: -webkit-center;">
-                    <h3>Distribución de estados de guías de envío</h3>
+                    <h3 class="fw-bold">Distribución de estados en guías de envío</h3>
                     <canvas id="distributionChart" width="400" height="200"></canvas>
                 </div>
             </div>
