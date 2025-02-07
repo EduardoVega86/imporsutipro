1680fd7d1b2b9365d969ed7fcb31435c74443b1b
Tony Plaza - Desarrollador <desarrollo2imporfactory@gmail.com>
2025-02-07 14:25:58 -0500

    seccion de pedidos sin productos

---
+++ Views/Funnelish/js/pedidos_sin_producto.js
@@ -0,0 +1,122 @@
+let dataTablePedidosSinProducto;
+let dataTablePedidosSinProductoIsInitialized = false;
+
+const dataTablePedidosSinProductoOptions = {
+  columnDefs: [
+    { className: "centered", targets: [1, 2, 3, 4, 5] },
+    { orderable: false, targets: 0 }, // ocultar para columna 0 el ordenar columna
+  ],
+  pageLength: 10,
+  destroy: true,
+  language: {
+    lengthMenu: "Mostrar _MENU_ registros por página",
+    zeroRecords: "Ningún pedido encontrado",
+    info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+    infoEmpty: "Ningún pedido encontrado",
+    infoFiltered: "(filtrados desde _MAX_ registros totales)",
+    search: "Buscar:",
+    loadingRecords: "Cargando...",
+    paginate: {
+      first: "Primero",
+      last: "Último",
+      next: "Siguiente",
+      previous: "Anterior",
+    },
+  },
+};
+
+const initDataTablePedidosSinProducto = async () => {
+  if (dataTablePedidosSinProductoIsInitialized) {
+    dataTablePedidosSinProducto.destroy();
+  }
+
+  await listPedidosSinProducto();
+
+  dataTablePedidosSinProducto = $(
+    "#datatable_pedidos_sin_producto"
+  ).DataTable(dataTablePedidosSinProductoOptions);
+
+  dataTablePedidosSinProductoIsInitialized = true;
+};
+
+const listPedidosSinProducto = async () => {
+  try {
+    const formData = new FormData();
+    formData.append("fecha_inicio", fecha_inicio);
+    formData.append("fecha_fin", fecha_fin);
+
+    const response = await fetch(`${SERVERURL}pedidos/cargar_pedidos_sin_producto`, {
+      method: "POST",
+      body: formData,
+    });
+    const pedidosSinProducto = await response.json();
+
+    let content = ``;
+
+    pedidosSinProducto.forEach((pedido, index) => {
+      let editar = "";
+      let placa = "";
+      if (pedido.cargo_users == 35) {
+        editar = `<button class="btn btn-sm btn-primary" onclick="abrir_editar_motorizado(${pedido.id_users})"><i class="fa-solid fa-pencil"></i>Editar</button>`;
+
+        placa = `<i class="fa-solid fa-store" style='cursor:pointer' onclick="abrir_modal_subirPlaca(${pedido.id_users})"></i>`;
+      } else {
+        editar = `<button class="btn btn-sm btn-primary" onclick="abrir_editar_usuario(${pedido.id_users})"><i class="fa-solid fa-pencil"></i>Editar</button>`;
+      }
+
+      content += `
+                <tr>
+                <td>${pedido.id_users}</td>
+                <td>${pedido.nombre_users}</td>
+                <td>${pedido.usuario_users}</td>
+                <td>${pedido.email_users}</td>
+                <td>
+                <a href="https://wa.me/${formatPhoneNumber(
+                  pedido.whatsapp
+                )}" target="_blank" style="font-size: 45px; vertical-align: middle; margin-left: 10px;" target="_blank">
+                <i class='bx bxl-whatsapp-square' style="color: green;"></i>
+                </a></td>
+                <td>${pedido.nombre_tienda}</td>
+                <td>${pedido.date_added}</td>
+                <td>${placa}</td>
+                <td>
+                ${editar}
+                <button class="btn btn-sm btn-danger" onclick="eliminar_usuario(${
+                  pedido.id_users
+                })"><i class="fa-solid fa-trash-can"></i>Borrar</button>
+                </td>
+                </tr>`;
+    });
+    document.getElementById("tableBody_pedidos_sin_producto").innerHTML =
+      content;
+  } catch (ex) {
+    alert(ex);
+  }
+};
+
+window.addEventListener("load", async () => {
+  await initDataTablePedidosSinProducto();
+});
+
+function formatPhoneNumber(number) {
+  // Eliminar caracteres no numéricos excepto el signo +
+  number = number.replace(/[^\d+]/g, "");
+
+  // Verificar si el número ya tiene el código de país +593
+  if (/^\+593/.test(number)) {
+    // El número ya está correctamente formateado con +593
+    return number;
+  } else if (/^593/.test(number)) {
+    // El número tiene 593 al inicio pero le falta el +
+    return "+" + number;
+  } else {
+    // Si el número comienza con 0, quitarlo
+    if (number.startsWith("0")) {
+      number = number.substring(1);
+    }
+    // Agregar el código de país +593 al inicio del número
+    number = "+593" + number;
+  }
+
+  return number;
+}
---
+++ Views/Funnelish/pedidos_sin_producto.php
@@ -0,0 +1,97 @@
+<?php require_once './Views/templates/header.php'; ?>
+<?php require_once './Views/Funnelish/css/pedidos_sin_producto_style.php'; ?>
+
+<div class="custom-container-fluid">
+    <div class="container mt-5" style="max-width: 1600px;">
+        <h2 class="text-center mb-4">Lista de Usuarios</h2>
+
+        <div class="primer_seccionFiltro" style="width: 100%;">
+            <div class="d-flex flex-row align-items-end filtro_fecha">
+                <div class="flex-fill">
+                    <h6>Seleccione el rango de fechas:</h6>
+                    <div class="input-group">
+                        <input type="text" class="form-control" id="daterange">
+                        <span class="input-group-text"><i class="fa fa-calendar" aria-hidden="true"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="table-responsive">
+            <!-- <table class="table table-bordered table-striped table-hover"> -->
+            <table id="datatable_obtener_usuarios_plataforma" class="table table-striped">
+                <thead>
+                    <tr>
+                        <th class="centered">ID</th>
+                        <th class="centered">Nombres</th>
+                        <th class="centered">Usuario</th>
+                        <th class="centered">Email</th>
+                        <th class="centered">Telefono</th>
+                        <th class="centered">Tienda</th>
+                        <th class="centered">Agregado</th>
+                        <th class="centered">Agregar Placa</th>
+                        <th class="centered">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody id="tableBody_obtener_usuarios_plataforma"></tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<script>
+    let fecha_inicio = "";
+    let fecha_fin = "";
+
+    // Calcula la fecha de inicio (hace 7 días) y la fecha de fin (hoy)
+    let hoy = moment();
+    let haceUnaSemana = moment().subtract(6, 'days'); // Rango de 7 días
+
+    // Asignar las fechas a las variables al cargar la página
+    fecha_inicio = haceUnaSemana.format('YYYY-MM-DD') + ' 00:00:00';
+    fecha_fin = hoy.format('YYYY-MM-DD') + ' 23:59:59';
+
+    $(function() {
+        $('#daterange').daterangepicker({
+            opens: 'right',
+            startDate: haceUnaSemana, // Fecha de inicio predefinida
+            endDate: hoy, // Fecha de fin predefinida
+            locale: {
+                format: 'YYYY-MM-DD',
+                separator: ' - ',
+                applyLabel: 'Aplicar',
+                cancelLabel: 'Cancelar',
+                fromLabel: 'Desde',
+                toLabel: 'Hasta',
+                customRangeLabel: 'Custom',
+                weekLabel: 'S',
+                daysOfWeek: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],
+                monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+                firstDay: 1
+            },
+            autoUpdateInput: true // Actualiza el input automáticamente
+        });
+
+        // Evento que se dispara cuando se aplica un nuevo rango de fechas
+        $('#daterange').on('apply.daterangepicker', function(ev, picker) {
+            // Actualiza el valor del input con el rango de fechas seleccionado
+            $(this).val(picker.startDate.format('YYYY-MM-DD') + ' - ' + picker.endDate.format('YYYY-MM-DD'));
+
+            // Actualizar las variables con las nuevas fechas seleccionadas
+            fecha_inicio = picker.startDate.format('YYYY-MM-DD') + ' 00:00:00';
+            fecha_fin = picker.endDate.format('YYYY-MM-DD') + ' 23:59:59';
+            initDataTable();
+        });
+
+        // Establece los valores iniciales en el input de fechas
+        $('#daterange').val(haceUnaSemana.format('YYYY-MM-DD') + ' - ' + hoy.format('YYYY-MM-DD'));
+    });
+
+    $(document).ready(function() {
+        // Inicializa la tabla cuando cambian los selectores
+        $("#tienda_q,#estado_q,#transporte,#impresion,#despachos").change(function() {
+            initDataTable();
+        });
+    });
+</script>
+<script src="<?php echo SERVERURL ?>/Views/Funnelish/js/pedidos_sin_producto.js"></script>
+<?php require_once './Views/templates/footer.php'; ?>
