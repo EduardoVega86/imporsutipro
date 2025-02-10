57324067f1b8cb75eef91190156ccecbd6b96027
Sebastian Ordoñez <desarrollo3imporfactory@gmail.com>
2025-02-10 12:26:50 -0500

    Fixiando porque no muestra estado factura 3 en guias - Sebastian

--- Models/PedidosModel.php
+++ Models/PedidosModel.php
@@ -133,7 +133,7 @@ class PedidosModel extends Query
         }
 
         $sql .= " ORDER BY fc.numero_factura DESC;";
-        echo json_encode(["sql" => $sql]); // forzando salida
+        error_log("CONSULTA FINAL: " . $sql);
 
         return $this->select($sql);
     }
