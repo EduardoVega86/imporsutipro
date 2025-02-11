9d69ee92fe2859b6749c052a30e188f9cc6c5cc9
Sebastian Ordoñez <desarrollo3imporfactory@gmail.com>
2025-02-11 12:09:56 -0500

    Aplicando filtro de cargando con  guias administrador3- Sebastian

--- Views/Pedidos/guias_administrador_3.php
+++ Views/Pedidos/guias_administrador_3.php
@@ -88,12 +88,11 @@
         <div style="padding-top: 20px;">
             <button id="btnAplicarFiltros" class="btn btn-primary">Aplicar Filtros</button>
             <button id="imprimir_guias" class="btn btn-success">Generar Impresion</button>
-        </div>
-
-        <!-- Loader que se mostrará únicamente sobre el área de la tabla -->
-        <div id="tableLoader" style="display: none;">
-            <div class="spinner-border text-primary" role="status">
-                <span class="visually-hidden">Cargando...</span>
+            <!-- Loader que se mostrará únicamente sobre el área de la tabla -->
+            <div id="tableLoader" style="display: none;">
+                <div class="spinner-border text-primary" role="status">
+                    <span class="visually-hidden">Cargando...</span>
+                </div>
             </div>
         </div>
         <div class="table-container" style="position: relative;">
