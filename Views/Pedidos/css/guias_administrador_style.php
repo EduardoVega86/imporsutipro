eee01193fdf4895ede67fdbd33047181319e0b5f
Sebastian Ordoñez <desarrollo3imporfactory@gmail.com>
2025-02-11 12:33:09 -0500

    Aplicando filtro de cargando con  guias administrador3- Sebastian

--- Views/Pedidos/css/guias_administrador_style.php
+++ Views/Pedidos/css/guias_administrador_style.php
@@ -174,8 +174,8 @@
 
     #tableLoader {
         position: absolute;
-        top: 42px;
-        left: -432px;
+        top: -18px;
+        left: -340px;
         width: 100%;
         height: 100%;
         background: rgba(255, 255, 255, 0.8);
