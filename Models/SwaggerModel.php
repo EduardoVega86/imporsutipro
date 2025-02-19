53e8a8804c20fa150953898e6811d00f92ba16ed
Sebastian Ordoñez <desarrollo3imporfactory@gmail.com>
2025-02-19 14:40:47 -0500

    Documentacion Swagger controller y model de Editar  Producto - Sebastian

--- Models/SwaggerModel.php
+++ Models/SwaggerModel.php
@@ -1598,4 +1598,92 @@ class SwaggerModel extends Query
             ];
         }
     }
+
+    public function editarProducto(
+        $uuid,
+        $id_producto,
+        $codigo_producto,
+        $nombre_producto,
+        $descripcion_producto,
+        $id_linea_producto,
+        $inv_producto,
+        $producto_variable,
+        $aplica_iva,
+        $estado_producto,
+        $id_imp_producto,
+        $pagina_web,
+        $formato,
+        $drogshipin,
+        $destacado,
+        $envio_prioritario,
+        $enlace_funnelish,
+        $stock_inicial,
+        $bodega,
+        $pcp,
+        $pvp,
+        $costo_producto,
+        $date_added,
+        $image_path,
+        $pref
+    ) {
+        try {
+            // 1. Verificar si existe usuario con ese UUID
+            $usuario = $this->accesoModel->getUserByUUID($uuid);
+            if (empty($usuario)) {
+                return [
+                    'status'  => 404,
+                    'message' => "No existe un usuario con el UUID: $uuid"
+                ];
+            }
+
+            // 2. Obtener el ID de usuario y luego el ID de plataforma asociado
+            $id_users   = $usuario[0]['id_users'];
+            $plataforma = $this->accesoModel->getPlatformByUserId($id_users);
+
+            if (empty($plataforma) || !isset($plataforma[0]['id_plataforma'])) {
+                return [
+                    'status'  => 404,
+                    'message' => 'No se encontró la plataforma asociada al usuario'
+                ];
+            }
+
+            // 3. Obtener ID de plataforma
+            $idPlataforma = $plataforma[0]['id_plataforma'];
+
+            // 4. Llamar al método de productos para insertar la categoría
+            return $this->productosModel->editarProducto(
+                $id_producto,
+                $codigo_producto,
+                $nombre_producto,
+                $descripcion_producto,
+                $id_linea_producto,
+                $inv_producto,
+                $producto_variable,
+                $costo_producto,
+                $aplica_iva,
+                $estado_producto,
+                $date_added,
+                $image_path,
+                $id_imp_producto,
+                $pagina_web,
+                $formato,
+                $drogshipin,
+                $destacado,
+                $idPlataforma,
+                $stock_inicial,
+                $bodega,
+                $pcp,
+                $pvp,
+                $pref,
+                $enlace_funnelish,
+                $envio_prioritario
+            );
+        } catch (Exception $e) {
+            return [
+                'status'  => 500,
+                'message' => 'Error interno al editar el producto',
+                'error'   => $e->getMessage()
+            ];
+        }
+    }
 }
