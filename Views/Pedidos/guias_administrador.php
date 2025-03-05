6cbdcbd177f6795a4c90282212b820001d3096a9
Sebastian Ordoñez <desarrollo3imporfactory@gmail.com>
2025-03-05 14:13:59 -0500

    Reordenando datos al descargar excel en guias administrador 1  - Sebastian

--- Views/Pedidos/guias_administrador.php
+++ Views/Pedidos/guias_administrador.php
@@ -259,5 +259,20 @@
             </table>
         </div>
     </div>
+    <!-- jQuery primero (por si tu DataTables lo requiere) -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- DataTables núcleo -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" />
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+
+    <!-- DataTables Buttons -->
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.dataTables.min.css" />
+    <!-- 1) JSZip, para permitir exportación a Excel (fundamental) -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+
+    <!-- 2) Librería de exportación a HTML5 (Excel, CSV, etc.) -->
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
     <script src="<?php echo SERVERURL ?>/Views/Pedidos/js/guias_administrador.js"></script>
     <?php require_once './Views/templates/footer.php'; ?>
