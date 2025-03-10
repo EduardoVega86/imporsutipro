d1854eab684b15011dcd29b5cb6218c824478955
Sebastian Ordoñez <desarrollo3imporfactory@gmail.com>
2025-03-10 16:51:08 -0500

    Indicando si una guia fue pagada o no en vista guias en header - Sebastian

--- Controllers/Pedidos.php
+++ Controllers/Pedidos.php
@@ -1223,7 +1223,7 @@ class Pedidos extends Controller
             $sheet->setCellValue("P{$fila}", ($guia['cod'] == 1 ? 'SI' : 'NO'));
 
             // Recaudo => SI/Pendiente
-            $sheet->setCellValue("Q{$fila}", (intval($guia['pagado']) == 1 ? 'ACREDITADA' : $guia['pagado']));
+            $sheet->setCellValue("Q{$fila}", ($guia['pagado'] == 1 ? 'SI' : 'NO'));
 
             $fila++;
         }
