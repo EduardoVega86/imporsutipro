e83fa0066a2d25ed01abf3252d370feeeaa4a9ee
Tony Plaza - Desarrollador <desarrollo2imporfactory@gmail.com>
2025-03-13 10:03:57 -0500

    añadir seccion de agregar dominio

--- Views/Usuarios/tienda_online.php
+++ Views/Usuarios/tienda_online.php
@@ -829,11 +829,11 @@
 
         <div class="accordion-item">
             <h2 class="accordion-header" id="headingFive">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
                     DOMINIO
                 </button>
             </h2>
-            <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
+            <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
                     <button class="btn btn-success" id="agregar_dominio" onclick="guardar_dominio()"></i>Guardar cambios</button>
                     <div class="row mb-3">
