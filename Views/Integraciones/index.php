af7c78bad22c424122763c91395e6a1b6d8b468c
Tony Plaza - Desarrollador <desarrollo2imporfactory@gmail.com>
2025-03-17 12:40:42 -0500

    cambios en integraciones

--- Views/Integraciones/index.php
+++ Views/Integraciones/index.php
@@ -43,23 +43,30 @@
             </div>
         </div>
 
-        <!-- Clarity -->
+        <!-- Shopify -->
         <div class="col-md-4">
             <div class="card-custom">
-                <img src="https://cdn.worldvectorlogo.com/logos/microsoft-clarity.svg" alt="Clarity Logo">
-                <h5 class="card-title">Clarity</h5>
+                <img src="<?php echo SERVERURL; ?>/public/img/facebook.png" alt="Facebook Logo">
+                <h5 class="card-title">Shopify</h5>
                 <p class="card-text">Manten actualizadas tus campañas con nuestra api.</p>
-                <p class="status connected">Conectado</p>
-                <button class="btn btn-primary">Conectar</button>
+                <input type="hidden" id="conectado_facebook" name="conectado_facebook">
+                <div id="conectarFacebook" style="display: none;">
+                    <p class="status connected">Conectado</p>
+                </div>
+                <div id="desconectarFacebook" style="display: none;">
+                    <p class="status disconnected">Desconectado</p>
+                </div>
+
+                <button class="btn btn-primary" onclick="abrirmodal_facebook()">Conectar</button>
             </div>
         </div>
     </div>
     <div class="row justify-content-center">
-        <!-- Facebook -->
+        <!-- Funnelish -->
         <div class="col-md-4">
             <div class="card-custom">
                 <img src="<?php echo SERVERURL; ?>/public/img/facebook.png" alt="Facebook Logo">
-                <h5 class="card-title">Facebook</h5>
+                <h5 class="card-title">Funnelish</h5>
                 <p class="card-text">Manten actualizadas tus campañas con nuestra api.</p>
                 <input type="hidden" id="conectado_facebook" name="conectado_facebook">
                 <div id="conectarFacebook" style="display: none;">
