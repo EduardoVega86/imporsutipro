51bb3dbbde2e9161bcb61de8eda2d5deb51bffff
Tony Plaza - Desarrollador <desarrollo2imporfactory@gmail.com>
2025-03-17 12:38:12 -0500

    cambios en integraciones

--- Views/Integraciones/index.php
+++ Views/Integraciones/index.php
@@ -54,6 +54,55 @@
             </div>
         </div>
     </div>
+    <div class="row justify-content-center">
+        <!-- Facebook -->
+        <div class="col-md-4">
+            <div class="card-custom">
+                <img src="<?php echo SERVERURL; ?>/public/img/facebook.png" alt="Facebook Logo">
+                <h5 class="card-title">Facebook</h5>
+                <p class="card-text">Manten actualizadas tus campañas con nuestra api.</p>
+                <input type="hidden" id="conectado_facebook" name="conectado_facebook">
+                <div id="conectarFacebook" style="display: none;">
+                    <p class="status connected">Conectado</p>
+                </div>
+                <div id="desconectarFacebook" style="display: none;">
+                    <p class="status disconnected">Desconectado</p>
+                </div>
+
+                <button class="btn btn-primary" onclick="abrirmodal_facebook()">Conectar</button>
+            </div>
+        </div>
+
+        <!-- Vacio -->
+        <div class="col-md-4">
+            <!-- <div class="card-custom">
+                <img src="<?php echo SERVERURL; ?>/public/img/tiktok.png" alt="TikTok Logo">
+                <h5 class="card-title">TikTok</h5>
+                <p class="card-text">Manten actualizadas tus campañas con nuestra api.</p>
+                <input type="hidden" id="conectado_tiktok" name="conectado_tiktok">
+
+                <div id="conectarTiktok" style="display: none;">
+                    <p class="status connected">Conectado</p>
+                </div>
+                <div id="desconectarTiktok" style="display: none;">
+                    <p class="status disconnected">Desconectado</p>
+                </div>
+
+                <button class="btn btn-primary" onclick="abrirmodal_tiktok()">Conectar</button>
+            </div> -->
+        </div>
+
+        <!-- Clarity -->
+        <div class="col-md-4">
+            <!-- <div class="card-custom">
+                <img src="https://cdn.worldvectorlogo.com/logos/microsoft-clarity.svg" alt="Clarity Logo">
+                <h5 class="card-title">Clarity</h5>
+                <p class="card-text">Manten actualizadas tus campañas con nuestra api.</p>
+                <p class="status connected">Conectado</p>
+                <button class="btn btn-primary">Conectar</button>
+            </div> -->
+        </div>
+    </div>
 </div>
 
 <script src="<?php echo SERVERURL ?>Views/Integraciones/js/integraciones.js"></script>
