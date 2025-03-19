f73ffc49562b6ac491062444a638b86303db8e56
Tony Plaza - Desarrollador <desarrollo2imporfactory@gmail.com>
2025-03-19 10:31:34 -0500

    depurando error anular guias

--- Models/PedidosModel.php
+++ Models/PedidosModel.php
@@ -1069,9 +1069,13 @@ class PedidosModel extends Query
 
     public function cargarAnuladas($filtro)
     {
-        $sql = "SELECT * FROM facturas_cot where anulado = 1";
-
-        return $this->select($sql);
+        try {
+            $sql = "SELECT * FROM facturas_cot WHERE anulado = 1";
+            return $this->select($sql);
+        } catch (Exception $e) {
+            error_log("Error en cargarAnuladas: " . $e->getMessage());
+            return [];
+        }
     }
 
     public function nuevo_pedido($fecha_factura, $id_usuario, $monto_factura, $estado_factura, $nombre_cliente, $telefono_cliente, $c_principal, $ciudad_cot, $c_secundaria, $referencia, $observacion, $guia_enviada, $transporte, $identificacion, $celular, $id_producto_venta, $dropshipping, $id_plataforma, $dueño_id, $importado, $plataforma_importa, $cod, $estado_guia_sistema, $impreso, $facturada, $factura_numero, $numero_guia, $anulada, $identificacionO, $celularO, $nombreO, $ciudadO, $provinciaO, $direccionO, $referenciaO, $numeroCasaO, $valor_segura, $no_piezas, $tipo_servicio, $peso, $contiene, $costo_flete, $costo_producto, $comentario, $id_transporte, $provincia, $id_bodega, $nombre_responsable)
