8cf8400bd210a12e776c2a780c3c113a09ae9ad5
Sebastian Ordoñez <desarrollo3imporfactory@gmail.com>
2025-03-20 16:45:41 -0500

    Header dashboard - Sebastian

--- Views/Dashboard/dashboard.php
+++ Views/Dashboard/dashboard.php
@@ -1,5 +1,5 @@
 <?php require_once './Views/templates/header.php'; ?>
-<?php require_once './Views/Dashboard/css/dashboard_style.php'; ?>
+<?php require_once './Views/Dashboard/css/dashboard2_style.php'; ?>
 
 <!-- Agregar CDN de Boxicons -->
 <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
@@ -12,22 +12,52 @@
                     <div class="flex-fill" style="padding: 10px;">
                         <div class="input-group">
                             <h6 class="h6">Seleccione el rango de fechas:</h6>
-                            <input type="text" class="form-control" id="daterange">
+                            <input type="text" class="form-control" id="daterange" style="margin-left: 10px">
                             <span class="input-group-text"><i class="fa fa-calendar" aria-hidden="true"></i></span>
                         </div>
                     </div>
                     <!-- Card: Total Vendido -->
-                    <div class="col-md-4">
+                    <div class="col-md-3">
                         <div class="card shadow-sm p-3 text-center" style="background: white; border-left: 5px solid #28a745;">
                             <h5 class="text-success">
-                                <i class="bx bx-money"></i> Valor de Pedidos
+                                <i class="bx bx-money"></i> Valor Total
                                 <i class="bx bx-help-circle text-muted" data-toggle="tooltip" title="Valor total de pedidos"></i>
                             </h5>
-                            <h3 class="font-weight-bold" id="total_ventas"></h3>
+                            <h3 class="font-weight-bold" id="total_ventas">0</h3>
+                        </div>
+                    </div>
+                    <!-- Card: Total Guias Generadas -->
+                    <div class="col-md-3">
+                        <div class="card shadow-sm p-3 text-center" style="background: white; border-left: 5px solid #ffc107;">
+                            <h5 class="text-warning">
+                                <i class="bx bx-package"></i> Guías Generadas
+                                <i class="bx bx-help-circle text-muted" data-toggle="tooltip" title="Cantidad total de guías generadas"></i>
+                            </h5>
+                            <h3 class="font-weight-bold" id="total_guias">0</h3>
+                        </div>
+                    </div>
+                    <!-- Card: Total Recaudo -->
+                    <div class="col-md-3">
+                        <div class="card shadow-sm p-3 text-center" style="background: white; border-left: 5px solid #17a2b8;">
+                            <h5 class="text-info">
+                                <i class="bx bx-wallet"></i> Utilidad Total
+                                <i class="bx bx-help-circle text-muted" data-toggle="tooltip" title="Monto total recaudado"></i>
+                            </h5>
+                            <h3 class="font-weight-bold" id="total_recaudo">0</h3>
+                        </div>
+                    </div>
+                    <!-- Card: Pedidos Entregados -->
+                    <div class="col-md-3">
+                        <div class="card shadow-sm p-3 text-center" style="background: white; border-left: 5px solid #28a745;">
+                            <h5 class="text-success">
+                                <i class="bx bx-package"></i> Guías Entregadas
+                                <i class="bx bx-help-circle text-muted" data-toggle="tooltip" title="Cantidad total de pedidos entregados"></i>
+                            </h5>
+                            <h3 class="font-weight-bold" id="total_entregado">0</h3>
                         </div>
                     </div>
                     <!-- Card: Total Pedidos -->
-                    <div class="col-md-4">
+                    <!-- <div class="col-md-4">
                         <div class="card shadow-sm p-3 text-center" style="background: white; border-left: 5px solid #007bff;">
                             <h5 class="text-primary">
                                 <i class="bx bx-money"></i> Total de Pedidos
@@ -35,21 +65,11 @@
                             </h5>
                             <h3 class="font-weight-bold" id="total_pedidos">0</h3>
                         </div>
-                    </div>
-                    <!-- Card: Total Guias Generadas -->
-                    <div class="col-md-4">
-                        <div class="card shadow-sm p-3 text-center" style="background: white; border-left: 5px solid #ffc107;">
-                            <h5 class="text-warning">
-                                <i class="bx bx-package"></i> Total Guías
-                                <i class="bx bx-help-circle text-muted" data-toggle="tooltip" title="Cantidad total de guías generadas"></i>
-                            </h5>
-                            <h3 class="font-weight-bold" id="total_guias">0</h3>
-                        </div>
-                    </div>
+                    </div> -->
                 </div>
-                <div class="row mb-4">
-                    <!-- Card: Total Recaudo -->
-                    <div class="col-md-4">
+                <!-- <div class="row mb-4"> -->
+                <!-- Card: Total Recaudo -->
+                <!-- <div class="col-md-4">
                         <div class="card shadow-sm p-3 text-center" style="background: white; border-left: 5px solid #17a2b8;">
                             <h5 class="text-info">
                                 <i class="bx bx-wallet"></i> Total Utilidad
@@ -57,9 +77,9 @@
                             </h5>
                             <h3 class="font-weight-bold" id="total_recaudo"></h3>
                         </div>
-                    </div>
-                    <!-- Card: Total Fletes -->
-                    <div class="col-md-4">
+                    </div> -->
+                <!-- Card: Total Fletes -->
+                <!-- <div class="col-md-4">
                         <div class="card shadow-sm p-3 text-center" style="background: white; border-left: 5px solid #fd7e14;">
                             <h5 style="color: #fd7e14;">
                                 <i class="bx bx-dollar"></i> Total Fletes
@@ -67,9 +87,9 @@
                             </h5>
                             <h3 class="font-weight-bold" id="total_fletes">0</h3>
                         </div>
-                    </div>
-                    <!-- Card: Devoluciones -->
-                    <div class="col-md-4">
+                    </div> -->
+                <!-- Card: Devoluciones -->
+                <!-- <div class="col-md-4">
                         <div class="card shadow-sm p-3 text-center" style="background: white; border-left: 5px solid #dc3545;">
                             <h5 class="text-danger">
                                 <i class="bx bx-dollar"></i> Devoluciones
@@ -77,21 +97,20 @@
                             </h5>
                             <h3 class="font-weight-bold" id="devoluciones">0</h3>
                         </div>
-                    </div>
-                </div>
-            </div>
-            <div class="slider-container">
-                <img src="<?php echo BANNER_INICIO; ?>" alt="Slider">
+                    </div> -->
+                <!-- </div> -->
             </div>
         </div>
         <div class="tablas_estaditicas">
             <div class="content-container">
                 <div class="content-box">
-                    <h3 class="fs-5 fw-bold">Ventas del último Mes</h3>
+                    <h3 class="fs-5 fw-bold">Rendimiento</h3>
                     <canvas id="salesChart"></canvas>
                 </div>
-                <!-- tabla ultimos pedidos -->
-                <div class="content-box">
+                <!-- div adicional para cerrar lo que usamos -->
+            </div>
+            <!-- tabla ultimos pedidos -->
+            <!-- <div class="content-box">
                     <h3 class="fs-5 fw-bold">Últimos pedidos</h3>
                     <div class="table-responsive">
                         <table class="table">
@@ -102,22 +121,22 @@
                                     <th>Monto</th>
                                 </tr>
                             </thead>
-                            <tbody id="facturas-body">
-                                <!-- Aquí se cargarán los datos dinámicamente -->
-                            </tbody>
+                            <tbody id="facturas-body"> -->
+            <!-- Aquí se cargarán los datos dinámicamente -->
+            <!-- </tbody>
                         </table>
-                    </div>
-                    <!-- <button class="btn btn-primary">Ver todas las Ventas</button> -->
-                </div>
-                <!-- fin de tabla ultimos pedidos -->
-                <div class="content-box" id="pie-chart-container" style="text-align: -webkit-center;">
+                    </div> -->
+            <!-- <button class="btn btn-primary">Ver todas las Ventas</button> -->
+            <!-- </div> -->
+            <!-- fin de tabla ultimos pedidos -->
+            <!-- <div class="content-box" id="pie-chart-container" style="text-align: -webkit-center;">
                     <h3 class="fs-5 fw-bold">Distribución de estados en guías de envío</h3>
                     <canvas id="distributionChart" width="400" height="200"></canvas>
-                </div>
-            </div>
-        </div>
+                </div> -->
+            <!-- </div>
+        </div> -->
 
-        <div class="tablas_estaditicas">
+            <!-- <div class="tablas_estaditicas">
             <div class="content-container">
                 <div class="d-flex flex-column seccion_dashboard1">
                     <div class="d-flex flex-row">
@@ -136,13 +155,13 @@
                             <p>Flete Promedio</p>
                         </div>
                     </div>
-                    <!-- Sección de Productos por cantidad -->
+                    Sección de Productos por cantidad
                     <div class="content-box1 productos" style="height: 100%;">
                         <h4>Productos por cantidad</h4>
                         <div id="products-container"></div>
-                    </div>
-                    <!-- Fin Sección de Productos por cantidad -->
-                </div>
+                    </div> -->
+            <!-- Fin Sección de Productos por cantidad -->
+            <!-- </div>
 
                 <div class="d-flex flex-column seccion_dashboard1">
                     <div class="d-flex flex-row">
@@ -160,50 +179,50 @@
                             <h3> <span id="tiempo_promedio_entrega">30 Horas</span></h3>
                             <p>Tiempo Promedio Entrega</p>
                         </div>
-                    </div>
-                    <!-- Sección de Ciudades con más despachos -->
-                    <div class="content-box1 ciudades" style="height: 100%;">
+                    </div> -->
+            <!-- Sección de Ciudades con más despachos -->
+            <!-- <div class="content-box1 ciudades" style="height: 100%;">
                         <h4>Ciudades con más despachos</h4>
                         <div id="ciudades-container"></div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="tablas_estaditicas">
-            <div class="content-container">
-                <!-- Sección de productos_entrega por cantidad -->
-                <div class="content-box1 productos_entrega seccion_dashboard1">
+        </div> -->
+            <!-- <div class="tablas_estaditicas">
+            <div class="content-container"> -->
+            <!-- Sección de productos_entrega por cantidad -->
+            <!-- <div class="content-box1 productos_entrega seccion_dashboard1">
                     <h4>Productos entregados por cantidad</h4>
                     <div id="productsEntregados-container"></div>
-                </div>
-                <!-- Fin Sección de productos_entrega por cantidad -->
-                <!-- Sección de Ciudades_entrega con más despachos -->
-                <div class="content-box1 ciudades_entrega seccion_dashboard1">
+                </div> -->
+            <!-- Fin Sección de productos_entrega por cantidad -->
+            <!-- Sección de Ciudades_entrega con más despachos -->
+            <!-- <div class="content-box1 ciudades_entrega seccion_dashboard1">
                     <h4>Ciudades con mayores entregadas</h4>
                     <div id="ciudadesEntregadas-container"></div>
                 </div>
             </div>
-        </div>
+        </div> -->
 
-        <div class="tablas_estaditicas">
-            <div class="content-container">
-                <!-- Sección de productos_devolucion por cantidad -->
-                <div class="content-box1 productos_devolucion seccion_dashboard1">
+            <!-- <div class="tablas_estaditicas">
+            <div class="content-container"> -->
+            <!-- Sección de productos_devolucion por cantidad -->
+            <!-- <div class="content-box1 productos_devolucion seccion_dashboard1">
                     <h4>Productos devueltos por cantidad</h4>
                     <div id="productsDevolucion-container"></div>
-                </div>
+                </div> -->
 
-                <!-- Sección de Ciudades_devolucion con más despachos -->
-                <div class="content-box1 ciudades_devolucion seccion_dashboard1">
+            <!-- Sección de Ciudades_devolucion con más despachos -->
+            <!-- <div class="content-box1 ciudades_devolucion seccion_dashboard1">
                     <h4>Ciudades con mayores devoluciones</h4>
                     <div id="ciudadesDevolucion-container"></div>
                 </div>
             </div>
+        </div> -->
+        <?php } ?>
+
         </div>
-    <?php } ?>
 
-</div>
+        <script src="<?php echo SERVERURL ?>/Views/Dashboard/js/dashboard2.js"></script>
 
-<script src="<?php echo SERVERURL ?>/Views/Dashboard/js/dashboard.js"></script>
-
-<?php require_once './Views/templates/footer.php'; ?>
+        <?php require_once './Views/templates/footer.php'; ?>
