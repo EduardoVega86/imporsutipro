ef3dbe67fe80c0ee2daba986254751fcbef6d4c6
Sebastian Ordoñez <desarrollo3imporfactory@gmail.com>
2025-03-26 13:06:19 -0500

    Duplicamos vista con nuevas implementaciones para permitir login de facebook] - Sebastian

--- Views/Pedidos/Modales/modal_conectar_whatsapp.php
+++ Views/Pedidos/Modales/modal_conectar_whatsapp.php
@@ -1,3 +1,4 @@
+<!-- Modal para conectar a WhatsApp -->
 <div class="modal fade" id="modalConectarWhatsApp" tabindex="-1" aria-labelledby="modalConectarWhatsAppLabel" aria-hidden="true">
     <div class="modal-dialog modal-xl modal-dialog-centered">
         <div class="modal-content">
@@ -5,15 +6,26 @@
                 <h5 class="modal-title" id="modalConectarWhatsAppLabel">Conectar número de WhatsApp</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
             </div>
-            <div class="modal-body">
-                <iframe
-                    src="https://www.facebook.com/business/wa/onboarding?app_id=1790001771377467&redirect_uri=<?php echo SERVERURL; ?>controladores/WhatsappController.php?m=onboarding"
-                    width="100%"
-                    height="700px"
-                    style="border:none; overflow:hidden;"
-                    allowfullscreen>
-                </iframe>
+            <div class="modal-body text-center">
+                <p>
+                    Para iniciar el proceso de configuración de tu número de WhatsApp,
+                    da clic en el siguiente botón. Se abrirá la ventana de Onboarding
+                    de Facebook en otra pestaña.
+                </p>
+                <button class="btn btn-primary" onclick="abrirOnboardingWhatsApp()">
+                    Abrir Onboarding de WhatsApp
+                </button>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    function abrirOnboardingWhatsApp() {
+        // Abre la URL de onboarding en una pestaña nueva:
+        window.open(
+            'https://www.facebook.com/business/wa/onboarding?app_id=1790001771377467&redirect_uri=<?php echo SERVERURL; ?>controladores/WhatsappController.php?m=onboarding',
+            '_blank'
+        );
+    }
+</script>
