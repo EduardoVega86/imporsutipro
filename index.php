cce75be718fc483c2acbdcb41dade1d2386db14e
Jeimy Jara <desarrollo1@imporfactoryusa.com>
2025-03-28 12:12:10 -0500

    Bodega: Fix

--- index.php
+++ index.php
@@ -52,20 +52,7 @@ if (!empty($array[2])) {
         $parameter = trim($parameter, '-||-');
     }
 }
-function loadViewScripts(string $controller, string $view): void
-{
-    $funcionalidad = explode('_', $view)[0] ?? '';
-    $modulo = explode('_', $view)[1] ?? '';
-    $path = "Views/{$controller}/js/" . $modulo . "/" . $funcionalidad;
-    echo $path;
-    if (is_dir($path)) {
-        foreach (scandir($path) as $file) {
-            if (pathinfo($file, PATHINFO_EXTENSION) === 'js') {
-                echo "<script src='" . SERVERURL . $path . "/" . $file . "'></script>" . PHP_EOL;
-            }
-        }
-    }
-}
+
 // Cargar archivos necesarios
 require_once 'Config/App/autoload.php';
 
@@ -81,4 +68,19 @@ if (file_exists($dirController)) {
     }
 } else {
     echo "No existe el controlador";
+}
+
+function loadViewScripts(string $controller, string $view): void
+{
+    $funcionalidad = explode('_', $view)[0] ?? '';
+    $modulo = explode('_', $view)[1] ?? '';
+    $path = "Views/{$controller}/js/" . $modulo . "/" . $funcionalidad;
+    echo $path;
+    if (is_dir($path)) {
+        foreach (scandir($path) as $file) {
+            if (pathinfo($file, PATHINFO_EXTENSION) === 'js') {
+                echo "<script src='" . SERVERURL . $path . "/" . $file . "'></script>" . PHP_EOL;
+            }
+        }
+    }
 }
