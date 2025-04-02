fba84be09cba62a9a004c484a82a46e5c90187dc
Sebastian Ordoñez <desarrollo3imporfactory@gmail.com>
2025-04-02 14:52:32 -0500

    Quitando limit 1 - Sebastian

--- Models/PedidosModel.php
+++ Models/PedidosModel.php
@@ -3220,7 +3220,7 @@ class PedidosModel extends Query
 
     public function cargarPlantillasWhatsApp($id_plataforma)
     {
-        $sql = "SELECT * FROM configuraciones WHERE id_plataforma = $id_plataforma LIMIT 1";
+        $sql = "SELECT * FROM configuraciones WHERE id_plataforma = $id_plataforma";
         $result = $this->select($sql);
 
         $whatsappId  = $result[0]['id_whatsapp'];
