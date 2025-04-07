cf62358239f1096ec973b463af42a1895cfd64a8
Tony Plaza - Desarrollador <desarrollo2imporfactory@gmail.com>
2025-04-07 16:45:48 -0500

    cambios para creacion de chats openia

--- Models/PedidosModel.php
+++ Models/PedidosModel.php
@@ -3408,7 +3408,7 @@ class PedidosModel extends Query
 
         if ($bloque_info) {
             $payload = [
-                "role" => "system",
+                "role" => "user",
                 "content" => "🧾 Información del cliente para usar como contexto:\n\n" . $bloque_info
             ];
 
